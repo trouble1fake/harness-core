72d9180f5503670fc7db4e54d5ccd982742150f9
K Rohit Reddy <rohit.reddy@harness.io>
2021-01-28 20:40:54 +0530

    [CE-3893]: Duplicate ChangeStreams Code from Search framework

---
+++ 360-change-data-capture/src/main/java/io/harness/changestreams/ChangeType.java
@@ -0,0 +1,9 @@
+package io.harness.changestreams;
+
+/**
+ * Possible types of operations that can take place.
+ *
+ * @author utkarsh
+ */
+
+public enum ChangeType { INSERT, DELETE, REPLACE, UPDATE, INVALIDATE }
