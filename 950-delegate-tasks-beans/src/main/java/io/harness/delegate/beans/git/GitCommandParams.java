1fa31013c79568eadba9c3fdc91033528c7954db
Matt Lin <matthew.lin@harness.io>
2021-01-29 07:01:43 -0800

    [DEL-1251]: checkstyle

--- 950-delegate-tasks-beans/src/main/java/io/harness/delegate/beans/git/GitCommandParams.java
+++ 950-delegate-tasks-beans/src/main/java/io/harness/delegate/beans/git/GitCommandParams.java
@@ -1,16 +1,11 @@
 package io.harness.delegate.beans.git;
 
 import io.harness.delegate.beans.connector.scm.ScmConnector;
-import io.harness.delegate.beans.connector.scm.adapter.ScmConnectorMapper;
-import io.harness.delegate.beans.executioncapability.ExecutionCapability;
-import io.harness.delegate.beans.executioncapability.ExecutionCapabilityDemander;
 import io.harness.delegate.task.TaskParameters;
-import io.harness.expression.ExpressionEvaluator;
 import io.harness.git.model.GitBaseRequest;
 import io.harness.ng.core.dto.secrets.SSHKeySpecDTO;
 import io.harness.security.encryption.EncryptedDataDetail;
 
-import java.util.Collections;
 import java.util.List;
 import lombok.Builder;
 import lombok.Value;
