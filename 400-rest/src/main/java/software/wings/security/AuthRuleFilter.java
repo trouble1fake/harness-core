669a21a5ac7b77195b6ac229ad3965f7452ba355
Rama Tummala <rama@harness.io>
2021-02-11 02:10:54 -0700

    [DX-2301]: Whitelist support for webhooks and Graphql apis (#20648) (#20657)
    These changes are to test whitelist changes on UAT env

--- 400-rest/src/main/java/software/wings/security/AuthRuleFilter.java
+++ 400-rest/src/main/java/software/wings/security/AuthRuleFilter.java
@@ -16,6 +16,8 @@ import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import static org.apache.commons.lang3.StringUtils.startsWith;
 
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.beans.FeatureFlag;
+import io.harness.beans.FeatureName;
 import io.harness.exception.AccessDeniedException;
 import io.harness.exception.InvalidRequestException;
 import io.harness.exception.WingsException;
@@ -24,6 +26,7 @@ import io.harness.security.annotations.HarnessApiKeyAuth;
 import io.harness.security.annotations.LearningEngineAuth;
 import io.harness.security.annotations.NextGenManagerAuth;
 import io.harness.security.annotations.PublicApi;
+import io.harness.security.annotations.PublicApiWithWhitelist;
 
 import software.wings.beans.Account;
 import software.wings.beans.AccountStatus;
@@ -100,6 +103,7 @@ public class AuthRuleFilter implements ContainerRequestFilter {
       "setup-as-code/yaml/internal/template-yaml-sync", "infrastructure-definitions/list"};
   private static final String[] EXEMPTED_URI_SUFFIXES = new String[] {"sales-contacts", "addSubdomainUrl"};
   private static final String USER_NOT_AUTHORIZED = "User not authorized";
+  private static final String X_FORWARDED_FOR = "X-Forwarded-For";
 
   @Context private ResourceInfo resourceInfo;
   @Context private HttpServletRequest servletRequest;
@@ -213,12 +217,20 @@ public class AuthRuleFilter implements ContainerRequestFilter {
       accountId = appService.get(appIdsFromRequest.get(0)).getAccountId();
     }
 
+    if (isPublicApiWithWhitelist()) {
+      checkForWhitelisting(accountId, FeatureName.WHITELIST_PUBLIC_API);
+      return;
+    }
+
     String uriPath = requestContext.getUriInfo().getPath();
 
-    if (isInternalGraphQLRequest(uriPath)) {
-      graphQLUtils.validateGraphQLCall(accountId, true);
-    } else if (isExternalGraphQLRequest(uriPath)) {
-      graphQLUtils.validateGraphQLCall(accountId, false);
+    if (isGraphQLRequest(uriPath)) {
+      checkForWhitelisting(accountId, FeatureName.WHITELIST_GRAPHQL);
+      if (isInternalGraphQLRequest(uriPath)) {
+        graphQLUtils.validateGraphQLCall(accountId, true);
+      } else if (isExternalGraphQLRequest(uriPath)) {
+        graphQLUtils.validateGraphQLCall(accountId, false);
+      }
     }
 
     boolean isHarnessUserExemptedRequest = isHarnessUserExemptedRequest(uriPath);
@@ -260,13 +272,7 @@ public class AuthRuleFilter implements ContainerRequestFilter {
     }
 
     if (servletRequest != null) {
-      String forwardedFor = servletRequest.getHeader("X-Forwarded-For");
-      String remoteHost = isNotBlank(forwardedFor) ? forwardedFor : servletRequest.getRemoteHost();
-      if (!whitelistService.isValidIPAddress(accountId, remoteHost)) {
-        String msg = "Current IP Address (" + remoteHost + ") is not whitelisted.";
-        log.warn(msg);
-        throw new WingsException(NOT_WHITELISTED_IP, USER).addParam("args", msg);
-      }
+      checkForWhitelisting(accountId, null);
     }
 
     requiredPermissionAttributes = getAllRequiredPermissionAttributes(requestContext);
@@ -309,6 +315,30 @@ public class AuthRuleFilter implements ContainerRequestFilter {
           }
         }
       }
+    }
+  }
+
+  private boolean isPublicApiWithWhitelist() {
+      Class<?> resourceClass = resourceInfo.getResourceClass();
+      Method resourceMethod = resourceInfo.getResourceMethod();
+      return resourceMethod.getAnnotation(PublicApiWithWhitelist.class) != null
+              || resourceClass.getAnnotation(PublicApiWithWhitelist.class) != null;
+  }
+
+  private void checkForWhitelisting(String accountId, FeatureName featureName) {
+    String forwardedFor = servletRequest.getHeader(X_FORWARDED_FOR);
+    String remoteHost = isNotBlank(forwardedFor) ? forwardedFor : servletRequest.getRemoteHost();
+    boolean isWhitelisted;
+    if (featureName == null) {
+      isWhitelisted = whitelistService.isValidIPAddress(accountId, remoteHost);
+    } else {
+      isWhitelisted = whitelistService.checkIfFeatureIsEnabledAndWhitelisting(accountId, remoteHost, featureName);
+    }
+
+    if (!isWhitelisted) {
+      String msg = "Current IP Address (" + remoteHost + ") is not whitelisted.";
+      log.warn(msg);
+      throw new AccessDeniedException(msg, USER);
     }
   }
 
