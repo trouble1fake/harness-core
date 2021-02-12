6d67ecf37e50c8f31743a9b33665c8018a7fdece
Shivakumar Ningappa <shivakumar.ningappa@harness.io>
2021-02-12 04:38:40 -0800

    [CI-737]: add unit tests for test/source matchers

---
+++ 980-java-agent/src/test/java/io/harness/callgraph/util/SourceClassesMatcherTest.java
@@ -0,0 +1,42 @@
+package io.harness.callgraph.util;
+
+import static io.harness.rule.OwnerRule.SHIVAKUMAR;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import io.harness.callgraph.MockitoRule;
+import io.harness.category.element.UnitTests;
+import io.harness.rule.Owner;
+
+import java.security.ProtectionDomain;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.utility.JavaModule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.rules.TestRule;
+import org.mockito.Mock;
+
+public class SourceClassesMatcherTest {
+  @Rule public TestRule mockitoRule = new MockitoRule(this);
+
+  @Mock private TypeDescription typeDescription;
+
+  @Mock private ClassLoader classLoader;
+
+  @Mock private JavaModule module;
+
+  @Mock private ProtectionDomain protectionDomain;
+
+  @Test
+  @Owner(developers = SHIVAKUMAR)
+  @Category(UnitTests.class)
+  public void testMatches() throws Exception {
+    SourceClassesMatcher rawMatcher = new SourceClassesMatcher(false);
+    assertThat(rawMatcher.matches(typeDescription, classLoader, module, Foo.class, protectionDomain), is(true));
+  }
+
+  private static class Foo { /* empty */
+  }
+}
