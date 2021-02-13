fe3c08634439a58f9ed17425d5e7da71de697ee5
Shubhanshu Verma <shubhanshu.verma@harness.io>
2021-02-14 02:48:54 +0530

    [BT-218]: Updated batch processinng app

--- 280-batch-processing/src/main/java/io/harness/batch/processing/BatchProcessingApplication.java
+++ 280-batch-processing/src/main/java/io/harness/batch/processing/BatchProcessingApplication.java
@@ -9,14 +9,21 @@ import org.springframework.boot.autoconfigure.hazelcast.HazelcastAutoConfigurati
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
-@SpringBootApplication(
-    exclude = {DataSourceAutoConfiguration.class, MongoAutoConfiguration.class, MongoDataAutoConfiguration.class,
-        EmbeddedMongoAutoConfiguration.class, HazelcastAutoConfiguration.class})
+@SpringBootApplication
+@ComponentScan(excludeFilters =
+    {
+      @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+          classes = {DataSourceAutoConfiguration.class, MongoAutoConfiguration.class, MongoDataAutoConfiguration.class,
+              EmbeddedMongoAutoConfiguration.class, HazelcastAutoConfiguration.class})
+    })
 @EnableBatchProcessing(modular = true)
 @Slf4j
 public class BatchProcessingApplication {
   public static void main(String[] args) {
+    log.info("yo howdy");
     SpringApplication.run(BatchProcessingApplication.class, args);
   }
 }
