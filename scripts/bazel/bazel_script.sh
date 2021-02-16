f2803fed181c7aa06062c853100ed3b77552bd91
Brijesh Dhakar <brijesh.dhakar@harness.io>
2021-02-16 17:46:04 +0530

    [BT-254]: correcting bazel_script

--- scripts/bazel/bazel_script.sh
+++ scripts/bazel/bazel_script.sh
@@ -32,8 +32,8 @@ fi
 
 if [ "${RUN_BAZEL_TESTS}" == "true" ]
 then
-  bazel ${bazelrc} build ${GCP} ${BAZEL_ARGUMENTS} -- //... -//product/... -//commons/... -//260-delegate/...  -//120-ng-manager/... -//310-ci-manager/... -//320-ci-execution/... -//330-ci-beans/...
-  bazel ${bazelrc} test --keep_going ${GCP} ${BAZEL_ARGUMENTS} -- //... -//product/... -//commons/... -//260-delegate/... -//120-ng-manager/...  -//310-ci-manager/... -//320-ci-execution/... -//330-ci-beans/... || true
+  bazel ${bazelrc} build ${GCP} ${BAZEL_ARGUMENTS} -- //... -//product/... -//commons/...  -//120-ng-manager/... -//310-ci-manager/... -//320-ci-execution/... -//330-ci-beans/...
+  bazel ${bazelrc} test --keep_going ${GCP} ${BAZEL_ARGUMENTS} -- //... -//product/... -//commons/... -//120-ng-manager/...  -//310-ci-manager/... -//320-ci-execution/... -//330-ci-beans/... || true
 fi
 
 if [ "${RUN_CHECKS}" == "true" ]
@@ -61,6 +61,8 @@ BAZEL_MODULES="\
   //136-git-sync-manager:module \
   //220-graphql-test:supporter-test \
   //230-model-test:module \
+  //260-delegate:module \
+  //260-delegate:module_deploy.jar \
   //340-ce-nextgen:module \
   //350-event-server:module \
   //350-event-server:module_deploy.jar \
@@ -285,6 +287,7 @@ build_bazel_application 940-notification-client
 build_bazel_application 350-event-server
 build_bazel_application 360-cg-manager
 build_bazel_application 160-model-gen-tool
+build_bazel_application 260-delegate
 build_bazel_module 125-cd-nextgen
 build_bazel_module 130-resource-group
 build_bazel_module 136-git-sync-manager
