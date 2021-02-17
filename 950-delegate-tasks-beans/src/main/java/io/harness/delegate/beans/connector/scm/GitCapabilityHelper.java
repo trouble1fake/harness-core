e69b8ac20a96368e48fe64a9fff6f859deb7c309
Matt Lin <matthew.lin@harness.io>
2021-02-17 15:41:01 -0800

    [DEL-1251]: sync update

--- 950-delegate-tasks-beans/src/main/java/io/harness/delegate/beans/connector/scm/GitCapabilityHelper.java
+++ 950-delegate-tasks-beans/src/main/java/io/harness/delegate/beans/connector/scm/GitCapabilityHelper.java
@@ -3,11 +3,13 @@ package io.harness.delegate.beans.connector.scm;
 import io.harness.delegate.beans.connector.scm.adapter.ScmConnectorMapper;
 import io.harness.delegate.beans.connector.scm.genericgitconnector.GitConfigDTO;
 import io.harness.delegate.beans.executioncapability.ExecutionCapability;
-import io.harness.delegate.beans.executioncapability.GitConnectionNGCapability;
+import io.harness.delegate.beans.executioncapability.GitInstallationCapability;
+import io.harness.delegate.beans.executioncapability.HttpConnectionExecutionCapability;
 import io.harness.expression.ExpressionEvaluator;
 import io.harness.ng.core.dto.secrets.SSHKeySpecDTO;
 import io.harness.security.encryption.EncryptedDataDetail;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import lombok.experimental.UtilityClass;
@@ -16,10 +18,9 @@ import lombok.experimental.UtilityClass;
 public class GitCapabilityHelper {
   public List<ExecutionCapability> fetchRequiredExecutionCapabilities(ExpressionEvaluator maskingEvaluator,
       GitConfigDTO gitConfig, List<EncryptedDataDetail> encryptionDetails, SSHKeySpecDTO sshKeySpecDTO) {
-    return Collections.singletonList(GitConnectionNGCapability.builder()
-                                         .encryptedDataDetails(encryptionDetails)
-                                         .gitConfig(ScmConnectorMapper.toGitConfigDTO(gitConfig))
-                                         .sshKeySpecDTO(sshKeySpecDTO)
-                                         .build());
+    List<ExecutionCapability> executionCapabilities = new ArrayList<>();
+    executionCapabilities.add(GitInstallationCapability.builder().build());
+    executionCapabilities.add(HttpConnectionExecutionCapability.builder().url(gitConfig.getUrl()).build());
+    return executionCapabilities;
   }
 }
