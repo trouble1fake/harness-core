b80b191b980304d28673cb414dcaaf1955153220
Rohit Karelia <rohit.karelia@harness.io>
2021-02-17 10:12:20 -0800

    [CDC-11270]: Support to configure secretManagerCache expiry ttl

---
+++ 420-delegate-agent/src/main/java/io/harness/delegate/provider/NoopDelegatePropertiesServiceProviderImpl.java
@@ -0,0 +1,12 @@
+package io.harness.delegate.provider;
+
+import io.harness.delegate.DelegatePropertiesServiceProvider;
+import io.harness.managerclient.GetDelegatePropertiesRequest;
+import io.harness.managerclient.GetDelegatePropertiesResponse;
+
+public class NoopDelegatePropertiesServiceProviderImpl implements DelegatePropertiesServiceProvider {
+  @Override
+  public GetDelegatePropertiesResponse getDelegateProperties(GetDelegatePropertiesRequest request) {
+    return null;
+  }
+}
