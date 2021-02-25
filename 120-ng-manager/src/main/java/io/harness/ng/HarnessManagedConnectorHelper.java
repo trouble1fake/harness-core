af65b7f7f76fbfb827ab668ea768cf6250ebd299
Sri Hari Chidella <srihari.chidella@harness.io>
2021-02-25 17:43:10 +0530

    [CDNG-4662]: HarnessManagedSecret

---
+++ 120-ng-manager/src/main/java/io/harness/ng/HarnessManagedConnectorHelper.java
@@ -0,0 +1,26 @@
+package io.harness.ng;
+
+import io.harness.connector.ConnectorInfoDTO;
+import io.harness.delegate.beans.connector.gcpkmsconnector.GcpKmsConnectorDTO;
+import io.harness.delegate.beans.connector.localconnector.LocalConnectorDTO;
+
+import com.google.inject.Singleton;
+
+@Singleton
+public class HarnessManagedConnectorHelper {
+  public HarnessManagedConnectorHelper() {}
+
+  public boolean isHarnessManagedSecretManager(ConnectorInfoDTO connector) {
+    if (connector == null) {
+      return false;
+    }
+    switch (connector.getConnectorType()) {
+      case GCP_KMS:
+        return ((GcpKmsConnectorDTO) connector.getConnectorConfig()).isHarnessManaged();
+      case LOCAL:
+        return ((LocalConnectorDTO) connector.getConnectorConfig()).isHarnessManaged();
+      default:
+        return false;
+    }
+  }
+}
