02411a9e70f09e905ebeec4749d13731e039ae68
K Rohit Reddy <rohit.reddy@harness.io>
2021-03-03 19:08:36 +0530

    [CE-3893]: TimescaleDB Handler Update and Delete Handling

--- 110-change-data-capture/src/main/java/io/harness/TimeScaleDBChangeHandler.java
+++ 110-change-data-capture/src/main/java/io/harness/TimeScaleDBChangeHandler.java
@@ -10,6 +10,7 @@ import com.google.inject.Singleton;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
@@ -26,7 +27,14 @@ public class TimeScaleDBChangeHandler implements ChangeHandler {
     log.info("In TimeScale Change Handler: {}, {}, {}", changeEvent, tableName, fields);
     switch (changeEvent.getChangeType()) {
       case INSERT:
-        create(tableName, getColumnValueMapping(changeEvent, fields));
+        dbOperation(insertSQL(tableName, getColumnValueMapping(changeEvent, fields)));
+        break;
+      case UPDATE:
+        dbOperation(updateSQL(tableName, getColumnValueMapping(changeEvent, fields),
+            Collections.singletonMap("UUID", changeEvent.getUuid())));
+        break;
+      case DELETE:
+        dbOperation(deleteSQL(tableName, Collections.singletonMap("UUID", changeEvent.getUuid())));
         break;
       default:
         log.info("Change Event Type not Handled: {}", changeEvent.getChangeType());
@@ -34,25 +42,24 @@ public class TimeScaleDBChangeHandler implements ChangeHandler {
     return false;
   }
 
-  public boolean create(String tableName, Map<String, String> columnValueMappingForInsert) {
-    boolean successfulInsert = false;
+  public boolean dbOperation(String query) {
+    boolean successfulOperation = false;
     if (timeScaleDBService.isValid()) {
       int retryCount = 0;
-      while (!successfulInsert && retryCount < MAX_RETRY_COUNT) {
+      while (!successfulOperation && retryCount < MAX_RETRY_COUNT) {
         try (Connection dbConnection = timeScaleDBService.getDBConnection();
-             PreparedStatement statement =
-                 dbConnection.prepareStatement(insertSQL(tableName, columnValueMappingForInsert))) {
+             PreparedStatement statement = dbConnection.prepareStatement(query)) {
           statement.execute();
-          successfulInsert = true;
+          successfulOperation = true;
         } catch (SQLException e) {
-          log.error("Failed to save ceCloudAccount data,retryCount=[{}], Exception: ", retryCount, e);
+          log.error("Failed to save/update/delete data Query = {},retryCount=[{}], Exception: ", query, retryCount, e);
           retryCount++;
         }
       }
     } else {
       log.warn("TimeScale Down");
     }
-    return successfulInsert;
+    return successfulOperation;
   }
 
   private Map<String, String> getColumnValueMapping(ChangeEvent<?> changeEvent, String[] fields) {
@@ -110,5 +117,94 @@ public class TimeScaleDBChangeHandler implements ChangeHandler {
 
     // Returning the generated INSERT SQL Query as a String...
     return insertSQLBuilder.toString();
+  }
+
+  public static String updateSQL(String tableName, Map<String, String> columnValueMappingForSet,
+      Map<String, String> columnValueMappingForCondition) {
+    StringBuilder updateQueryBuilder = new StringBuilder();
+
+    /**
+     * Removing column that holds NULL value or Blank value...
+     */
+    if (!columnValueMappingForSet.isEmpty()) {
+      for (Map.Entry<String, String> entry : columnValueMappingForSet.entrySet()) {
+        if (entry.getValue() == null || entry.getValue().equals("")) {
+          columnValueMappingForSet.remove(entry.getKey());
+        }
+      }
+    }
+
+    /**
+     * Removing column that holds NULL value or Blank value...
+     */
+    if (!columnValueMappingForCondition.isEmpty()) {
+      for (Map.Entry<String, String> entry : columnValueMappingForCondition.entrySet()) {
+        if (entry.getValue() == null || entry.getValue().equals("")) {
+          columnValueMappingForCondition.remove(entry.getKey());
+        }
+      }
+    }
+
+    /* Making the UPDATE Query */
+    updateQueryBuilder.append("UPDATE");
+    updateQueryBuilder.append(" ").append(tableName);
+    updateQueryBuilder.append(" SET");
+    updateQueryBuilder.append(" ");
+
+    if (!columnValueMappingForSet.isEmpty()) {
+      for (Map.Entry<String, String> entry : columnValueMappingForSet.entrySet()) {
+        updateQueryBuilder.append(entry.getKey()).append("=").append(String.format("'%s'", entry.getValue()));
+        updateQueryBuilder.append(",");
+      }
+    }
+
+    updateQueryBuilder = new StringBuilder(updateQueryBuilder.subSequence(0, updateQueryBuilder.length() - 1));
+    updateQueryBuilder.append(" WHERE");
+    updateQueryBuilder.append(" ");
+
+    if (!columnValueMappingForCondition.isEmpty()) {
+      for (Map.Entry<String, String> entry : columnValueMappingForCondition.entrySet()) {
+        updateQueryBuilder.append(entry.getKey()).append("=").append(String.format("'%s'", entry.getValue()));
+        updateQueryBuilder.append(",");
+      }
+    }
+
+    updateQueryBuilder = new StringBuilder(updateQueryBuilder.subSequence(0, updateQueryBuilder.length() - 1));
+
+    // Returning the generated UPDATE SQL Query as a String...
+    return updateQueryBuilder.toString();
+  }
+
+  public static String deleteSQL(String tableName, Map<String, String> columnValueMappingForCondition) {
+    StringBuilder deleteSQLBuilder = new StringBuilder();
+
+    /**
+     * Removing column that holds NULL value or Blank value...
+     */
+    if (!columnValueMappingForCondition.isEmpty()) {
+      for (Map.Entry<String, String> entry : columnValueMappingForCondition.entrySet()) {
+        if (entry.getValue() == null || entry.getValue().equals("")) {
+          columnValueMappingForCondition.remove(entry.getKey());
+        }
+      }
+    }
+
+    /* Making the DELETE Query */
+    deleteSQLBuilder.append("DELETE FROM");
+    deleteSQLBuilder.append(" ").append(tableName);
+    deleteSQLBuilder.append(" WHERE");
+    deleteSQLBuilder.append(" ");
+
+    if (!columnValueMappingForCondition.isEmpty()) {
+      for (Map.Entry<String, String> entry : columnValueMappingForCondition.entrySet()) {
+        deleteSQLBuilder.append(entry.getKey()).append("=").append(String.format("'%s'", entry.getValue()));
+        deleteSQLBuilder.append(" AND ");
+      }
+    }
+
+    deleteSQLBuilder = new StringBuilder(deleteSQLBuilder.subSequence(0, deleteSQLBuilder.length() - 5));
+
+    // Returning the generated DELETE SQL Query as a String...
+    return deleteSQLBuilder.toString();
   }
 }
