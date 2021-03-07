dba0cc3c745eade3d8b5b96ddc9b7f3e4aa2103e
Brijesh Dhakar <brijesh.dhakar@harness.io>
2021-03-07 22:40:15 +0530

    [CDNG-4942]: removing unused method in AwsConfigMapper

--- 400-rest/src/main/java/software/wings/service/mappers/artifact/AwsConfigToInternalMapper.java
+++ 400-rest/src/main/java/software/wings/service/mappers/artifact/AwsConfigToInternalMapper.java
@@ -1,24 +1,11 @@
 package software.wings.service.mappers.artifact;
 
 import io.harness.aws.beans.AwsInternalConfig;
-
-import software.wings.beans.AwsConfig;
-import software.wings.beans.AwsCrossAccountAttributes;
-
 import lombok.experimental.UtilityClass;
+import software.wings.beans.AwsConfig;
 
 @UtilityClass
 public class AwsConfigToInternalMapper {
-  public AwsInternalConfig toAwsInternalConfig(String AWS_URL, char[] accessKey, char[] secretKey,
-      boolean useEc2IamCredentials, AwsCrossAccountAttributes crossAccountAttributes, String defaultRegion) {
-    return AwsInternalConfig.builder()
-        .accessKey(accessKey)
-        .secretKey(secretKey)
-        .useEc2IamCredentials(useEc2IamCredentials)
-        .crossAccountAttributes(crossAccountAttributes)
-        .defaultRegion(defaultRegion)
-        .build();
-  }
   public AwsInternalConfig toAwsInternalConfig(AwsConfig awsConfig) {
     return AwsInternalConfig.builder()
         .accessKey(awsConfig.getAccessKey())
