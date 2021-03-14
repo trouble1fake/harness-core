e24bca422446025310a75791754ea7e2c9a700dc
Sri Hari Chidella <srihari.chidella@harness.io>
2021-03-14 13:37:38 +0530

    [DX-2394]: Promote Inner Classes 2

---
+++ 400-rest/src/main/java/software/wings/verification/datadog/DatadogLogCVConfigurationYaml.java
@@ -0,0 +1,20 @@
+package software.wings.verification.datadog;
+
+import io.harness.annotations.dev.Module;
+import io.harness.annotations.dev.TargetModule;
+
+import software.wings.verification.log.LogsCVConfiguration;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@TargetModule(Module._870_CG_YAML_BEANS)
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@JsonPropertyOrder({"type", "harnessApiVersion"})
+public final class DatadogLogCVConfigurationYaml extends LogsCVConfiguration.LogsCVConfigurationYaml {
+  private String hostnameField;
+}
