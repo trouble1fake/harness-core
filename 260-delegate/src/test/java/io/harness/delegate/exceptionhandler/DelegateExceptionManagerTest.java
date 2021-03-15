5f58b9920f74398cf4f52ed49ab60e205ca2d63f
Mohit Garg <mohit.garg@harness.io>
2021-03-15 17:28:52 +0530

    [CDNG-5126]: added test cases for delegate exception manager

--- 260-delegate/src/test/java/io/harness/delegate/exceptionhandler/DelegateExceptionManagerTest.java
+++ 260-delegate/src/test/java/io/harness/delegate/exceptionhandler/DelegateExceptionManagerTest.java
@@ -8,6 +8,11 @@ import io.harness.DelegateTestBase;
 import io.harness.category.element.UnitTests;
 import io.harness.delegate.beans.DelegateResponseData;
 import io.harness.delegate.beans.ErrorNotifyResponseData;
+import io.harness.exception.ExceptionUtils;
+import io.harness.exception.InvalidArtifactServerException;
+import io.harness.exception.InvalidRequestException;
+import io.harness.exception.UnexpectedException;
+import io.harness.exception.WingsException;
 import io.harness.rule.Owner;
 
 import com.amazonaws.services.codedeploy.model.AmazonCodeDeployException;
@@ -28,7 +33,7 @@ public class DelegateExceptionManagerTest extends DelegateTestBase {
   @Test
   @Owner(developers = MOHIT_GARG)
   @Category(UnitTests.class)
-  public void testBasicSanitCaseForSingleException() {
+  public void testBasicSanityCaseForSingleException() {
     AmazonCodeDeployException amazonServiceException = new AmazonCodeDeployException("Amazon Code deploy exception");
     DelegateResponseData delegateResponseData =
         delegateExceptionManager.getResponseData(amazonServiceException, null, true);
@@ -36,5 +41,53 @@ public class DelegateExceptionManagerTest extends DelegateTestBase {
 
     ErrorNotifyResponseData errorNotifyResponseData = (ErrorNotifyResponseData) delegateResponseData;
     assertThat(errorNotifyResponseData.getException()).isNotNull();
+  }
+
+  @Test
+  @Owner(developers = MOHIT_GARG)
+  @Category(UnitTests.class)
+  public void testUnhandledException() {
+    RuntimeException exception = new RuntimeException("Unknown Runtime exception");
+    DelegateResponseData delegateResponseData = delegateExceptionManager.getResponseData(exception, null, true);
+    assertThat(delegateResponseData).isNotNull();
+
+    ErrorNotifyResponseData errorNotifyResponseData = (ErrorNotifyResponseData) delegateResponseData;
+    assertThat(errorNotifyResponseData.getException()).isNotNull();
+    assertThat(errorNotifyResponseData.getException() instanceof UnexpectedException).isTrue();
+  }
+
+  @Test
+  @Owner(developers = MOHIT_GARG)
+  @Category(UnitTests.class)
+  public void testExceptionWhenFrameworkUnsupported() {
+    RuntimeException exception = new RuntimeException("Unknown Runtime exception");
+    DelegateResponseData delegateResponseData = delegateExceptionManager.getResponseData(exception, null, false);
+    assertThat(delegateResponseData).isNotNull();
+
+    ErrorNotifyResponseData errorNotifyResponseData = (ErrorNotifyResponseData) delegateResponseData;
+    assertThat(errorNotifyResponseData.getException()).isNull();
+    assertThat(errorNotifyResponseData.getErrorMessage().equals(ExceptionUtils.getMessage(exception))).isTrue();
+    assertThat(errorNotifyResponseData.getFailureTypes().equals(ExceptionUtils.getFailureTypes(exception))).isTrue();
+  }
+
+  @Test
+  @Owner(developers = MOHIT_GARG)
+  @Category(UnitTests.class)
+  public void testCascadedException() {
+    AmazonCodeDeployException amazonServiceException = new AmazonCodeDeployException("Amazon Code deploy exception");
+    InvalidArtifactServerException invalidArtifactServerException =
+        new InvalidArtifactServerException("Invalid Artifact Server", amazonServiceException);
+
+    DelegateResponseData delegateResponseData =
+        delegateExceptionManager.getResponseData(invalidArtifactServerException, null, true);
+    assertThat(delegateResponseData).isNotNull();
+
+    ErrorNotifyResponseData errorNotifyResponseData = (ErrorNotifyResponseData) delegateResponseData;
+    assertThat(errorNotifyResponseData.getException()).isNotNull();
+
+    WingsException exception = errorNotifyResponseData.getException();
+    assertThat(exception instanceof InvalidArtifactServerException).isTrue();
+    Exception cause = (Exception) exception.getCause();
+    assertThat(cause instanceof InvalidRequestException).isTrue();
   }
 }
