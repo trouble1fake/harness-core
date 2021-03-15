5dfc9bbcca823639d0b2e46d67034de1c8c4e3b3
Deepak Puthraya <deepak.puthraya@harness.io>
2021-03-15 15:54:32 +0530

    [CDC-12355]: Increase timeout to 15s behind FF (#21461)
    * [CDC-12355]: Increase timeout to 15s behind FF
    
    * [CDC-12355]: Update build number

--- 400-rest/src/main/java/software/wings/service/impl/artifact/ArtifactCollectionServiceAsyncImpl.java
+++ 400-rest/src/main/java/software/wings/service/impl/artifact/ArtifactCollectionServiceAsyncImpl.java
@@ -20,7 +20,6 @@ import static software.wings.beans.artifact.ArtifactStreamType.GCS;
 import static software.wings.beans.artifact.ArtifactStreamType.NEXUS;
 import static software.wings.beans.artifact.ArtifactStreamType.SFTP;
 import static software.wings.beans.artifact.ArtifactStreamType.SMB;
-import static software.wings.service.impl.artifact.ArtifactCollectionUtils.DELEGATE_QUEUE_TIMEOUT;
 
 import static java.util.Arrays.asList;
 
@@ -136,10 +135,11 @@ public class ArtifactCollectionServiceAsyncImpl implements ArtifactCollectionSer
 
     String waitId = generateUuid();
     final TaskDataBuilder dataBuilder = TaskData.builder().async(true).taskType(TaskType.BUILD_SOURCE_TASK.name());
-    DelegateTaskBuilder delegateTaskBuilder = DelegateTask.builder()
-                                                  .setupAbstraction(Cd1SetupFields.APP_ID_FIELD, GLOBAL_APP_ID)
-                                                  .waitId(waitId)
-                                                  .expiry(System.currentTimeMillis() + DELEGATE_QUEUE_TIMEOUT);
+    DelegateTaskBuilder delegateTaskBuilder =
+        DelegateTask.builder()
+            .setupAbstraction(Cd1SetupFields.APP_ID_FIELD, GLOBAL_APP_ID)
+            .waitId(waitId)
+            .expiry(artifactCollectionUtils.getDelegateQueueTimeout(artifactStream.getAccountId()));
 
     if (CUSTOM.name().equals(artifactStreamType)) {
       ArtifactStreamAttributes artifactStreamAttributes =
