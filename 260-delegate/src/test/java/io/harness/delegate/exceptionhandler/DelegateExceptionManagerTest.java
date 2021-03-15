dbbec3beec262a43ad1c5d115e1e3f414e9b9b1e
Mohit Garg <mohit.garg@harness.io>
2021-03-15 12:21:00 +0530

    [CDNG-5126]: adding unit test cases for delegate error framework

--- 260-delegate/src/test/java/io/harness/delegate/exceptionhandler/DelegateExceptionManagerTest.java
+++ 260-delegate/src/test/java/io/harness/delegate/exceptionhandler/DelegateExceptionManagerTest.java
@@ -2,9 +2,12 @@ package io.harness.delegate.exceptionhandler;
 
 import static io.harness.rule.OwnerRule.MOHIT_GARG;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import io.harness.DelegateTestBase;
 import io.harness.category.element.UnitTests;
 import io.harness.delegate.beans.DelegateResponseData;
+import io.harness.delegate.beans.ErrorNotifyResponseData;
 import io.harness.rule.Owner;
 
 import com.amazonaws.services.codedeploy.model.AmazonCodeDeployException;
@@ -25,9 +28,13 @@ public class DelegateExceptionManagerTest extends DelegateTestBase {
   @Test
   @Owner(developers = MOHIT_GARG)
   @Category(UnitTests.class)
-  public void testMapAwsConfig() {
+  public void testBasicSanitCaseForSingleException() {
     AmazonCodeDeployException amazonServiceException = new AmazonCodeDeployException("Amazon Code deploy exception");
     DelegateResponseData delegateResponseData =
         delegateExceptionManager.getResponseData(amazonServiceException, null, true);
+    assertThat(delegateResponseData).isNotNull();
+
+    ErrorNotifyResponseData errorNotifyResponseData = (ErrorNotifyResponseData) delegateResponseData;
+    assertThat(errorNotifyResponseData.getException()).isNotNull();
   }
 }
