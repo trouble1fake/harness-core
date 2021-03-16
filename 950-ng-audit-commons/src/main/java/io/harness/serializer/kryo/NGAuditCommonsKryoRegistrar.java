9c65e9e7015cd059edb2ed8d4ef3eeeac6b8df72
Karan Siwach <karan.siwach@harness.io>
2021-03-16 14:48:52 +0530

    [CDNG-4979]: type

--- 950-ng-audit-commons/src/main/java/io/harness/serializer/kryo/NGAuditCommonsKryoRegistrar.java
+++ 950-ng-audit-commons/src/main/java/io/harness/serializer/kryo/NGAuditCommonsKryoRegistrar.java
@@ -3,6 +3,7 @@ package io.harness.serializer.kryo;
 import io.harness.audit.beans.AuditEventDTO;
 import io.harness.audit.beans.AuditEventData;
 import io.harness.audit.beans.AuthenticationInfo;
+import io.harness.audit.beans.Principal;
 import io.harness.audit.beans.YamlDiff;
 import io.harness.audit.beans.YamlRecord;
 import io.harness.serializer.KryoRegistrar;
@@ -13,9 +14,10 @@ public class NGAuditCommonsKryoRegistrar implements KryoRegistrar {
   @Override
   public void register(Kryo kryo) {
     kryo.register(AuditEventData.class, 17360);
-    kryo.register(AuthenticationInfo.class, 17361);
-    kryo.register(YamlRecord.class, 17362);
-    kryo.register(YamlDiff.class, 17363);
-    kryo.register(AuditEventDTO.class, 17364);
+    kryo.register(Principal.class, 17361);
+    kryo.register(AuthenticationInfo.class, 17362);
+    kryo.register(YamlRecord.class, 17363);
+    kryo.register(YamlDiff.class, 17364);
+    kryo.register(AuditEventDTO.class, 17365);
   }
 }
