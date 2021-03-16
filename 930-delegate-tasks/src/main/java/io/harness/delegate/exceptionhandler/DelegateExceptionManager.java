9a3d38e59d3bf254da4c9b4d624c57363c7694bb
Mohit Garg <mohit.garg@harness.io>
2021-03-17 03:05:52 +0530

    [CDNG-5126]: delegate exception manager added

---
+++ 930-delegate-tasks/src/main/java/io/harness/delegate/exceptionhandler/DelegateExceptionManager.java
@@ -0,0 +1,97 @@
+package io.harness.delegate.exceptionhandler;
+
+import io.harness.delegate.beans.DelegateResponseData;
+import io.harness.delegate.beans.ErrorNotifyResponseData;
+import io.harness.delegate.beans.ErrorNotifyResponseData.ErrorNotifyResponseDataBuilder;
+import io.harness.delegate.exceptionhandler.handler.DelegateExceptionHandler;
+import io.harness.exception.DelegateErrorHandlerException;
+import io.harness.exception.ExceptionUtils;
+import io.harness.exception.KryoHandlerNotFoundException;
+import io.harness.exception.WingsException;
+import io.harness.reflection.ReflectionUtils;
+import io.harness.serializer.KryoSerializer;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+
+@Singleton
+@Slf4j
+public class DelegateExceptionManager {
+  @Inject private Map<Class<? extends Exception>, DelegateExceptionHandler> exceptionHandler;
+  @Inject private KryoSerializer kryoSerializer;
+
+  public DelegateResponseData getResponseData(Exception exception,
+      ErrorNotifyResponseDataBuilder errorNotifyResponseDataBuilder, boolean isErrorFrameworkSupportedByTask) {
+    if (!isErrorFrameworkSupportedByTask) {
+      // return default response
+      return prepareErrorResponse(exception, errorNotifyResponseDataBuilder).build();
+    }
+
+    WingsException exception1 = handleException(exception);
+    DelegateResponseData responseData =
+        prepareErrorResponse(exception1, errorNotifyResponseDataBuilder).exception(exception1).build();
+
+    return responseData;
+  }
+
+  // ---------- PRIVATE METHODS -------------
+
+  private DelegateExceptionHandler getExceptionHandler(Exception exception) {
+    return exceptionHandler.entrySet()
+        .stream()
+        .filter(e -> e.getKey().isInstance(exception))
+        .map(Map.Entry::getValue)
+        .findFirst()
+        .orElse(null);
+  }
+
+  private WingsException handleException(Exception exception) {
+    WingsException handledException;
+    DelegateExceptionHandler delegateExceptionHandler = getExceptionHandler(exception);
+    try {
+      if (delegateExceptionHandler != null) {
+        handledException = delegateExceptionHandler.handleException(exception);
+      } else {
+        if (exception instanceof WingsException) {
+          handledException = (WingsException) exception;
+        } else {
+          throw new DelegateErrorHandlerException("Delegate exception handler not registered");
+        }
+      }
+
+      if (exception.getCause() != null) {
+        setExceptionCause(handledException, handleException((Exception) exception.getCause()));
+      }
+      return handledException;
+
+    } catch (Exception e) {
+      log.error("Exception occured while handling delegate exception : {}", exception, e);
+      return prepareUnhandledExceptionResponse(exception);
+    }
+  }
+
+  private WingsException prepareUnhandledExceptionResponse(Exception exception) {
+    Exception unhandledException = exception;
+    if (!kryoSerializer.isRegistered(unhandledException.getClass())) {
+      log.error("Kyro handler not found for exception", unhandledException);
+      unhandledException = new KryoHandlerNotFoundException(unhandledException.getMessage());
+    }
+    return new DelegateErrorHandlerException("Unable to handle delegate exception", unhandledException);
+  }
+
+  private ErrorNotifyResponseDataBuilder prepareErrorResponse(
+      Exception exception, ErrorNotifyResponseDataBuilder errorNotifyResponseDataBuilder) {
+    if (errorNotifyResponseDataBuilder == null) {
+      errorNotifyResponseDataBuilder = ErrorNotifyResponseData.builder();
+    }
+
+    return errorNotifyResponseDataBuilder.failureTypes(ExceptionUtils.getFailureTypes(exception))
+        .errorMessage(ExceptionUtils.getMessage(exception));
+  }
+
+  private void setExceptionCause(WingsException exception, Exception cause) throws IllegalAccessException {
+    ReflectionUtils.setObjectField(ReflectionUtils.getFieldByName(exception.getClass(), "cause"), exception, cause);
+  }
+}
