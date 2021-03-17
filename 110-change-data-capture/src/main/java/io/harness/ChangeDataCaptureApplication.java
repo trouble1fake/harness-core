137387f4686ef172668d2db9e81062638bb7c37b
K Rohit Reddy <rohit.reddy@harness.io>
2021-03-17 16:14:45 +0530

    [CE-4087]: Bazel Changes

--- 110-change-data-capture/src/main/java/io/harness/ChangeDataCaptureApplication.java
+++ 110-change-data-capture/src/main/java/io/harness/ChangeDataCaptureApplication.java
@@ -6,12 +6,15 @@ import io.harness.delegate.beans.DelegateAsyncTaskResponse;
 import io.harness.delegate.beans.DelegateSyncTaskResponse;
 import io.harness.delegate.beans.DelegateTaskProgressResponse;
 import io.harness.govern.ProviderModule;
+import io.harness.mongo.AbstractMongoModule;
 import io.harness.mongo.MongoConfig;
-import io.harness.mongo.MongoModule;
 import io.harness.morphia.MorphiaModule;
 import io.harness.persistence.HPersistence;
+import io.harness.persistence.NoopUserProvider;
 import io.harness.persistence.Store;
+import io.harness.persistence.UserProvider;
 import io.harness.serializer.PersistenceRegistrars;
+import io.harness.serializer.YamlUtils;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
@@ -53,7 +56,12 @@ public class ChangeDataCaptureApplication extends Application<ChangeDataCaptureS
       }
     });
 
-    modules.add(MongoModule.getInstance());
+    modules.add(new AbstractMongoModule() {
+      @Override
+      public UserProvider userProvider() {
+        return new NoopUserProvider();
+      }
+    });
 
     modules.add(new ProviderModule() {
       @Provides
