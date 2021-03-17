662a66aeebf14c3d157fd2dc700a3478ff67091f
Harsh Jain <harsh.jain@harness.io>
2021-03-17 20:04:37 +0530

    [CI-0]: check ado

---
+++ 330-ci-beans/src/main/java/io/harness/beans/execution/CustomExecutionSource.java
@@ -0,0 +1,23 @@
+package io.harness.beans.execution;
+
+import static io.harness.beans.execution.ExecutionSource.Type.CUSTOM;
+import static io.harness.beans.execution.ExecutionSource.Type.MANUAL;
+
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import lombok.Builder;
+import lombok.Value;
+import org.springframework.data.annotation.TypeAlias;
+
+@Value
+@Builder
+@JsonTypeName("Custom")
+@TypeAlias("CUSTOM")
+public class CustomExecutionSource implements ExecutionSource {
+  private String branch;
+  private String tag;
+
+  @Override
+  public Type getType() {
+    return CUSTOM;
+  }
+}
