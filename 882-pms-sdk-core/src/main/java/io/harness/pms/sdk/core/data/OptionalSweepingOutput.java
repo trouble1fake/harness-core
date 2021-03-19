1adcb10c05798b1dfba3c0e1655bf3ee3689107d
Sahil Hindwani <sahil.hindwani@harness.io>
2021-03-20 00:07:08 +0530

    [CDNG-0]: Expose one function to convert to sweepingOutput from string

--- 882-pms-sdk-core/src/main/java/io/harness/pms/sdk/core/data/OptionalSweepingOutput.java
+++ 882-pms-sdk-core/src/main/java/io/harness/pms/sdk/core/data/OptionalSweepingOutput.java
@@ -1,5 +1,7 @@
 package io.harness.pms.sdk.core.data;
 
+import io.harness.pms.serializer.recaster.RecastOrchestrationUtils;
+
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,4 +10,8 @@ import lombok.Data;
 public class OptionalSweepingOutput {
   boolean found;
   String output;
+
+  public SweepingOutput convertOutputToSweepingOutput() {
+    return RecastOrchestrationUtils.fromDocumentJson(output, SweepingOutput.class);
+  }
 }
