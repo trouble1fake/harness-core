b89fcd505dcfe5607a1335a8e2d0a376b6c35b38
Mohit Garg <mohit.garg@harness.io>
2021-03-22 11:49:23 +0530

    [CDNG-5126]: added new messages to exceptions

--- 980-commons/src/main/java/io/harness/exception/HintException.java
+++ 980-commons/src/main/java/io/harness/exception/HintException.java
@@ -7,6 +7,11 @@ import static io.harness.eraro.Level.INFO;
 import java.util.EnumSet;
 
 public class HintException extends WingsException {
+  public static final String HINT_EMPTY_ACCESS_KEY = "Check if access key is empty";
+  public static final String HINT_EMPTY_SECRET_KEY = "Check if secret key is empty";
+  public static final String HINT_AWS_IAM_ROLE_CHECK = "Check IAM role on delegate ec2";
+  public static final String HINT_AWS_CLIENT_UNKNOWN_ISSUE = "Check AWS client on delegate";
+
   public static final HintException MOVE_TO_THE_PARENT_OBJECT =
       new HintException("Navigate back to the parent object page and continue from there.");
   public static final HintException REFRESH_THE_PAGE = new HintException("Refresh the web page to update the data.");
