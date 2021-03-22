a71942c2c16661082a2d001190c4bfc00d64e599
Sahil Hindwani <sahil.hindwani@harness.io>
2021-03-22 17:41:00 +0530

    [CDNG-5274]: EnvironmentOutcome in sweepingOutput for expression resolution (#21779)
    * [CDNG-5274]: Add environmentOutcome to sweepingOutput
    
    * [CDNG-5274]: Update build.properties
    
    * [CDNG-5274]: Fix check-style:

--- 884-pms-commons/src/main/java/io/harness/pms/yaml/YAMLFieldNameConstants.java
+++ 884-pms-commons/src/main/java/io/harness/pms/yaml/YAMLFieldNameConstants.java
@@ -28,4 +28,5 @@ public class YAMLFieldNameConstants {
   public final String VALUE = "value";
   public final String UUID = "uuid";
   public final String TIMEOUT = "timeout";
+  public final String ENVIRONMENT = "environment";
 }
