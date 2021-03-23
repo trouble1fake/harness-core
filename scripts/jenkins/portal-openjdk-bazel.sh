e02eb56e0d259f46be3c9fb91c808bb40534cb4f
K Rohit Reddy <rohit.reddy@harness.io>
2021-03-23 19:09:25 +0530

    [CE-4087]: Dockerisation Bug Fix

--- scripts/jenkins/portal-openjdk-bazel.sh
+++ scripts/jenkins/portal-openjdk-bazel.sh
@@ -127,9 +127,9 @@ fi
 cd ../..
 
 mkdir -p dist/change-data-capture ;
-cd dist/event-server
+cd dist/change-data-capture
 cp ${HOME}/.bazel-dirs/bin/110-change-data-capture/module_deploy.jar change-data-capture.jar
-cp ../../350-event-server/config.yml .
+cp ../../110-change-data-capture/config.yml .
 cp ../../dockerization/change-data-capture/Dockerfile-change-data-capture-jenkins-k8-openjdk Dockerfile
 cp ../../dockerization/change-data-capture/Dockerfile-change-data-capture-jenkins-k8-gcr-openjdk Dockerfile-gcr
 cp -r ../../dockerization/change-data-capture/scripts/ .
