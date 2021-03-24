7445731da10809e4d5adc0280e89ce39e42b0d5b
Mohit Garg <mohit.garg@harness.io>
2021-03-24 17:59:35 +0530

    [CDNG-5126]: nested exception utils created to support different versions of nested exceptions

--- 980-commons/src/main/java/io/harness/exception/HintWithExplanationException.java
+++
@@ -1,7 +0,0 @@
-package io.harness.exception;
-
-public class HintWithExplanationException {
-  public static WingsException build(String hintMessage, String explanationMessage, Throwable cause) {
-    return new HintException(hintMessage, new ExplanationException(explanationMessage, cause));
-  }
-}
