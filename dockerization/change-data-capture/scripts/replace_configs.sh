0a11b67193987d02aed00cdcc888091365954b3b
K Rohit Reddy <rohit.reddy@harness.io>
2021-03-24 19:00:32 +0530

    [CE-4087]: Add Time Scale Config Replace

--- dockerization/change-data-capture/scripts/replace_configs.sh
+++ dockerization/change-data-capture/scripts/replace_configs.sh
@@ -37,3 +37,16 @@ if [[ "" != "$CDC_MONGO_URI" ]]; then
   yq write -i $CONFIG_FILE cdc-mongo.uri "$CDC_MONGO_URI"
 fi
 
+if [[ "" != "$TIMESCALEDB_URI" ]]; then
+  yq write -i $CONFIG_FILE timescaledb.timescaledbUrl "$TIMESCALEDB_URI"
+fi
+
+if [[ "" != "$TIMESCALEDB_USERNAME" ]]; then
+  yq write -i $CONFIG_FILE timescaledb.timescaledbUsername "$TIMESCALEDB_USERNAME"
+fi
+
+if [[ "" != "$TIMESCALEDB_PASSWORD" ]]; then
+  yq write -i $CONFIG_FILE timescaledb.timescaledbPassword "$TIMESCALEDB_PASSWORD"
+fi
+
+
