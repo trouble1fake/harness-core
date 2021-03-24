ae61f311c8a1397497089dc9c865c76abdfdf263
Garvit Pahal <garvit.pahal@harness.io>
2021-03-24 13:44:37 +0530

    [CDNG-0]: TMP

--- 884-pms-commons/src/main/java/io/harness/pms/yaml/ParameterFieldValueWrapper.java
+++ 884-pms-commons/src/main/java/io/harness/pms/yaml/ParameterFieldValueWrapper.java
@@ -2,8 +2,10 @@ package io.harness.pms.yaml;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ParameterFieldValueWrapper<T> {
   public static final String VALUE_FIELD = "value";
