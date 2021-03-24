8768fa022a371b9532da252bee43137e63224076
Inderpreet Chera <inderpreet.chera@harness.io>
2021-03-24 22:23:04 +0530

    [CDC-12244]: remove unused import

--- 400-rest/src/main/java/software/wings/security/AuthRuleFilter.java
+++ 400-rest/src/main/java/software/wings/security/AuthRuleFilter.java
@@ -83,7 +83,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Created by anubhaw on 3/11/16.
