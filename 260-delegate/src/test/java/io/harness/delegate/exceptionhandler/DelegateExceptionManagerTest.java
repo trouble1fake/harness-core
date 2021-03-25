4dfe3ebee17c6cddac8f77d399a75c62d0799d9e
Mohit Garg <mohit.garg@harness.io>
2021-03-26 01:29:25 +0530

    [CDNG-5126]: added unit tests for delegate exception manager

--- 260-delegate/src/test/java/io/harness/delegate/exceptionhandler/DelegateExceptionManagerTest.java
+++ 260-delegate/src/test/java/io/harness/delegate/exceptionhandler/DelegateExceptionManagerTest.java
@@ -5,11 +5,16 @@ import static io.harness.rule.OwnerRule.MOHIT_GARG;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.harness.DelegateTestBase;
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
 import io.harness.category.element.UnitTests;
 import io.harness.delegate.beans.DelegateResponseData;
 import io.harness.delegate.beans.ErrorNotifyResponseData;
 import io.harness.exception.DelegateErrorHandlerException;
 import io.harness.exception.ExceptionUtils;
+import io.harness.exception.ExplanationException;
+import io.harness.exception.HintException;
+import io.harness.exception.HourAggregationException;
 import io.harness.exception.InvalidArtifactServerException;
 import io.harness.exception.InvalidRequestException;
 import io.harness.exception.KryoHandlerNotFoundException;
@@ -23,6 +28,7 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.mockito.MockitoAnnotations;
 
+@OwnedBy(HarnessTeam.DX)
 public class DelegateExceptionManagerTest extends DelegateTestBase {
   @Inject DelegateExceptionManager delegateExceptionManager;
 
@@ -95,22 +101,20 @@ public class DelegateExceptionManagerTest extends DelegateTestBase {
   @Test
   @Owner(developers = MOHIT_GARG)
   @Category(UnitTests.class)
-  public void testUnhandledExceptionNotRegisteredWithKryo() {
-    String errorMessage = "Random exception not registered to Kryo";
-    RandomRuntimeException randomRuntimeException = new RandomRuntimeException(errorMessage);
+  public void testWingsExceptionNotRegisteredWithKryo() {
+    String errorMessage = "Random wings exception not registered to Kryo";
+    HourAggregationException hourAggregationException = new HourAggregationException(errorMessage, null);
 
     DelegateResponseData delegateResponseData =
-        delegateExceptionManager.getResponseData(randomRuntimeException, null, true);
+        delegateExceptionManager.getResponseData(hourAggregationException, null, true);
     assertThat(delegateResponseData).isNotNull();
 
     ErrorNotifyResponseData errorNotifyResponseData = (ErrorNotifyResponseData) delegateResponseData;
     assertThat(errorNotifyResponseData.getException()).isNotNull();
 
     WingsException exception = errorNotifyResponseData.getException();
-    assertThat(exception instanceof DelegateErrorHandlerException).isTrue();
-    Exception cause = (Exception) exception.getCause();
-    assertThat(cause instanceof KryoHandlerNotFoundException).isTrue();
-    assertThat(cause.getMessage().equals(errorMessage)).isTrue();
+    assertThat(exception instanceof KryoHandlerNotFoundException).isTrue();
+    assertThat(exception.getMessage().equals(errorMessage)).isTrue();
   }
 
   @Test
@@ -125,6 +129,33 @@ public class DelegateExceptionManagerTest extends DelegateTestBase {
     assertThat(errorNotifyResponseData.getException()).isNull();
     assertThat(errorNotifyResponseData.getErrorMessage().equals(ExceptionUtils.getMessage(throwable))).isTrue();
     assertThat(errorNotifyResponseData.getFailureTypes().equals(ExceptionUtils.getFailureTypes(throwable))).isTrue();
+  }
+
+  @Test
+  @Owner(developers = MOHIT_GARG)
+  @Category(UnitTests.class)
+  public void testCascadedExceptionNotRegisteredWithKryo() {
+    String errorMessage1 = "Error Message 1";
+    String errorMessage2 = "Error Message 2";
+    String errorMessage3 = "Error Message 3";
+    Exception cascadedException = new HintException(
+        errorMessage1, new HourAggregationException(errorMessage2, new ExplanationException(errorMessage3, null)));
+
+    DelegateResponseData delegateResponseData = delegateExceptionManager.getResponseData(cascadedException, null, true);
+    assertThat(delegateResponseData).isNotNull();
+
+    ErrorNotifyResponseData errorNotifyResponseData = (ErrorNotifyResponseData) delegateResponseData;
+    assertThat(errorNotifyResponseData.getException()).isNotNull();
+
+    WingsException exception = errorNotifyResponseData.getException();
+    assertThat(exception instanceof HintException).isTrue();
+    assertThat(exception.getMessage().equals(errorMessage1)).isTrue();
+    exception = (WingsException) exception.getCause();
+    assertThat(exception instanceof KryoHandlerNotFoundException).isTrue();
+    assertThat(exception.getMessage().equals(errorMessage2)).isTrue();
+    exception = (WingsException) exception.getCause();
+    assertThat(exception instanceof ExplanationException).isTrue();
+    assertThat(exception.getMessage().equals(errorMessage3)).isTrue();
   }
 
   public static class RandomRuntimeException extends RuntimeException {
