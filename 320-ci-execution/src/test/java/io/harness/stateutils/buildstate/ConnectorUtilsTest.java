e648a009b67f7a142ad546d86d306bfb91219a86
Aman Singh <aman.singh@harness.io>
2021-03-25 22:53:27 +0530

    Update ConnectorUtilsTest.java

--- 320-ci-execution/src/test/java/io/harness/stateutils/buildstate/ConnectorUtilsTest.java
+++ 320-ci-execution/src/test/java/io/harness/stateutils/buildstate/ConnectorUtilsTest.java
@@ -11,6 +11,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+
 import io.harness.beans.DecryptableEntity;
 import io.harness.category.element.UnitTests;
 import io.harness.connector.ConnectorDTO;
