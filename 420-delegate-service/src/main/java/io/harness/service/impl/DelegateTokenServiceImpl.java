0a49b9cb6707f181587e00e531eab4820f6bed22
Lucas Mari <lucas.mari@harness.io>
2021-03-29 12:08:10 -0300

    [DEL-1738]: Create DelegateTokenResource and DelegateTokenService in 420-service-delegate module

--- 420-delegate-service/src/main/java/io/harness/service/impl/DelegateTokenServiceImpl.java
+++ 420-delegate-service/src/main/java/io/harness/service/impl/DelegateTokenServiceImpl.java
@@ -2,7 +2,11 @@ package io.harness.service.impl;
 
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
-import io.harness.service.intfc.DelegateTokenService;
+import io.harness.persistence.HPersistence;
+
+import com.google.inject.Inject;
 
 @OwnedBy(HarnessTeam.DEL)
-public class DelegateTokenServiceImpl implements DelegateTokenService {}
+public class DelegateTokenServiceImpl {
+  @Inject private HPersistence persistence;
+}
