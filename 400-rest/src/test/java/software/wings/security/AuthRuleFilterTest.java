af13f0fb2677e418fac7672822fff49022f1f54c
Inderpreet Chera <inderpreet.chera@harness.io>
2021-03-30 15:02:17 +0530

    [CDC-12244]: removed unused import

--- 400-rest/src/test/java/software/wings/security/AuthRuleFilterTest.java
+++ 400-rest/src/test/java/software/wings/security/AuthRuleFilterTest.java
@@ -40,7 +40,6 @@ import static org.mockito.Matchers.anyBoolean;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
