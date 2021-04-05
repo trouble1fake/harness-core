a4bb593679a876fe79cf32a1e0a415127d0139fb
Nandan Chandrashekar <nandan.chandrashekar@harness.io>
2021-04-05 12:01:06 +0530

    [PL-13902]: updated AccessReqeustServiceImpl

--- 400-rest/src/main/java/software/wings/service/impl/AccessRequestServiceImpl.java
+++ 400-rest/src/main/java/software/wings/service/impl/AccessRequestServiceImpl.java
@@ -26,60 +26,60 @@ public class AccessRequestServiceImpl implements AccessRequestService {
   @Inject AccountService accountService;
   @Inject HarnessUserGroupService harnessUserGroupService;
 
-//  @Override
-//  public AccessRequest create(
-//      String accountId, String harnessUserGroupId, long accessStartAt, long accessEndAt, boolean accessActive) {
-//    AccessRequest accessRequest = AccessRequest.builder()
-//                                      .accountId(accountId)
-//                                      .harnessUserGroupId(harnessUserGroupId)
-//                                      .accessStartAt(accessStartAt)
-//                                      .accessEndAt(accessEndAt)
-//                                      .accessActive(accessActive)
-//                                      .build();
-//    String uuid = wingsPersistence.save(accessRequest);
-//    return wingsPersistence.get(AccessRequest.class, uuid);
-//  }
-//
-//  @Override
-//  public AccessRequest get(String accessRequestId) {
-//    return wingsPersistence.get(AccessRequest.class, accessRequestId);
-//  }
-//
-//  @Override
-//  public List<AccessRequest> getActiveAccessRequest(String harnessUserGroupId) {
-//    HarnessUserGroup harnessUserGroup = harnessUserGroupService.get(harnessUserGroupId);
-//    notNullCheck(String.format("Invalid Harness User Group with id: {}", harnessUserGroupId), harnessUserGroup);
-//    Query<AccessRequest> query = wingsPersistence.createQuery(AccessRequest.class, excludeAuthority);
-//    query.filter("harnessUserGroupId", harnessUserGroupId);
-//    query.filter("accessActive", true);
-//    return query.asList();
-//  }
-//
-//  @Override
-//  public List<AccessRequest> getActiveAccessRequestForAccount(String accountId) {
-//    Account account = accountService.get(accountId);
-//    notNullCheck("Invalid account with id: " + accountId, account);
-//    Query<AccessRequest> query = wingsPersistence.createQuery(AccessRequest.class, excludeAuthority);
-//    query.filter("harnessUserGroupId", accountId);
-//    query.filter("accessActive", true);
-//    return query.asList();
-//  }
-//
-//  @Override
-//  public AccessRequest update(String accessRequestId, long startTime, long endTime) {
-//    AccessRequest accessRequest = wingsPersistence.get(AccessRequest.class, accessRequestId);
-//    notNullCheck(String.format("Invalid Access Request with id: {}", accessRequestId), accessRequest);
-//    return accessRequest;
-//  }
+  @Override
+  public AccessRequest create(
+      String accountId, String harnessUserGroupId, long accessStartAt, long accessEndAt, boolean accessActive) {
+    AccessRequest accessRequest = AccessRequest.builder()
+                                      .accountId(accountId)
+                                      .harnessUserGroupId(harnessUserGroupId)
+                                      .accessStartAt(accessStartAt)
+                                      .accessEndAt(accessEndAt)
+                                      .accessActive(accessActive)
+                                      .build();
+    String uuid = wingsPersistence.save(accessRequest);
+    return wingsPersistence.get(AccessRequest.class, uuid);
+  }
+
+  @Override
+  public AccessRequest get(String accessRequestId) {
+    return wingsPersistence.get(AccessRequest.class, accessRequestId);
+  }
+
+  @Override
+  public List<AccessRequest> getActiveAccessRequest(String harnessUserGroupId) {
+    HarnessUserGroup harnessUserGroup = harnessUserGroupService.get(harnessUserGroupId);
+    notNullCheck(String.format("Invalid Harness User Group with id: {}", harnessUserGroupId), harnessUserGroup);
+    Query<AccessRequest> query = wingsPersistence.createQuery(AccessRequest.class, excludeAuthority);
+    query.filter("harnessUserGroupId", harnessUserGroupId);
+    query.filter("accessActive", true);
+    return query.asList();
+  }
+
+  @Override
+  public List<AccessRequest> getActiveAccessRequestForAccount(String accountId) {
+    Account account = accountService.get(accountId);
+    notNullCheck("Invalid account with id: " + accountId, account);
+    Query<AccessRequest> query = wingsPersistence.createQuery(AccessRequest.class, excludeAuthority);
+    query.filter("harnessUserGroupId", accountId);
+    query.filter("accessActive", true);
+    return query.asList();
+  }
+
+  @Override
+  public AccessRequest update(String accessRequestId, long startTime, long endTime) {
+    AccessRequest accessRequest = wingsPersistence.get(AccessRequest.class, accessRequestId);
+    notNullCheck(String.format("Invalid Access Request with id: {}", accessRequestId), accessRequest);
+    return accessRequest;
+  }
 
   @Override
   public boolean delete(String accessRequestId) {
     return wingsPersistence.delete(AccessRequest.class, accessRequestId);
   }
 
-//  @Override
-//  public AccessRequest updateStatus(String accessRequestId, boolean acccessStatus){
-//    AccessRequest accessRequest = AccessRequest.builder().build();
-//    return accessRequest;
-//  }
+  @Override
+  public AccessRequest updateStatus(String accessRequestId, boolean acccessStatus){
+    AccessRequest accessRequest = AccessRequest.builder().build();
+    return accessRequest;
+  }
 }
