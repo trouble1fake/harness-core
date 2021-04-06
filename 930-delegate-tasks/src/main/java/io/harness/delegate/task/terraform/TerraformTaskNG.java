9d2745599f0241dceb4394fe550e253dc6d3568a
Rohit Karelia <rohit.karelia@harness.io>
2021-04-06 11:04:34 -0700

    [CDNG-6761]: TF apply handler for CDNG

--- 930-delegate-tasks/src/main/java/io/harness/delegate/task/terraform/TerraformTaskNG.java
+++ 930-delegate-tasks/src/main/java/io/harness/delegate/task/terraform/TerraformTaskNG.java
@@ -3,12 +3,18 @@ package io.harness.delegate.task.terraform;
 import io.harness.delegate.beans.DelegateResponseData;
 import io.harness.delegate.beans.DelegateTaskPackage;
 import io.harness.delegate.beans.DelegateTaskResponse;
+import io.harness.delegate.beans.connector.scm.genericgitconnector.GitConfigDTO;
 import io.harness.delegate.beans.logstreaming.CommandUnitsProgress;
 import io.harness.delegate.beans.logstreaming.ILogStreamingTaskClient;
+import io.harness.delegate.beans.storeconfig.GitStoreDelegateConfig;
+import io.harness.delegate.git.NGGitService;
 import io.harness.delegate.task.AbstractDelegateRunnableTask;
 import io.harness.delegate.task.TaskParameters;
+import io.harness.delegate.task.shell.SshSessionConfigMapper;
 import io.harness.delegate.task.terraform.handlers.TerraformAbstractTaskHandler;
 import io.harness.exception.UnexpectedTypeException;
+import io.harness.git.model.AuthRequest;
+import io.harness.shell.SshSessionConfig;
 
 import com.google.inject.Inject;
 import java.util.Map;
@@ -17,6 +23,8 @@ import java.util.function.Consumer;
 
 public class TerraformTaskNG extends AbstractDelegateRunnableTask {
   @Inject private Map<TFTaskType, TerraformAbstractTaskHandler> tfTaskTypeToHandlerMap;
+  @Inject private SshSessionConfigMapper sshSessionConfigMapper;
+  @Inject private NGGitService ngGitService;
 
   public TerraformTaskNG(DelegateTaskPackage delegateTaskPackage, ILogStreamingTaskClient logStreamingTaskClient,
       Consumer<DelegateTaskResponse> consumer, BooleanSupplier preExecute) {
@@ -38,7 +46,15 @@ public class TerraformTaskNG extends AbstractDelegateRunnableTask {
           String.format("Unexpected Terraform Task Type: [%s]", taskParameters.getTaskType()));
     }
 
+    GitStoreDelegateConfig gitStoreDelegateConfig = taskParameters.getConfigFile().getGitStoreDelegateConfig();
+    SshSessionConfig sshSessionConfig = sshSessionConfigMapper.getSSHSessionConfig(
+        gitStoreDelegateConfig.getSshKeySpecDTO(), gitStoreDelegateConfig.getEncryptedDataDetails());
+
+    AuthRequest authRequest =
+        ngGitService.getAuthRequest((GitConfigDTO) gitStoreDelegateConfig.getGitConfigDTO(), sshSessionConfig);
+
     TerraformAbstractTaskHandler taskHandler = tfTaskTypeToHandlerMap.get(taskParameters.getTaskType());
-    return taskHandler.executeTask(taskParameters, getLogStreamingTaskClient(), commandUnitsProgress);
+    return taskHandler.executeTask(
+        taskParameters, getLogStreamingTaskClient(), commandUnitsProgress, authRequest, getDelegateId(), getTaskId());
   }
 }
