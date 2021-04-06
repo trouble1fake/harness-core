a30aca61024173f3fc62c64b4ac6a8d5e6369513
Meenakshi Raikwar <meenakshi.raikwar@harness.io>
2021-04-06 13:49:40 +0530

    reabased

--- 950-delegate-tasks-beans/src/main/java/io/harness/delegate/task/gcp/request/GcpRequest.java
+++ 950-delegate-tasks-beans/src/main/java/io/harness/delegate/task/gcp/request/GcpRequest.java
@@ -21,14 +21,7 @@ import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder
-<<<<<<< HEAD
-<<<<<<< HEAD
 @OwnedBy(CDP)
-=======
->>>>>>> [CDNG-5543)]: removed delete permission
-=======
-
->>>>>>> [CDNG-5543)]: removed delete permission
 public abstract class GcpRequest extends ConnectorTaskParams implements ExecutionCapabilityDemander {
   // Below 2 are NG specific.
   private List<EncryptedDataDetail> encryptionDetails;
