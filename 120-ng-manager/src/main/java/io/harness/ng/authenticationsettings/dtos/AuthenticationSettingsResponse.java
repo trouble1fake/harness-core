b0c8cc1015471f1ad5e5edcc2b88a7e5c1bd44d8
Raj Patel <raj.patel@harness.io>
2021-04-07 10:38:42 +0530

    [CDNG-5586]: add private field annotation

--- 120-ng-manager/src/main/java/io/harness/ng/authenticationsettings/dtos/AuthenticationSettingsResponse.java
+++ 120-ng-manager/src/main/java/io/harness/ng/authenticationsettings/dtos/AuthenticationSettingsResponse.java
@@ -9,12 +9,15 @@ import software.wings.security.authentication.AuthenticationMechanism;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 import java.util.Set;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
+import lombok.experimental.FieldDefaults;
 
 @Data
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 @OwnedBy(HarnessTeam.PL)
 public class AuthenticationSettingsResponse {
   List<NGAuthSettings> ngAuthSettings;
