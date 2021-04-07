0415a700f3bbef876be8dac949367fe23ced61ee
Abhinav Singh <abhinav.singh@harness.io>
2021-04-07 16:10:44 +0530

    [CDNG-5852]: Connector git sync adoption pt1

--- 949-git-sync-sdk/src/main/java/io/harness/gitsync/interceptor/GitEntityDeleteInfoDTO.java
+++ 949-git-sync-sdk/src/main/java/io/harness/gitsync/interceptor/GitEntityDeleteInfoDTO.java
@@ -6,14 +6,18 @@ import io.harness.annotations.dev.OwnedBy;
 import io.harness.gitsync.sdk.GitSyncApiConstants;
 
 import javax.ws.rs.QueryParam;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldNameConstants;
 
 @Getter
 @Builder
 @FieldNameConstants(innerTypeName = "GitEntityDeleteInfoKeys")
 @OwnedBy(DX)
+@AllArgsConstructor
+@NoArgsConstructor
 public class GitEntityDeleteInfoDTO {
   @QueryParam(GitSyncApiConstants.BRANCH_KEY) String branch;
   @QueryParam(GitSyncApiConstants.REPO_IDENTIFIER_KEY) String yamlGitConfigId;
--- 949-git-sync-sdk/src/main/java/io/harness/gitsync/interceptor/GitEntityUpdateInfoDTO.java
+++ 949-git-sync-sdk/src/main/java/io/harness/gitsync/interceptor/GitEntityUpdateInfoDTO.java
@@ -6,14 +6,18 @@ import io.harness.annotations.dev.OwnedBy;
 import io.harness.gitsync.sdk.GitSyncApiConstants;
 
 import javax.ws.rs.QueryParam;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldNameConstants;
 
 @Getter
 @Builder
 @FieldNameConstants(innerTypeName = "GitEntityUpdateInfoKeys")
 @OwnedBy(DX)
+@AllArgsConstructor
+@NoArgsConstructor
 public class GitEntityUpdateInfoDTO {
   @QueryParam(GitSyncApiConstants.BRANCH_KEY) String branch;
   @QueryParam(GitSyncApiConstants.REPO_IDENTIFIER_KEY) String yamlGitConfigId;
