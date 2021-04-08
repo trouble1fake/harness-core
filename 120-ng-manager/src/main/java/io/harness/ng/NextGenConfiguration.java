6c4e699ea92ccf617ede940bd8cd668710811460
Zhuo Yin <zhuo.yin@harness.io>
2021-04-07 23:26:14 -0400

    [GTM-109]: License Module Implementation

--- 120-ng-manager/src/main/java/io/harness/ng/NextGenConfiguration.java
+++ 120-ng-manager/src/main/java/io/harness/ng/NextGenConfiguration.java
@@ -50,6 +50,7 @@ public class NextGenConfiguration extends Configuration {
   public static final String SIGNUP_PACKAGE = "io.harness.signup";
   public static final String MOCKSERVER_PACKAGE = "io.harness.ng.core.acl.mockserver";
   public static final String ACCOUNT_PACKAGE = "io.harness.account.resource";
+  public static final String LICENSE_PACKAGE = "io.harness.licensing.api.resource";
 
   @JsonProperty("swagger") private SwaggerBundleConfiguration swaggerBundleConfiguration;
   @Setter @JsonProperty("mongo") private MongoConfig mongoConfig;
@@ -108,7 +109,7 @@ public class NextGenConfiguration extends Configuration {
   public static Collection<Class<?>> getResourceClasses() {
     Reflections reflections = new Reflections(BASE_PACKAGE, CONNECTOR_PACKAGE, GIT_SYNC_PACKAGE, CDNG_RESOURCES_PACKAGE,
         OVERLAY_INPUT_SET_RESOURCE_PACKAGE, YAML_PACKAGE, FILTER_PACKAGE, SIGNUP_PACKAGE, MOCKSERVER_PACKAGE,
-        ACCOUNT_PACKAGE);
+        ACCOUNT_PACKAGE, LICENSE_PACKAGE);
     return reflections.getTypesAnnotatedWith(Path.class);
   }
 
