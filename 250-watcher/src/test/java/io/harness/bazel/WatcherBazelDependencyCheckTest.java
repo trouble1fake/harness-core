c50d11cff89cc6a6d09521f2cb795abea16db299
George Georgiev <george@harness.io>
2021-04-09 21:43:05 -0700

    [BT-313]: unify net.minidev

--- 250-watcher/src/test/java/io/harness/bazel/WatcherBazelDependencyCheckTest.java
+++ 250-watcher/src/test/java/io/harness/bazel/WatcherBazelDependencyCheckTest.java
@@ -65,7 +65,7 @@ public class WatcherBazelDependencyCheckTest extends CategoryTest {
       }
     }
 
-    assertThat(mismatchedVersions.stream().sorted()).hasSize(5);
+    assertThat(mismatchedVersions.stream().sorted()).hasSize(3);
   }
 
   List<String> getDepsInMavenInstallJson() throws IOException {
