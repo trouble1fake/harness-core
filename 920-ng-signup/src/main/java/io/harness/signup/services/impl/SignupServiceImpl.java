e2c11cd9c1124d77a2d4d5371e6fffa6a4e8423c
Nathan Nguyen <lenathan.nguyen@harness.io>
2021-04-13 13:53:38 -0600

    [GTM-110]: added a signup stub

--- 920-ng-signup/src/main/java/io/harness/signup/services/impl/SignupServiceImpl.java
+++ 920-ng-signup/src/main/java/io/harness/signup/services/impl/SignupServiceImpl.java
@@ -4,16 +4,24 @@ import static io.harness.annotations.dev.HarnessTeam.GTM;
 
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.ng.core.user.UserInfo;
+import io.harness.remote.client.RestClientUtils;
 import io.harness.signup.dto.SignupDTO;
 import io.harness.signup.services.SignupService;
+import io.harness.user.remote.UserClient;
 
+import com.google.inject.Inject;
+import java.util.Optional;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PACKAGE, onConstructor = @__({ @Inject }))
 @OwnedBy(GTM)
 public class SignupServiceImpl implements SignupService {
+  private final UserClient userClient;
   @Override
   public UserInfo signup(SignupDTO dto) {
-    return UserInfo.builder().build();
+    Optional<UserInfo> userInfo = RestClientUtils.getResponse(userClient.getUserByEmailId(dto.getEmail()));
+
+    return userInfo.get();
   }
 }
