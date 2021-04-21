aa34075973ba390a2d8c36bf5ac917a7615087c0
Mohit Garg <mohit.garg@harness.io>
2021-04-21 14:22:39 +0530

    [CDNG-5126]: added class description comments for data exception

--- 980-commons/src/main/java/io/harness/exception/DataException.java
+++ 980-commons/src/main/java/io/harness/exception/DataException.java
@@ -3,6 +3,11 @@ package io.harness.exception;
 import static io.harness.eraro.ErrorCode.DATA;
 import static io.harness.eraro.Level.INFO;
 
+/**
+ * This exception serves as parent exception class for all exceptions to be used to store metadata for error
+ * handling framework
+ * It is a dedicated exception to be used for error handling framework
+ */
 public class DataException extends WingsException {
   public DataException(Throwable cause) {
     super(null, cause, DATA, INFO, null, null);
