40e09083186e5d59bf72488fe3ca4e0656dcd5c0
Shivakumar Ningappa <shivakumar.ningappa@harness.io>
2021-04-26 09:14:11 -0700

    Update OrchestrationExecutionEventHandlerRegistrar.java

--- 320-ci-execution/src/main/java/ci/pipeline/execution/OrchestrationExecutionEventHandlerRegistrar.java
+++ 320-ci-execution/src/main/java/ci/pipeline/execution/OrchestrationExecutionEventHandlerRegistrar.java
@@ -32,6 +32,6 @@ public class OrchestrationExecutionEventHandlerRegistrar {
       OrchestrationModuleRegistrarHelper.mergeEventHandlers(
           engineEventHandlersMap, OrchestrationModuleEventHandlerRegistrar.getEngineEventHandlers());
     }
-    return engineEventHandlersMap;
+    return engineEventHandlersMap ;
   }
 }
