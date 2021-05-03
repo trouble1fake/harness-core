2a25af39465ceb64ebf1149a863ebafe351ad255
Praveen Kambam Sugavanam <praveen.sugavanam@harness.io>
2021-05-03 16:33:08 -0700

    update return data type

--- 300-cv-nextgen/src/main/java/io/harness/cvng/core/beans/PrometheusSampleData.java
+++ 300-cv-nextgen/src/main/java/io/harness/cvng/core/beans/PrometheusSampleData.java
@@ -1,5 +1,9 @@
 package io.harness.cvng.core.beans;
 
+import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
+
+import com.google.common.base.Preconditions;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import lombok.Builder;
@@ -11,6 +15,19 @@ import lombok.Value;
 public class PrometheusSampleData {
   Map<String, String> metricDetails;
   List<List<Double>> data;
+
+  public List<DataPoint> getData() {
+    List<DataPoint> dataPoints = new ArrayList<>();
+    if (isNotEmpty(data)) {
+      data.forEach(listItem -> {
+        Preconditions.checkState(listItem.size() == 2);
+        dataPoints.add(
+            DataPoint.builder().timestamp(Double.valueOf(listItem.get(0)).longValue()).value(listItem.get(1)).build());
+      });
+      return dataPoints;
+    }
+    return null;
+  }
 
   @Data
   @Builder
