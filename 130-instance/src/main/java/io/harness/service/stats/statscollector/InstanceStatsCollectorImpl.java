b0add6d0d8d2e78844fb14ac88b62de197cadb5d
Mohit Garg <mohit.garg@harness.io>
2021-05-09 22:02:50 +0530

    [CDNG-7482]: added iterator framework and integrated it with instance stats service

--- 130-instance/src/main/java/io/harness/service/stats/statscollector/InstanceStatsCollectorImpl.java
+++ 130-instance/src/main/java/io/harness/service/stats/statscollector/InstanceStatsCollectorImpl.java
@@ -9,6 +9,7 @@ import io.harness.service.instancestats.InstanceStatsService;
 import io.harness.service.stats.usagemetrics.eventpublisher.UsageMetricsEventPublisher;
 
 import com.google.inject.Inject;
+import com.google.inject.Singleton;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
@@ -18,9 +19,10 @@ import java.util.concurrent.TimeUnit;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@AllArgsConstructor(onConstructor = @__({ @Inject }))
 @Slf4j
+@Singleton
 @OwnedBy(HarnessTeam.DX)
+@AllArgsConstructor(onConstructor = @__({ @Inject }))
 public class InstanceStatsCollectorImpl implements StatsCollector {
   private static final int SYNC_INTERVAL_MINUTES = 10;
   private static final long SYNC_INTERVAL = TimeUnit.MINUTES.toMinutes(SYNC_INTERVAL_MINUTES);
