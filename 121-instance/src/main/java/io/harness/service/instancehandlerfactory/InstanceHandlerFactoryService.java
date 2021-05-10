827cd1ce1d30454043ad3e3eca394a6d6511f199
Mohit Garg <mohit.garg@harness.io>
2021-05-11 03:17:16 +0530

    [CDNG-7275]: removed redundant methods from instance handler factory interface

--- 121-instance/src/main/java/io/harness/service/instancehandlerfactory/InstanceHandlerFactoryService.java
+++ 121-instance/src/main/java/io/harness/service/instancehandlerfactory/InstanceHandlerFactoryService.java
@@ -1,14 +1,16 @@
 package io.harness.service.instancehandlerfactory;
 
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
 import io.harness.dto.infrastructureMapping.InfrastructureMapping;
 import io.harness.service.InstanceHandler;
 
+import com.google.inject.Singleton;
 import java.util.Set;
 
+@OwnedBy(HarnessTeam.DX)
 public interface InstanceHandlerFactoryService {
   InstanceHandler getInstanceHandler(InfrastructureMapping infraMapping);
 
   InstanceHandler getInstanceHandlerByType(String infrastructureMappingType);
-
-  Set<InstanceHandler> getAllInstanceHandlers();
 }
--- 121-instance/src/main/java/io/harness/service/instancehandlerfactory/InstanceHandlerFactoryServiceImpl.java
+++ 121-instance/src/main/java/io/harness/service/instancehandlerfactory/InstanceHandlerFactoryServiceImpl.java
@@ -2,6 +2,8 @@ package io.harness.service.instancehandlerfactory;
 
 import static io.harness.cdng.infra.yaml.InfrastructureKind.KUBERNETES_DIRECT;
 
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
 import io.harness.dto.infrastructureMapping.InfrastructureMapping;
 import io.harness.exception.UnexpectedException;
 import io.harness.service.ContainerInstanceHandler;
@@ -9,9 +11,9 @@ import io.harness.service.InstanceHandler;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import java.util.Set;
 
 @Singleton
+@OwnedBy(HarnessTeam.DX)
 public class InstanceHandlerFactoryServiceImpl implements InstanceHandlerFactoryService {
   @Inject private ContainerInstanceHandler containerInstanceHandler;
 
@@ -29,10 +31,5 @@ public class InstanceHandlerFactoryServiceImpl implements InstanceHandlerFactory
         throw new UnexpectedException(
             "No instance handler defined for infra mapping type: " + infrastructureMappingType);
     }
-  }
-
-  @Override
-  public Set<InstanceHandler> getAllInstanceHandlers() {
-    return null;
   }
 }
