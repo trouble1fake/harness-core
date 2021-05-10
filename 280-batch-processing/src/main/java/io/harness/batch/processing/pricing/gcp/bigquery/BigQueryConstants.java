d90995b5de3bee722374ef6c9651f8e7b9b6ba19
Hitesh Aringa <hitesh.aringa@harness.io>
2021-05-10 14:58:08 +0530

    [CE-4440]: Unblended cost for AWS CUR (#23456)
    * [CE-4440]: build number ++
    
    * [CE-4440]: Unblended cost for AWS CUR (#23443)

--- 280-batch-processing/src/main/java/io/harness/batch/processing/pricing/gcp/bigquery/BigQueryConstants.java
+++ 280-batch-processing/src/main/java/io/harness/batch/processing/pricing/gcp/bigquery/BigQueryConstants.java
@@ -8,7 +8,7 @@ public class BigQueryConstants {
   private BigQueryConstants() {}
 
   public static final String AWS_EC2_BILLING_QUERY =
-      "SELECT SUM(blendedcost) as cost, sum(effectivecost) as effectivecost, resourceid, servicecode, productfamily  "
+      "SELECT SUM(unblendedcost) as cost, sum(effectivecost) as effectivecost, resourceid, servicecode, productfamily  "
       + "FROM `%s` "
       + "WHERE resourceid IN "
       + "( '%s' )  AND "
@@ -19,7 +19,7 @@ public class BigQueryConstants {
       + " FROM  `%s`  "
       + "WHERE accountid = '%s' and starttime = %s ; ";
 
-  public static final String EKS_FARGATE_BILLING_QUERY = "SELECT SUM(blendedcost) as cost, resourceid, usagetype  "
+  public static final String EKS_FARGATE_BILLING_QUERY = "SELECT SUM(unblendedcost) as cost, resourceid, usagetype  "
       + "FROM `%s` "
       + "WHERE  "
       + " (%s) AND "
