1184bc8ee71bed833072afb30e7da7d1e5e39545
George Georgiev <george@harness.io>
2021-05-12 14:50:48 -0700

    [DEL-1854]: fix the version in the delegate logs (#23576)

---
+++ 980-commons/src/main/java/io/harness/logging/VersionConverter.java
@@ -0,0 +1,15 @@
+package io.harness.logging;
+
+import io.harness.version.VersionInfoManager;
+
+import ch.qos.logback.classic.pattern.ClassicConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+public class VersionConverter extends ClassicConverter {
+  static VersionInfoManager versionInfoManager = new VersionInfoManager();
+  @Override
+  public String convert(final ILoggingEvent event) {
+    // for every logging event return processId from mx bean
+    return versionInfoManager.getVersionInfo().getVersion();
+  }
+}
