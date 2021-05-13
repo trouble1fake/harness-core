865b2aa90948a1ed0a6f0e6ac1aede6b6b4ae757
diptiman.adak <diptiman.adak@harness.io>
2021-05-13 20:16:29 +0530

    [BT-367]: test

--- 990-commons-test/src/main/java/io/harness/category/FastUnitTests.java
+++ 990-commons-test/src/main/java/io/harness/category/FastUnitTests.java
@@ -3,4 +3,4 @@ package io.harness.category;
 import io.harness.category.element.UnitTests;
 import io.harness.category.speed.FastTests;
 
-public interface FastUnitTests extends FastTests, UnitTests {}
+aaa public interface FastUnitTests extends FastTests, UnitTests {}
