74898ca83a83fdadad82cc1319d4eeed1d4fbec9
Meenakshi Raikwar <meenakshi.raikwar@harness.io>
2021-05-18 15:39:50 +0530

    [CDNG-7164]: migration demo- git synvc

---
+++ 120-ng-manager/src/main/java/io/harness/ng/migration/GitSyncMigrationProvider.java
@@ -0,0 +1,36 @@
+package io.harness.ng.migration;
+
+import static io.harness.annotations.dev.HarnessTeam.DX;
+
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.migration.MigrationDetails;
+import io.harness.migration.MigrationProvider;
+import io.harness.migration.entities.NGSchema;
+
+import com.google.inject.Singleton;
+import java.util.ArrayList;
+import java.util.List;
+
+@OwnedBy(DX)
+@Singleton
+public class GitSyncMigrationProvider implements MigrationProvider {
+  @Override
+  public String getServiceName() {
+    return "gitsync";
+  }
+
+  @Override
+  public Class<? extends NGSchema> getSchemaClass() {
+    return NGGitSyncSchema.class;
+  }
+
+  @Override
+  public List<Class<? extends MigrationDetails>> getMigrationDetailsList() {
+    return new ArrayList<Class<? extends MigrationDetails>>() {
+      {
+        add(NGCoreMigrationDetails.class);
+        add(NGCoreBGMigrationDetails.class);
+      }
+    };
+  }
+}
---
+++ 120-ng-manager/src/main/java/io/harness/ng/migration/NGGitSyncSchema.java
@@ -0,0 +1,24 @@
+package io.harness.ng.migration;
+
+import static io.harness.annotations.dev.HarnessTeam.DX;
+
+import io.harness.annotation.StoreIn;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.migration.beans.MigrationType;
+import io.harness.migration.entities.NGSchema;
+import io.harness.ng.DbAliases;
+
+import java.util.Map;
+import org.springframework.data.annotation.Persistent;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@StoreIn(DbAliases.NG_MANAGER)
+@Document("schema_gitsync")
+@Persistent
+@OwnedBy(DX)
+public class NGGitSyncSchema extends NGSchema {
+  public NGGitSyncSchema(
+      String id, Long createdAt, Long lastUpdatedAt, String name, Map<MigrationType, Integer> migrationDetails) {
+    super(id, createdAt, lastUpdatedAt, name, migrationDetails);
+  }
+}
