b25f09ae4179918468494d084074be8b4af4da53
Deepak Patankar <deepak.patankar@harness.io>
2021-05-27 01:35:03 +0530

    [CDNG-0]: dummy commiit

--- 120-ng-manager/src/main/java/io/harness/ng/NextGenApplication.java
+++ 120-ng-manager/src/main/java/io/harness/ng/NextGenApplication.java
@@ -255,7 +255,7 @@ public class NextGenApplication extends Application<NextGenConfiguration> {
     registerJobs(injector);
     registerMigrations(injector);
     registerQueueListeners(injector);
-    log.info(("checking new changes picked up"));
+    log.info(("checking new changes picked up 1"));
 
     intializeGitSync(injector, appConfig);
     //  This is ordered below health registration so that kubernetes deployment readiness check passes under 10 minutes
