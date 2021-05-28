82992fe4b029e42738e84bcee879402a3dd85df9
Sahil Hindwani <sahil.hindwani@harness.io>
2021-05-28 16:21:26 +0530

    [CDNG-0]: Branch for load-test

--- 125-cd-nextgen/src/main/java/io/harness/cdng/k8s/K8sCanaryBaseStepInfo.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/k8s/K8sCanaryBaseStepInfo.java
@@ -5,13 +5,18 @@ import static io.harness.yaml.schema.beans.SupportedPossibleFieldTypes.bool;
 import static io.harness.yaml.schema.beans.SupportedPossibleFieldTypes.string;
 
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.beans.common.SwaggerConstants;
+import io.harness.plancreator.steps.TaskSelectorYaml;
 import io.harness.pms.yaml.ParameterField;
 import io.harness.yaml.YamlSchemaTypes;
 
+import io.swagger.annotations.ApiModelProperty;
+import java.util.List;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.FieldNameConstants;
 import org.springframework.data.annotation.TypeAlias;
 
 @OwnedBy(CDP)
@@ -19,7 +24,10 @@ import org.springframework.data.annotation.TypeAlias;
 @NoArgsConstructor
 @AllArgsConstructor
 @TypeAlias("K8sCanaryBaseStepInfo")
+@FieldNameConstants(innerTypeName = "K8sCanaryBaseStepInfoKeys")
 public class K8sCanaryBaseStepInfo {
   @NotNull InstanceSelectionWrapper instanceSelection;
   @YamlSchemaTypes({string, bool}) ParameterField<Boolean> skipDryRun;
+  @ApiModelProperty(dataType = SwaggerConstants.STRING_LIST_CLASSPATH)
+  ParameterField<List<TaskSelectorYaml>> delegateSelectors;
 }
