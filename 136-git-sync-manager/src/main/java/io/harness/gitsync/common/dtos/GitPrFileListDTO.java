9a1bd778e26310a5351288d93478d3b86f8e4448
Mohit Garg <mohit.garg@harness.io>
2021-06-01 19:04:40 +0530

    [CDNG-8821]: added new dto to return compare commits response

---
+++ 136-git-sync-manager/src/main/java/io/harness/gitsync/common/dtos/GitPrFileListDTO.java
@@ -0,0 +1,20 @@
+package io.harness.gitsync.common.dtos;
+
+import static io.harness.annotations.dev.HarnessTeam.DX;
+
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.product.ci.scm.proto.PRFile;
+
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@OwnedBy(DX)
+public class GitPrFileListDTO {
+  List<PRFile> prFileList;
+}
