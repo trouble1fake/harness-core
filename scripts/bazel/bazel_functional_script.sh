47c3974c7f7bf7c69733909df8f1fbf1456957d1
Abhinav Singh <abhinav.singh@harness.io>
2021-06-01 23:25:50 +0530

    Update bazel_functional_script.sh

--- scripts/bazel/bazel_functional_script.sh
+++ scripts/bazel/bazel_functional_script.sh
@@ -28,7 +28,7 @@ if [ "${RUN_BAZEL_FUNCTIONAL_TESTS}" == "true" ]; then
   bazel run ${BAZEL_ARGUMENTS} 230-model-test:app &
   MANAGER_PID=$!
   bazel test //200-functional-test:io.harness.functional.DummyFirstFunctionalTest || true
-  java -Xmx4096m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:mygclogfilename.gc -XX:+UseParallelGC -XX:MaxGCPauseMillis=500 -jar /home/jenkins/workspace/pr-portal-funtional-tests/260-delegate/target/delegate-capsule.jar /home/jenkins/workspace/pr-portal-funtional-tests/260-delegate/config-delegate.yml &  
+  java -Xbootclasspath/p:alpn-boot-8.1.13.v20181017.jar -Xmx4096m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:mygclogfilename.gc -XX:+UseParallelGC -XX:MaxGCPauseMillis=500 -jar /home/jenkins/workspace/pr-portal-funtional-tests/260-delegate/target/delegate-capsule.jar /home/jenkins/workspace/pr-portal-funtional-tests/260-delegate/config-delegate.yml &  
   DELEGATE_PID=$!
   bazel test --keep_going ${BAZEL_ARGUMENTS} --jobs=3 -- //200-functional-test/... || true
   kill -9 $MANAGER_PID || true
