2a4cc975d6f6c1dbf7c85c31f3a742ce8d280eb5
Ujjawal Prasad <ujjawal.prasad@harness.io>
2021-06-02 13:08:15 +0530

    [PL-14252]: Added changes for ldap job

--- 400-rest/src/main/java/software/wings/scheduler/LdapGroupSyncJobHandler.java
+++ 400-rest/src/main/java/software/wings/scheduler/LdapGroupSyncJobHandler.java
@@ -10,7 +10,6 @@ import static io.harness.mongo.iterator.MongoPersistenceIterator.SchedulingType.
 import static software.wings.beans.Application.GLOBAL_APP_ID;
 import static software.wings.beans.UserInvite.UserInviteBuilder.anUserInvite;
 
-import static java.time.Duration.ofHours;
 import static java.time.Duration.ofMinutes;
 
 import io.harness.annotations.dev.HarnessModule;
@@ -41,6 +40,7 @@ import software.wings.beans.sso.LdapTestResponse.Status;
 import software.wings.beans.sso.LdapUserResponse;
 import software.wings.beans.sso.SSOSettings;
 import software.wings.beans.sso.SSOSettings.SSOSettingsKeys;
+import software.wings.beans.sso.SSOType;
 import software.wings.delegatetasks.DelegateProxyFactory;
 import software.wings.dl.WingsPersistence;
 import software.wings.features.LdapFeature;
@@ -76,6 +76,7 @@ import org.mongodb.morphia.FindAndModifyOptions;
 import org.mongodb.morphia.query.Query;
 import org.mongodb.morphia.query.UpdateOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 
 @OwnedBy(PL)
 @TargetModule(HarnessModule._360_CG_MANAGER)
@@ -113,9 +114,10 @@ public class LdapGroupSyncJobHandler implements MongoPersistenceIterator.Handler
         MongoPersistenceIterator.<SSOSettings, SpringFilterExpander>builder()
             .clazz(SSOSettings.class)
             .fieldName(SSOSettingsKeys.nextIteration)
-            .targetInterval(ofHours(12))
-            .acceptableNoAlertDelay(ofHours(14))
+            .targetInterval(ofMinutes(60))
+            .acceptableNoAlertDelay(ofMinutes(80))
             .handler(this)
+            .filterExpander(query -> query.addCriteria(Criteria.where(SSOSettingsKeys.type).is(SSOType.LDAP)))
             .schedulingType(REGULAR)
             .persistenceProvider(new SpringPersistenceProvider<>(mongoTemplate))
             .redistribute(true));
@@ -123,7 +125,7 @@ public class LdapGroupSyncJobHandler implements MongoPersistenceIterator.Handler
 
   @Override
   public void handle(SSOSettings ssoSettings) {
-    if (featureFlagService.isGlobalEnabled(FeatureName.LDAP_GROUP_SYNC_JOB_ITERATOR)
+    if (featureFlagService.isEnabled(FeatureName.LDAP_GROUP_SYNC_JOB_ITERATOR, ssoSettings.getAccountId())
         && ssoSettings instanceof LdapSettings) {
       LdapSettings ldapSettings = (LdapSettings) ssoSettings;
       String accountId = ldapSettings.getAccountId();
@@ -183,6 +185,8 @@ public class LdapGroupSyncJobHandler implements MongoPersistenceIterator.Handler
     if (!removedGroupMembers.containsKey(userGroup)) {
       removedGroupMembers.put(userGroup, Sets.newHashSet());
     }
+    log.info("LDAPIterator: Removing users {} as part of sync with usergroup {} in accountId {}", removedUsers,
+        userGroup.getUuid(), userGroup.getAccountId());
     removedGroupMembers.getOrDefault(userGroup, Sets.newHashSet()).addAll(removedUsers);
   }
 
