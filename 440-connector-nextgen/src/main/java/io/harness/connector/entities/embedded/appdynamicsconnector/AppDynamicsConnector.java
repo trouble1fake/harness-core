c314f211a767d005e211868ae7d9fe5a9461a217
Praveen Kambam Sugavanam <praveen.sugavanam@harness.io>
2021-06-02 17:36:15 -0700

    add ignoreUnknown annotation

--- 440-connector-nextgen/src/main/java/io/harness/connector/entities/embedded/appdynamicsconnector/AppDynamicsConnector.java
+++ 440-connector-nextgen/src/main/java/io/harness/connector/entities/embedded/appdynamicsconnector/AppDynamicsConnector.java
@@ -5,6 +5,7 @@ import io.harness.annotations.dev.OwnedBy;
 import io.harness.connector.entities.Connector;
 import io.harness.delegate.beans.connector.appdynamicsconnector.AppDynamicsAuthType;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
@@ -15,6 +16,7 @@ import org.springframework.data.annotation.TypeAlias;
 
 @Value
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 @EqualsAndHashCode(callSuper = true)
 @FieldNameConstants(innerTypeName = "AppDynamicsConnectorKeys")
 @Entity(value = "connectors", noClassnameStored = true)
