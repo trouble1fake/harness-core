5f42f30679cf9b92873293ba1085817f28d42de2
Raghvendra Singh <raghvendra.singh@harness.io>
2021-06-07 15:54:21 +0530

    [AWS-SAM-HACK]: Init commit delegate tasks

--- 930-delegate-tasks/src/main/java/io/harness/delegate/task/aws/AwsSamDeployTaskHandlerNG.java
+++ 930-delegate-tasks/src/main/java/io/harness/delegate/task/aws/AwsSamDeployTaskHandlerNG.java
@@ -5,42 +5,31 @@ import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
 import static io.harness.logging.LogLevel.ERROR;
 import static io.harness.logging.LogLevel.INFO;
 import static io.harness.provision.AWSConstants.AWS_SAM_WORKING_DIRECTORY;
-import static io.harness.provision.TerraformConstants.RESOURCE_READY_WAIT_TIME_SECONDS;
-import static io.harness.provision.TerraformConstants.TERRAFORM_BACKEND_CONFIGS_FILE_NAME;
-import static io.harness.provision.TerraformConstants.TERRAFORM_VARIABLES_FILE_NAME;
-import static io.harness.threading.Morpheus.sleep;
 
 import static java.lang.String.format;
-import static java.time.Duration.ofSeconds;
 
 import io.harness.annotations.dev.OwnedBy;
-import io.harness.cli.CliResponse;
+import io.harness.aws.AwsSamClient;
 import io.harness.delegate.beans.connector.scm.genericgitconnector.GitConfigDTO;
 import io.harness.delegate.beans.storeconfig.GitStoreDelegateConfig;
-import io.harness.delegate.task.terraform.TerraformTaskNGResponse;
 import io.harness.exception.AwsSamCommandExecutionException;
 import io.harness.exception.ExceptionUtils;
-import io.harness.exception.TerraformCommandExecutionException;
 import io.harness.git.model.GitBaseRequest;
 import io.harness.logging.CommandExecutionStatus;
 import io.harness.logging.LogCallback;
 import io.harness.logging.PlanJsonLogOutputStream;
-import io.harness.terraform.TerraformHelperUtils;
-import io.harness.terraform.request.TerraformExecuteStepRequest;
 
 import com.google.inject.Inject;
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.Charsets;
 
 @Slf4j
 @OwnedBy(CDP)
 public class AwsSamDeployTaskHandlerNG extends AwsSamAbstractTaskHandler {
   @Inject AwsBaseHelper awsBaseHelper;
+  @Inject AwsSamClient awsSamClient;
 
   @Override
   public AwsSamTaskNGResponse executeTaskInternal(AwsSamTaskParameters taskParameters, String delegateId, String taskId,
@@ -69,7 +58,15 @@ public class AwsSamDeployTaskHandlerNG extends AwsSamAbstractTaskHandler {
         taskParameters.getAccountId(), "", "", confileFileGitStore, logCallback, awsSamProjectDirectoryPath, baseDir);
 
     try (PlanJsonLogOutputStream planJsonLogOutputStream = new PlanJsonLogOutputStream()) {
+      String samBuildCommand = format("sam build");
+      Map<String, String> envVariables = new HashMap<>();
+      awsSamClient.runCommand(samBuildCommand, 120000l, envVariables, awsSamAppDirectory, logCallback);
 
+      return AwsSamTaskNGResponse.builder()
+          .outputs("")
+          .commitIdForConfigFilesMap(new HashMap<>())
+          .commandExecutionStatus(CommandExecutionStatus.SUCCESS)
+          .build();
     } catch (AwsSamCommandExecutionException awsSamCommandExecutionException) {
       log.warn("Failed to execute Aws Sam Deploy Step", awsSamCommandExecutionException);
       logCallback.saveExecutionLog("Failed", ERROR, CommandExecutionStatus.FAILURE);
@@ -81,72 +78,6 @@ public class AwsSamDeployTaskHandlerNG extends AwsSamAbstractTaskHandler {
       log.warn("Exception Occurred", exception);
       logCallback.saveExecutionLog("Failed", ERROR, CommandExecutionStatus.FAILURE);
       return AwsSamTaskNGResponse.builder()
-          .commandExecutionStatus(CommandExecutionStatus.FAILURE)
-          .errorMessage(ExceptionUtils.getMessage(exception))
-          .build();
-    }
-
-    File tfOutputsFile = Paths.get(awsSamAppDirectory, format(TERRAFORM_VARIABLES_FILE_NAME, "output")).toFile();
-
-    try (PlanJsonLogOutputStream planJsonLogOutputStream = new PlanJsonLogOutputStream()) {
-      TerraformExecuteStepRequest terraformExecuteStepRequest =
-          TerraformExecuteStepRequest.builder()
-              .tfBackendConfigsFile(taskParameters.getBackendConfig() != null
-                      ? TerraformHelperUtils.createFileFromStringContent(
-                          taskParameters.getBackendConfig(), awsSamAppDirectory, TERRAFORM_BACKEND_CONFIGS_FILE_NAME)
-                      : taskParameters.getBackendConfig())
-              .tfOutputsFile(tfOutputsFile.getAbsolutePath())
-              .tfVarFilePaths(varFilePaths)
-              .workspace(taskParameters.getWorkspace())
-              .targets(taskParameters.getTargets())
-              .scriptDirectory(awsSamAppDirectory)
-              .encryptedTfPlan(taskParameters.getEncryptedTfPlan())
-              .encryptionConfig(taskParameters.getEncryptionConfig())
-              .envVars(taskParameters.getEnvironmentVariables())
-              .isSaveTerraformJson(taskParameters.isSaveTerraformStateJson())
-              .logCallback(logCallback)
-              .planJsonLogOutputStream(planJsonLogOutputStream)
-              .timeoutInMillis(taskParameters.getTimeoutInMillis())
-              .build();
-
-      CliResponse response = terraformBaseHelper.executeTerraformApplyStep(terraformExecuteStepRequest);
-
-      logCallback.saveExecutionLog(
-          format("Waiting: [%s] seconds for resources to be ready", RESOURCE_READY_WAIT_TIME_SECONDS), INFO,
-          CommandExecutionStatus.RUNNING);
-      sleep(ofSeconds(RESOURCE_READY_WAIT_TIME_SECONDS));
-
-      logCallback.saveExecutionLog("Script execution finished with status: " + response.getCommandExecutionStatus(),
-          INFO, CommandExecutionStatus.RUNNING);
-
-      Map<String, String> commitIdToFetchedFilesMap = terraformBaseHelper.buildcommitIdToFetchedFilesMap(
-          taskParameters.getAccountId(), taskParameters.getConfigFile().getIdentifier(), gitBaseRequestForConfigFile,
-          taskParameters.getVarFileInfos());
-
-      File tfStateFile = TerraformHelperUtils.getTerraformStateFile(awsSamAppDirectory, taskParameters.getWorkspace());
-
-      String stateFileId = terraformBaseHelper.uploadTfStateFile(
-          taskParameters.getAccountId(), delegateId, taskId, taskParameters.getEntityId(), tfStateFile);
-
-      return TerraformTaskNGResponse.builder()
-          .outputs(new String(Files.readAllBytes(tfOutputsFile.toPath()), Charsets.UTF_8))
-          .commitIdForConfigFilesMap(commitIdToFetchedFilesMap)
-          .encryptedTfPlan(taskParameters.getEncryptedTfPlan())
-          .commandExecutionStatus(CommandExecutionStatus.SUCCESS)
-          .stateFileId(stateFileId)
-          .build();
-
-    } catch (TerraformCommandExecutionException terraformCommandExecutionException) {
-      log.warn("Failed to execute TerraformApplyStep", terraformCommandExecutionException);
-      logCallback.saveExecutionLog("Failed", ERROR, CommandExecutionStatus.FAILURE);
-      return TerraformTaskNGResponse.builder()
-          .commandExecutionStatus(CommandExecutionStatus.FAILURE)
-          .errorMessage(ExceptionUtils.getMessage(terraformCommandExecutionException))
-          .build();
-    } catch (Exception exception) {
-      log.warn("Exception Occurred", exception);
-      logCallback.saveExecutionLog("Failed", ERROR, CommandExecutionStatus.FAILURE);
-      return TerraformTaskNGResponse.builder()
           .commandExecutionStatus(CommandExecutionStatus.FAILURE)
           .errorMessage(ExceptionUtils.getMessage(exception))
           .build();
--- 950-delegate-tasks-beans/src/main/java/io/harness/delegate/task/aws/AwsSamTaskNGResponse.java
+++ 950-delegate-tasks-beans/src/main/java/io/harness/delegate/task/aws/AwsSamTaskNGResponse.java
@@ -1,34 +1,28 @@
 package io.harness.delegate.task.aws;
 
+import static io.harness.annotations.dev.HarnessTeam.CDP;
+
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.delegate.beans.DelegateMetaInfo;
 import io.harness.delegate.beans.DelegateTaskNotifyResponseData;
 import io.harness.delegate.beans.logstreaming.UnitProgressData;
 import io.harness.logging.CommandExecutionStatus;
-import io.harness.security.encryption.EncryptedRecordData;
+
+import java.util.Map;
 import lombok.Builder;
 import lombok.Setter;
 import lombok.Value;
 import lombok.experimental.NonFinal;
 
-import java.util.Map;
-
-import static io.harness.annotations.dev.HarnessTeam.CDP;
-
 @Value
 @Builder
 @OwnedBy(CDP)
 public class AwsSamTaskNGResponse implements DelegateTaskNotifyResponseData {
-    CommandExecutionStatus commandExecutionStatus;
-    String errorMessage;
-    @NonFinal
-    @Setter
-    UnitProgressData unitProgressData;
+  CommandExecutionStatus commandExecutionStatus;
+  String errorMessage;
+  @NonFinal @Setter UnitProgressData unitProgressData;
 
-    Map<String, String> commitIdForConfigFilesMap;
-    EncryptedRecordData encryptedTfPlan;
-    String outputs;
-    String stateFileId;
-    @NonFinal @Setter
-    DelegateMetaInfo delegateMetaInfo;
+  Map<String, String> commitIdForConfigFilesMap;
+  String outputs;
+  @NonFinal @Setter DelegateMetaInfo delegateMetaInfo;
 }
