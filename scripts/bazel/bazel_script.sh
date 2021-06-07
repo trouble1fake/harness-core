bfabe6ab86e22c0a263668d103ea20f61094a8fa
Abhinav Singh <abhinav.singh@harness.io>
2021-06-07 23:51:59 +0530

    Update bazel_script.sh

--- scripts/bazel/bazel_script.sh
+++ scripts/bazel/bazel_script.sh
@@ -30,8 +30,7 @@ cat ${BAZEL_DIRS}/out/stable-status.txt
 cat ${BAZEL_DIRS}/out/volatile-status.txt
 
 if [ "${RUN_BAZEL_TESTS}" == "true" ]; then
-  bazel ${bazelrc} build ${BAZEL_ARGUMENTS} -- //... -//product/... -//commons/... \
-  && bazel ${bazelrc} test --cache_test_results=${CACHE_TEST_RESULTS} --define=HARNESS_ARGS=${HARNESS_ARGS} --keep_going ${BAZEL_ARGUMENTS} -- \
+  bazel ${bazelrc} test --cache_test_results=${CACHE_TEST_RESULTS} --define=HARNESS_ARGS=${HARNESS_ARGS} --keep_going ${BAZEL_ARGUMENTS} -- \
   //... -//product/... -//commons/... -//200-functional-test/... -//190-deployment-functional-tests/...
   exit $?
 fi
