01eaa0f5ff466d3db2d423b52dc0eb0df2c566c1
Munish Jalota <munish.jalota@harness.io>
2021-06-07 23:43:21 +0100

    [CE-4382]: Backend - Add Dashboard Resource in Access Control Resource Groups

---
+++ 830-resource-group/src/main/java/io/harness/resourcegroup/resourceclient/dashboard/DashboardResourceImpl.java
@@ -0,0 +1,58 @@
+package io.harness.resourcegroup.resourceclient.dashboard;
+
+import com.google.inject.Inject;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.beans.Scope;
+import io.harness.beans.ScopeLevel;
+import io.harness.eventsframework.consumer.Message;
+import io.harness.resourcegroup.beans.ValidatorType;
+import io.harness.resourcegroup.framework.service.Resource;
+import io.harness.resourcegroup.framework.service.ResourceInfo;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.*;
+
+import static io.harness.annotations.dev.HarnessTeam.PL;
+import static io.harness.resourcegroup.beans.ValidatorType.DYNAMIC;
+
+@OwnedBy(PL)
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@AllArgsConstructor(access = AccessLevel.PUBLIC, onConstructor = @__({ @Inject }))
+@Slf4j
+public class DashboardResourceImpl implements Resource {
+
+  public static final String DASHBOARD = "DASHBOARDS" ;
+
+  @Override
+  public List<Boolean> validate(List<String> resourceIds, Scope scope) {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public EnumSet<ValidatorType> getSelectorKind() {
+    return EnumSet.of(DYNAMIC);
+  }
+
+  @Override
+  public String getType() {
+    return DASHBOARD;
+  }
+
+  @Override
+  public Set<ScopeLevel> getValidScopeLevels() {
+    return EnumSet.of(ScopeLevel.ACCOUNT, ScopeLevel.ORGANIZATION);
+  }
+
+  @Override
+  public Optional<String> getEventFrameworkEntityType() {
+    return Optional.empty();
+  }
+
+  @Override
+  public ResourceInfo getResourceInfoFromEvent(Message message) {
+    return null;
+  }
+}
