7aca0d686faa3ec138f80156123f3dde9bc86162
Vaibhav Singhal <vaibhav.si@harness.io>
2021-06-07 12:42:48 +0530

    added manifest POJO for AwsSamManifest

---
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/manifest/yaml/kinds/AwsSamManifest.java
@@ -0,0 +1,26 @@
+package io.harness.cdng.manifest.yaml.kinds;
+
+import io.harness.cdng.manifest.ManifestType;
+import io.harness.cdng.manifest.yaml.ManifestAttributes;
+import io.harness.cdng.manifest.yaml.StoreConfigWrapper;
+import io.harness.data.validator.EntityIdentifier;
+import lombok.Builder;
+import lombok.Data;
+
+
+@Data
+@Builder
+public class AwsSamManifest implements ManifestAttributes {
+  @EntityIdentifier String identifier;
+  StoreConfigWrapper store;
+
+  @Override
+  public String getKind() {
+    return ManifestType.AwsSamManifest;
+  }
+
+  @Override
+  public ManifestAttributes applyOverrides(ManifestAttributes overrideConfig) {
+    return null;
+  }
+}
