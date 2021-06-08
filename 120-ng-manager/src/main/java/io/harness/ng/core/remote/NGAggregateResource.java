54533f5280035a3fb60b0e9777fdc53e4b7a44f8
Sainath Batthala <sainath.batthala@harness.io>
2021-06-09 00:26:05 +0530

    [CDP-17539]: Added slack notification api

--- 120-ng-manager/src/main/java/io/harness/ng/core/remote/NGAggregateResource.java
+++ 120-ng-manager/src/main/java/io/harness/ng/core/remote/NGAggregateResource.java
@@ -150,7 +150,9 @@ public class NGAggregateResource {
       //Get the session object
       Properties props = new Properties();
       props.put("mail.smtp.host",host);
+      props.put("mail.smtp.port",587);
       props.put("mail.smtp.auth", "true");
+      props.put("mail.smtp.starttls.enable", "true");
 
       Session session = Session.getDefaultInstance(props,
               new javax.mail.Authenticator() {
