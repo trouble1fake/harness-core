bc95ecc731877a69a0e3eabf53542f14d9e88c78
Aleksandar Radisavljevic <aleksandar.radisavljevic@harness.io>
2021-06-08 19:57:22 +0200

    [CI-0]: added comment

--- 310-ci-manager/src/main/java/io/harness/app/intfc/CIStepMetricsService.java
+++ 310-ci-manager/src/main/java/io/harness/app/intfc/CIStepMetricsService.java
@@ -5,6 +5,14 @@ import io.harness.app.beans.dto.StepResourceMetricDTO;
 import java.util.List;
 
 public interface CIStepMetricsService {
+  /**
+   * Gets list of step metrics for each step in given pipeline
+   * @param accountIdentifier account identifier
+   * @param orgIdentifier organisation identifier
+   * @param projectIdentifier project identifier
+   * @param pipelineIdentifier pipeline identifier
+   * @return list of step metrics
+   */
   List<StepResourceMetricDTO> getCIStepMetrics(
       String accountIdentifier, String orgIdentifier, String projectIdentifier, String pipelineIdentifier);
 }
