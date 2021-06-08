e60d86538c42c23a089b1a3beb6c04c5a5db86bb
Praveen Kambam Sugavanam <praveen.sugavanam@harness.io>
2021-06-07 23:31:50 -0700

    [CV-0]: Delegate destroy

--- 400-rest/src/main/java/software/wings/sm/states/k8s/K8DelegateDestroyState.java
+++ 400-rest/src/main/java/software/wings/sm/states/k8s/K8DelegateDestroyState.java
@@ -3,6 +3,8 @@ package software.wings.sm.states.k8s;
 import io.harness.beans.ExecutionStatus;
 import io.harness.beans.SweepingOutputInstance;
 import io.harness.exception.WingsException;
+import io.harness.k8s.KubernetesContainerService;
+import io.harness.k8s.KubernetesHelperService;
 import io.harness.k8s.kubectl.Kubectl;
 import io.harness.k8s.model.KubernetesConfig;
 import io.harness.security.encryption.EncryptedDataDetail;
@@ -10,6 +12,11 @@ import io.harness.serializer.KryoSerializer;
 
 import software.wings.beans.KubernetesClusterConfig;
 import software.wings.beans.SettingAttribute;
+import software.wings.service.intfc.DelegateService;
+import software.wings.service.intfc.SettingsService;
+import software.wings.service.intfc.security.EncryptionService;
+import software.wings.service.intfc.security.SecretManager;
+import software.wings.service.intfc.verification.CVActivityLogService;
 import software.wings.sm.ExecutionContext;
 import software.wings.sm.ExecutionResponse;
 import software.wings.sm.State;
@@ -33,11 +40,21 @@ import org.mongodb.morphia.annotations.Transient;
 @Attributes
 @Slf4j
 public class K8DelegateDestroyState extends State implements SweepingOutputStateMixin {
+  private static final String kubectlBaseDir = "./client-tools/kubectl/";
+  private static final String defaultKubectlVersion = "v1.13.2";
+  private static String kubectlPath;
   @Getter @Setter private String kubernetesConnectorId;
   @Getter @Setter private String delegateName;
   @Getter @Setter private SweepingOutputInstance.Scope sweepingOutputScope;
   @Getter @Setter private String sweepingOutputName;
   @Transient @Inject @Getter KryoSerializer kryoSerializer;
+  @Inject private KubernetesHelperService kubernetesHelperService;
+  @Inject private SettingsService settingsService;
+  @Inject private EncryptionService encryptionService;
+  @Inject protected SecretManager secretManager;
+  @Inject private KubernetesContainerService kubernetesContainerService;
+  @Inject private DelegateService delegateService;
+  @Inject private CVActivityLogService cvActivityLogService;
 
   public K8DelegateDestroyState(String name) {
     super(name, StateType.DELEGATE_DESTROY.name());
