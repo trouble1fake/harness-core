d80d3324d64f3c9bc11c7ef2ca4874f551c6adb1
Rohit Karelia <rohit.karelia@harness.io>
2021-06-10 15:53:28 -0700

    [PL-14181]: Update versions in heartbeat

--- 260-delegate/src/main/java/io/harness/delegate/service/DelegateAgentServiceImpl.java
+++ 260-delegate/src/main/java/io/harness/delegate/service/DelegateAgentServiceImpl.java
@@ -404,7 +404,7 @@ public class DelegateAgentServiceImpl implements DelegateAgentService {
 
       connectionHeartbeat = DelegateConnectionHeartbeat.builder()
                                 .delegateConnectionId(delegateConnectionId)
-                                .version(getVersion())
+                                .version(getVersionWithPatch())
                                 .location(Paths.get("").toAbsolutePath().toString())
                                 .build();
 
@@ -481,7 +481,7 @@ public class DelegateAgentServiceImpl implements DelegateAgentService {
                                           .delegateGroupId(delegateGroupId)
                                           .delegateProfileId(delegateProfile)
                                           .description(description)
-                                          .version(getVersion())
+                                          .version(getVersionWithPatch())
                                           .delegateType(DELEGATE_TYPE)
                                           //.proxy(set to true if there is a system proxy)
                                           .pollingModeEnabled(delegateConfiguration.isPollForTasks())
@@ -543,7 +543,7 @@ public class DelegateAgentServiceImpl implements DelegateAgentService {
       startKeepAliveRequestWhenPollingEnabled(builder);
 
       if (!multiVersion) {
-        startUpgradeCheck(getVersion());
+        startUpgradeCheck(getVersionWithPatch());
       }
 
       log.info("Delegate started");
@@ -675,7 +675,7 @@ public class DelegateAgentServiceImpl implements DelegateAgentService {
 
       // Stream the request body
       RequestBuilder requestBuilder =
-          client.newRequestBuilder().method(METHOD.GET).uri(uri.toString()).header("Version", getVersion());
+          client.newRequestBuilder().method(METHOD.GET).uri(uri.toString()).header("Version", getVersionWithPatch());
 
       requestBuilder
           .encoder(new Encoder<Delegate, Reader>() { // Do not change this, wasync doesn't like lambdas
@@ -1483,8 +1483,8 @@ public class DelegateAgentServiceImpl implements DelegateAgentService {
           WATCHER_VERSION_MATCH_TIMEOUT / 1000L, watcherVersion, expectedVersion);
     }
 
-    boolean multiVersionRestartNeeded =
-        multiVersion && clock.millis() - startTime > WATCHER_VERSION_MATCH_TIMEOUT && !new File(getVersion()).exists();
+    boolean multiVersionRestartNeeded = multiVersion && clock.millis() - startTime > WATCHER_VERSION_MATCH_TIMEOUT
+        && !new File(getVersionWithPatch()).exists();
 
     if (heartbeatTimedOut || versionMatchTimedOut
         || (multiVersionRestartNeeded && multiVersionWatcherStarted.compareAndSet(false, true))) {
@@ -2311,7 +2311,8 @@ public class DelegateAgentServiceImpl implements DelegateAgentService {
 
   private void removeDelegateVersionFromCapsule() {
     try {
-      cleanup(new File(System.getProperty("capsule.dir")).getParentFile(), getVersion(), upgradeVersion, "delegate-");
+      cleanup(new File(System.getProperty("capsule.dir")).getParentFile(), getVersionWithPatch(), upgradeVersion,
+          "delegate-");
     } catch (Exception ex) {
       log.error("Failed to clean delegate version [{}] from Capsule", upgradeVersion, ex);
     }
