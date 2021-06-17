5828680929dcc1c23fd8249645239170d370c03f
Sahil Hindwani <sahil.hindwani@harness.io>
2021-06-18 01:27:47 +0530

    [CDNG-0]: Add principal in context for events too

--- 884-pms-commons/src/main/java/io/harness/pms/events/base/PmsBaseEventHandler.java
+++ 884-pms-commons/src/main/java/io/harness/pms/events/base/PmsBaseEventHandler.java
@@ -1,15 +1,29 @@
 package io.harness.pms.events.base;
 
+import static io.harness.AuthorizationServiceHeader.PIPELINE_SERVICE;
+
+import io.harness.accesscontrol.principals.PrincipalType;
 import io.harness.data.structure.CollectionUtils;
+import io.harness.data.structure.EmptyPredicate;
+import io.harness.eraro.ErrorCode;
+import io.harness.exception.AccessDeniedException;
+import io.harness.exception.WingsException;
 import io.harness.logging.AutoLogContext;
 import io.harness.logging.AutoLogContext.OverrideBehavior;
 import io.harness.metrics.ThreadAutoLogContext;
 import io.harness.monitoring.EventMonitoringService;
 import io.harness.monitoring.MonitoringInfo;
 import io.harness.pms.contracts.ambiance.Ambiance;
+import io.harness.pms.contracts.plan.ExecutionPrincipalInfo;
+import io.harness.pms.contracts.plan.ExecutionTriggerInfo;
 import io.harness.pms.execution.utils.AmbianceUtils;
 import io.harness.pms.gitsync.PmsGitSyncBranchContextGuard;
 import io.harness.pms.gitsync.PmsGitSyncHelper;
+import io.harness.pms.rbac.PrincipalTypeProtoToPrincipalTypeMapper;
+import io.harness.security.SecurityContextBuilder;
+import io.harness.security.dto.ApiKeyPrincipal;
+import io.harness.security.dto.ServicePrincipal;
+import io.harness.security.dto.UserPrincipal;
 
 import com.google.inject.Inject;
 import com.google.protobuf.Message;
@@ -38,6 +52,9 @@ public abstract class PmsBaseEventHandler<T extends Message> {
 
   public boolean handleEvent(T event, Map<String, String> metadataMap, long createdAt) {
     try (PmsGitSyncBranchContextGuard ignore1 = gitSyncContext(event); AutoLogContext ignore2 = autoLogContext(event)) {
+      if (extractAmbiance(event) != null) {
+        SecurityContextBuilder.setContext(getPrincipalFromAmbiance(extractAmbiance(event)));
+      }
       ThreadAutoLogContext metricContext =
           new ThreadAutoLogContext(extractMetricContext(event), OverrideBehavior.OVERRIDE_NESTS);
       MonitoringInfo monitoringInfo = MonitoringInfo.builder()
@@ -59,5 +76,33 @@ public abstract class PmsBaseEventHandler<T extends Message> {
     logContext.putAll(AmbianceUtils.logContextMap(extractAmbiance(event)));
     logContext.putAll(CollectionUtils.emptyIfNull(extraLogProperties(event)));
     return new AutoLogContext(logContext, OverrideBehavior.OVERRIDE_NESTS);
+  }
+
+  protected io.harness.security.dto.Principal getPrincipalFromAmbiance(Ambiance ambiance) {
+    ExecutionPrincipalInfo executionPrincipalInfo = ambiance.getMetadata().getPrincipalInfo();
+    ExecutionTriggerInfo executionTriggerInfo = ambiance.getMetadata().getTriggerInfo();
+
+    // NOTE: rbac should not be validated for triggers so all the resources should be validated with service principals
+    if (!executionPrincipalInfo.getShouldValidateRbac()) {
+      return new ServicePrincipal(PIPELINE_SERVICE.getServiceId());
+    }
+
+    String principal = executionPrincipalInfo.getPrincipal();
+    if (EmptyPredicate.isEmpty(principal)) {
+      throw new AccessDeniedException("Execution with empty principal found. Please contact harness customer care.",
+          ErrorCode.NG_ACCESS_DENIED, WingsException.USER);
+    }
+    PrincipalType principalType = PrincipalTypeProtoToPrincipalTypeMapper.convertToAccessControlPrincipalType(
+        executionPrincipalInfo.getPrincipalType());
+    switch (principalType) {
+      case USER:
+        return new UserPrincipal(principal, executionTriggerInfo.getTriggeredBy().getExtraInfoMap().get("email"),
+            executionTriggerInfo.getTriggeredBy().getIdentifier(), AmbianceUtils.getAccountId(ambiance));
+      case SERVICE:
+        return new ServicePrincipal(principal);
+      case API_KEY:
+        return new ApiKeyPrincipal(principal);
+    }
+    throw new AccessDeniedException("Unknown Principal Type", WingsException.USER);
   }
 }
