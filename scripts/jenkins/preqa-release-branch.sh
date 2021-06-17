2518415136c34fbc1e224976ad66965373fde7e8
Shivam negi <shivamnegi94>
2021-06-17 12:55:49 +0530

    added new release file

---
+++ scripts/jenkins/preqa-release-branch.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+PROJECTS="BT|CCE|CCM|CDC|CDNG|CDP|CE|CI|CV|CVNG|DEL|DOC|DX|ER|FFM|OPS|PL|SEC|SWAT|GTM|ONP"
+
+# Check for not merged hot fixes
+git log --remotes=origin/release/* --pretty=oneline --abbrev-commit | grep -iE "\[(${PROJECTS})-[0-9]+]:" -o | sort | uniq > release.txt
+git log --remotes=origin/[m]aster --pretty=oneline --abbrev-commit | grep -iE "\[(${PROJECTS})-[0-9]+]:" -o | sort | uniq > master.txt
+
+NOT_MERGED=`comm -23 release.txt master.txt`
+
+if [ ! -z "$NOT_MERGED" ]
+then
+    echo "There are jira issues in release branches that are not reflected in master."
+    exit 1
+fi
+
+
+# initialize variables
+export VERSION_FILE=preqa_build.properties
+
+# shellcheck disable=SC2006
+export VERSION=`cat ${VERSION_FILE} |\
+    grep 'build.number=' |\
+    sed -e 's: *build.number=::g'`
+export cat =${VERSION%??}
+export NEW_VERSION=$(( ${VERSION}+1 ))
+
+# shellcheck disable=SC2006
+export BRANCH=`echo "${GIT_BRANCH}" | sed -e "s/origin\///g"`
+# shellcheck disable=SC2006
+export SHA=`git rev-parse HEAD`
+
+# Update jira issues
+#scripts/jenkins/release-branch-update-jiras.sh
+
+# Prepare new release commit
+git checkout ${BRANCH}
+
+sed -i "s:build.number=${VERSION}00:build.number=${NEW_VERSION}00:g" ${VERSION_FILE}
+git add ${VERSION_FILE}
+git commit -m "Branching to release/${PURPOSE}/${VERSION}xx. New version ${NEW_VERSION}xx"
+
+
+# Export variables
+echo VERSION=${VERSION} > jenkins.properties
+echo BRANCH=${BRANCH} >> jenkins.properties
+echo SHA=${SHA} >> jenkins.properties
