b45d0c4b26b7430a442622ce72549bd3eb23bd93
Ankit Singhal <ankit.singhal@harness.io>
2021-06-21 17:38:57 +0530

    [PL-14298]: Register AccessControlMigrationJob

--- 120-ng-manager/src/main/java/io/harness/ng/NextGenApplication.java
+++ 120-ng-manager/src/main/java/io/harness/ng/NextGenApplication.java
@@ -48,6 +48,7 @@ import io.harness.migration.MigrationProvider;
 import io.harness.migration.NGMigrationSdkInitHelper;
 import io.harness.migration.NGMigrationSdkModule;
 import io.harness.migration.beans.NGMigrationConfiguration;
+import io.harness.ng.accesscontrol.migrations.AccessControlMigrationJob;
 import io.harness.ng.core.CorrelationFilter;
 import io.harness.ng.core.EtagFilter;
 import io.harness.ng.core.event.NGEventConsumerService;
@@ -431,6 +432,7 @@ public class NextGenApplication extends Application<NextGenConfiguration> {
     environment.lifecycle().manage(injector.getInstance(NotifierScheduledExecutorService.class));
     environment.lifecycle().manage(injector.getInstance(OutboxEventPollService.class));
     environment.lifecycle().manage(injector.getInstance(UserProjectMigrationService.class));
+    environment.lifecycle().manage(injector.getInstance(AccessControlMigrationJob.class));
     createConsumerThreadsToListenToEvents(environment, injector);
   }
 
