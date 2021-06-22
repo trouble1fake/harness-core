d9859281c77a48c24bf7cb6ac0820051606fcb4f
Raj Patel <raj.patel@harness.io>
2021-06-22 20:49:26 +0530

    [PL-14788]: remove unused import

--- 980-commons/src/main/java/io/harness/security/dto/ServiceAccountPrincipal.java
+++ 980-commons/src/main/java/io/harness/security/dto/ServiceAccountPrincipal.java
@@ -3,7 +3,6 @@ package io.harness.security.dto;
 import static io.harness.annotations.dev.HarnessTeam.PL;
 import static io.harness.security.SecurityContextBuilder.PRINCIPAL_NAME;
 import static io.harness.security.SecurityContextBuilder.PRINCIPAL_TYPE;
-import static io.harness.security.dto.PrincipalType.API_KEY;
 import static io.harness.security.dto.PrincipalType.SERVICE_ACCOUNT;
 
 import io.harness.annotations.dev.OwnedBy;
