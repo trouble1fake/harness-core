ff8f8d2289354ec1202d539271aeda13f5e9a1e2
TP Honey <tp@harness.io>
2021-06-22 15:23:45 +0100

    [CI-1946]: SCM, add test for delete webhook parsing

--- product/ci/scm/parser/parser_test.go
+++ product/ci/scm/parser/parser_test.go
@@ -181,3 +181,35 @@ func TestParseCreateBranch(t *testing.T) {
 		t.Log(want)
 	}
 }
+
+func TestParseDeleteBranch(t *testing.T) {
+	data, _ := ioutil.ReadFile("testdata/branch_delete.json")
+	in := &pb.ParseWebhookRequest{
+		Body: string(data),
+		Header: &pb.Header{
+			Fields: []*pb.Header_Pair{
+				{
+					Key:    "X-Github-Event",
+					Values: []string{"delete"},
+				},
+			},
+		},
+		Secret:   "",
+		Provider: pb.GitProvider_GITHUB,
+	}
+
+	log, _ := logs.GetObservedLogger(zap.InfoLevel)
+	got, err := ParseWebhook(context.Background(), in, log.Sugar())
+	assert.Nil(t, err)
+	assert.NotNil(t, got.GetBranch())
+
+	want := &pb.ParseWebhookResponse{}
+	raw, _ := ioutil.ReadFile("testdata/branch_delete.json.golden")
+	err = jsonpb.UnmarshalString(string(raw), want)
+
+	if !proto.Equal(got, want) {
+		t.Errorf("Unexpected Results error: %s", err)
+		t.Log(got)
+		t.Log(want)
+	}
+}
