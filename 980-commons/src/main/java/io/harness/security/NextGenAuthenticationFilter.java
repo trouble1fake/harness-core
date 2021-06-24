1b8614e5d9ecf55b379f9d1caf765a9bf12e4719
Ujjawal Prasad <ujjawal.prasad@harness.io>
2021-06-24 13:42:55 +0530

    [PL-14788]: Added filter changes

--- 980-commons/src/main/java/io/harness/security/NextGenAuthenticationFilter.java
+++ 980-commons/src/main/java/io/harness/security/NextGenAuthenticationFilter.java
@@ -8,18 +8,48 @@ import io.harness.annotations.dev.OwnedBy;
 
 import com.google.inject.Singleton;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Predicate;
 import javax.annotation.Priority;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ResourceInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
 
 @OwnedBy(PL)
 @Singleton
 @Priority(AUTHENTICATION)
+@Slf4j
 public class NextGenAuthenticationFilter extends JWTAuthenticationFilter {
+  public static final String X_API_KEY = "X-Api-Key";
+
   public NextGenAuthenticationFilter(Predicate<Pair<ResourceInfo, ContainerRequestContext>> predicate,
       Map<String, JWTTokenHandler> serviceToJWTTokenHandlerMapping, Map<String, String> serviceToSecretMapping) {
     super(predicate, serviceToJWTTokenHandlerMapping, serviceToSecretMapping);
+  }
+
+  @Override
+  public void filter(ContainerRequestContext containerRequestContext) {
+    if (!super.testRequestPredicate(containerRequestContext)) {
+      // Predicate testing failed with the current request context
+      return;
+    }
+
+    Optional<String> apiKeyOptional = getApiKeyFromHeaders(containerRequestContext);
+    if (apiKeyOptional.isPresent()) {
+      String apiKey = apiKeyOptional.get();
+      log.info("Found an API key in request {}", apiKey);
+      // @Todo(Ujjawal): Try to find a principal for apikey
+      // found --> set the principal and return
+      // not found --> throw unauthorized
+    } else {
+      super.filter(containerRequestContext);
+    }
+  }
+
+  private Optional<String> getApiKeyFromHeaders(ContainerRequestContext containerRequestContext) {
+    String apiKey = containerRequestContext.getHeaderString(X_API_KEY);
+    return StringUtils.isEmpty(apiKey) ? Optional.empty() : Optional.of(apiKey);
   }
 }
