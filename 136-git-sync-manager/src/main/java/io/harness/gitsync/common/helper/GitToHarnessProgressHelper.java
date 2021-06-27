a97c9fe4640896e079f57bbc3daf4fba1aa73c08
Deepak Patankar <deepak.patankar@harness.io>
2021-06-27 23:13:29 +0530

    [CDNG-10310]: Git to harness fixes (#25201)
    * [CDNG-10318]: change queue event handling based on yaml change set id
    
    * [CDNG-10310]: Multiple fixes to G2H flow (#25191)
    
    * [CDNG-10318]: change queue event handling based on yaml change set id
    
    * [CDNG-10318]: update progress status to error in case of branch sync event exception
    
    * [CDNG-10310]: added more debug logs to trace compare commits response
    
    * [CDNG-10310]: explicitly adding createdAt timestamp
    
    * [CDNG-10127]: fixed status handling at varioud different points in branch sync flow (#25057)
    
    * [CDNG-10310]: build bazel increment
    
    * [CDNG-10310]: ignore upsert commit if file processing summary is empty
    
    * [CDNG-10310]: null check fix in file processing summary
    
    Co-authored-by: Mohit Garg <mohit.garg@harness.io>

--- 136-git-sync-manager/src/main/java/io/harness/gitsync/common/helper/GitToHarnessProgressHelper.java
+++ 136-git-sync-manager/src/main/java/io/harness/gitsync/common/helper/GitToHarnessProgressHelper.java
@@ -5,7 +5,6 @@ import static io.harness.annotations.dev.HarnessTeam.DX;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.gitsync.GitToHarnessProgressConstants;
 import io.harness.gitsync.common.beans.GitToHarnessProgressStatus;
-import io.harness.gitsync.common.beans.YamlChangeSetEventType;
 import io.harness.gitsync.common.beans.YamlChangeSetStatus;
 import io.harness.gitsync.common.dtos.GitToHarnessProgressDTO;
 import io.harness.gitsync.common.service.GitToHarnessProgressService;
@@ -24,16 +23,12 @@ public class GitToHarnessProgressHelper {
   private GitToHarnessProgressService gitToHarnessProgressService;
 
   public YamlChangeSetStatus getQueueStatusIfEventInProgressOrAlreadyProcessed(YamlChangeSetDTO yamlChangeSetDTO) {
-    GitToHarnessProgressDTO gitToHarnessProgressDTO = null;
-    if (yamlChangeSetDTO.getEventType() == YamlChangeSetEventType.BRANCH_PUSH) {
-      gitToHarnessProgressDTO =
-          gitToHarnessProgressService.getByRepoUrlAndCommitIdAndEventType(yamlChangeSetDTO.getRepoUrl(),
-              yamlChangeSetDTO.getGitWebhookRequestAttributes().getHeadCommitId(), yamlChangeSetDTO.getEventType());
-    } else if (yamlChangeSetDTO.getEventType() == YamlChangeSetEventType.BRANCH_SYNC) {
-      gitToHarnessProgressDTO =
-          gitToHarnessProgressService.getBranchSyncStatus(yamlChangeSetDTO.getRepoUrl(), yamlChangeSetDTO.getBranch());
-    }
-
+    // TODO
+    //  currently processing on basis of yamlChangeSetId, relying that queue will make sure that
+    //  no 2 events of same repo-commitid-eventType are processed together
+    //  Should handle it properly later and remove this dependency
+    GitToHarnessProgressDTO gitToHarnessProgressDTO =
+        gitToHarnessProgressService.getByYamlChangeSetId(yamlChangeSetDTO.getChangesetId());
     if (gitToHarnessProgressDTO != null) {
       if (gitToHarnessProgressDTO.getGitToHarnessProgressStatus().isInProcess()) {
         return YamlChangeSetStatus.RUNNING;
@@ -48,8 +43,8 @@ public class GitToHarnessProgressHelper {
   public void doPreRunChecks(YamlChangeSetDTO yamlChangeSetDTO) {
     GitToHarnessProgressDTO gitToHarnessProgressDTO =
         gitToHarnessProgressService.getByYamlChangeSetId(yamlChangeSetDTO.getChangesetId());
-    if (gitToHarnessProgressDTO != null) {
-      // Check if the event has been long running over than threshold duration
+    if (gitToHarnessProgressDTO != null && !gitToHarnessProgressDTO.getGitToHarnessProgressStatus().isSuccessStatus()) {
+      // Check if the event has been long running over than threshold duration without reaching completion
       // Mark it as failed if thats true, so that it can be performed again
       if (System.currentTimeMillis() - gitToHarnessProgressDTO.getLastUpdatedAt()
           >= GitToHarnessProgressConstants.longRunningEventResetDurationInMs) {
