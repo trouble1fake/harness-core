f6381a22c64e5b7301c652a66c43a9eacf05b6a1
Prabu Rajendran <prabu.rajendran@harness.io>
2021-06-30 00:04:05 +0530

    [CDC-13239]: optimize imports

--- 400-rest/src/main/java/software/wings/graphql/datafetcher/execution/ResumePipelineController.java
+++ 400-rest/src/main/java/software/wings/graphql/datafetcher/execution/ResumePipelineController.java
@@ -10,7 +10,6 @@ import io.harness.annotations.dev.HarnessModule;
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.annotations.dev.TargetModule;
-import io.harness.beans.WorkflowType;
 import io.harness.exception.InvalidRequestException;
 
 import software.wings.beans.ArtifactVariable;
