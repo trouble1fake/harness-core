b07a556e9069414a79ab92e34f4ab79da4ba3564
Raj Patel <raj.patel@harness.io>
2021-06-30 18:03:30 +0530

    [PL-15073]: move up the filter for using 960-persistence for caching

--- 955-api-key-filter/src/main/java/io/harness/security/NextGenAuthenticationFilter.java
+++ 955-api-key-filter/src/main/java/io/harness/security/NextGenAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package io.harness.security;
 
 import static io.harness.annotations.dev.HarnessTeam.PL;
+import static io.harness.token.TokenClientModule.NG_HARNESS_API_KEY_CACHE;
 
 import static javax.ws.rs.Priorities.AUTHENTICATION;
 import static org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder.BCryptVersion.$2A;
@@ -22,6 +23,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.function.Predicate;
 import javax.annotation.Priority;
+import javax.cache.Cache;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ResourceInfo;
 import lombok.extern.slf4j.Slf4j;
@@ -37,13 +39,16 @@ public class NextGenAuthenticationFilter extends JWTAuthenticationFilter {
   public static final String X_API_KEY = "X-Api-Key";
   private static final String delimiter = "\\.";
 
-  private TokenClient tokenClient;
+  private final TokenClient tokenClient;
+  private final Cache<String, TokenDTO> tokenCache;
 
   public NextGenAuthenticationFilter(Predicate<Pair<ResourceInfo, ContainerRequestContext>> predicate,
       Map<String, JWTTokenHandler> serviceToJWTTokenHandlerMapping, Map<String, String> serviceToSecretMapping,
-      @Named("PRIVILEGED") TokenClient tokenClient) {
+      @Named("PRIVILEGED") TokenClient tokenClient,
+      @Named(NG_HARNESS_API_KEY_CACHE) Cache<String, TokenDTO> tokenCache) {
     super(predicate, serviceToJWTTokenHandlerMapping, serviceToSecretMapping);
     this.tokenClient = tokenClient;
+    this.tokenCache = tokenCache;
   }
 
   @Override
@@ -58,26 +63,33 @@ public class NextGenAuthenticationFilter extends JWTAuthenticationFilter {
       String apiKey = apiKeyOptional.get();
       log.info("Found an API key in request {}", apiKey);
       String[] splitToken = apiKey.split(delimiter);
+      String tokenId = splitToken[0];
       if (EmptyPredicate.isNotEmpty(splitToken)) {
-        TokenDTO tokenDTO = NGRestUtils.getResponse(tokenClient.getToken(splitToken[0]));
-        String rawPassword = apiKey.replaceAll(splitToken[0] + delimiter, "");
+        TokenDTO tokenDTO = getTokenDTO(tokenId);
+        String rawPassword = apiKey.replaceAll(tokenId + delimiter, "");
         if (tokenDTO != null) {
           if (!new BCryptPasswordEncoder($2A, 10).matches(rawPassword, tokenDTO.getIdentifier())) {
             throw new InvalidRequestException("Could not find the API token in Harness");
           }
           if (!tokenDTO.isValid()) {
+            // @Todo(Raj): Should we remove the entry from cache?
+            //             What if a user keeps on hitting with expired token? We should hit the cache and return
+            // removeTokenFromCache(tokenId);
             throw new InvalidRequestException("Incoming API token " + tokenDTO.getName() + " has expired");
           }
+
           Principal principal = null;
           if (tokenDTO.getApiKeyType() == ApiKeyType.SERVICE_ACCOUNT) {
             principal = new ServiceAccountPrincipal(tokenDTO.getParentIdentifier());
-          }
-          if (tokenDTO.getApiKeyType() == ApiKeyType.USER) {
+          } else if (tokenDTO.getApiKeyType() == ApiKeyType.USER) {
             principal = new UserPrincipal(tokenDTO.getParentIdentifier(), tokenDTO.getEmail(), tokenDTO.getUsername(),
                 tokenDTO.getAccountIdentifier());
           }
+
           SecurityContextBuilder.setContext(principal);
           SourcePrincipalContextBuilder.setSourcePrincipal(principal);
+
+          storeTokenInCache(tokenId, tokenDTO);
         } else {
           throw new InvalidRequestException("Could not find the API token in Harness");
         }
@@ -86,6 +98,43 @@ public class NextGenAuthenticationFilter extends JWTAuthenticationFilter {
       }
     } else {
       super.filter(containerRequestContext);
+    }
+  }
+
+  private TokenDTO getTokenDTO(String tokenId) {
+    TokenDTO tokenDTO = getTokenFromCache(tokenId);
+    if (tokenDTO != null) {
+      log.info("apikey cache hit for {}: {}", tokenId, tokenDTO);
+      return tokenDTO;
+    } else {
+      tokenDTO = NGRestUtils.getResponse(tokenClient.getToken(tokenId));
+    }
+
+    return tokenDTO;
+  }
+
+  private TokenDTO getTokenFromCache(String tokenId) {
+    try {
+      return tokenCache.get(tokenId);
+    } catch (Exception e) {
+      log.error("Exception while reading from apikey token cache, fetching from http call. tokenId: " + tokenId, e);
+      return null;
+    }
+  }
+
+  private void removeTokenFromCache(String tokenId) {
+    try {
+      tokenCache.remove(tokenId);
+    } catch (Exception e) {
+      log.error("Exception while removing apikey token cache. tokenId: " + tokenId, e);
+    }
+  }
+
+  private void storeTokenInCache(String tokenId, TokenDTO tokenDTO) {
+    try {
+      tokenCache.put(tokenId, tokenDTO);
+    } catch (Exception e) {
+      log.error("Exception while storing apikey token in cache. tokenId: " + tokenId, e);
     }
   }
 
--- 955-api-key-filter/src/main/java/io/harness/token/TokenClientModule.java
+++ 955-api-key-filter/src/main/java/io/harness/token/TokenClientModule.java
@@ -3,21 +3,31 @@ package io.harness.token;
 import static io.harness.annotations.dev.HarnessTeam.PL;
 
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.cache.HarnessCacheManager;
+import io.harness.ng.core.dto.TokenDTO;
 import io.harness.remote.client.ClientMode;
 import io.harness.remote.client.ServiceHttpClientConfig;
 import io.harness.security.ServiceTokenGenerator;
 import io.harness.token.remote.TokenClient;
 import io.harness.token.remote.TokenClientHttpFactory;
+import io.harness.version.VersionInfoManager;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Key;
 import com.google.inject.Provides;
 import com.google.inject.Scopes;
+import com.google.inject.Singleton;
+import com.google.inject.TypeLiteral;
+import com.google.inject.multibindings.MapBinder;
 import com.google.inject.name.Named;
 import com.google.inject.name.Names;
+import javax.cache.Cache;
+import javax.cache.expiry.AccessedExpiryPolicy;
+import javax.cache.expiry.Duration;
 
 @OwnedBy(PL)
 public class TokenClientModule extends AbstractModule {
+  public static final String NG_HARNESS_API_KEY_CACHE = "NGHarnessApiKeyCache";
   private final ServiceHttpClientConfig ngManagerClientConfig;
   private final String serviceSecret;
   private final String clientId;
@@ -42,8 +52,19 @@ public class TokenClientModule extends AbstractModule {
         ngManagerClientConfig, serviceSecret, new ServiceTokenGenerator(), clientId, ClientMode.NON_PRIVILEGED);
   }
 
+  @Provides
+  @Singleton
+  @Named(NG_HARNESS_API_KEY_CACHE)
+  public Cache<String, TokenDTO> getTokenCache(
+      HarnessCacheManager harnessCacheManager, VersionInfoManager versionInfoManager) {
+    return harnessCacheManager.getCache(NG_HARNESS_API_KEY_CACHE, String.class, TokenDTO.class,
+        AccessedExpiryPolicy.factoryOf(Duration.TEN_MINUTES), versionInfoManager.getVersionInfo().getBuildNo());
+  }
+
   @Override
   protected void configure() {
+    requireBinding(HarnessCacheManager.class);
+    requireBinding(VersionInfoManager.class);
     bind(TokenClient.class)
         .annotatedWith(Names.named(ClientMode.PRIVILEGED.name()))
         .toProvider(Key.get(TokenClientHttpFactory.class, Names.named(ClientMode.PRIVILEGED.name())))
@@ -51,5 +72,13 @@ public class TokenClientModule extends AbstractModule {
     bind(TokenClient.class)
         .toProvider(Key.get(TokenClientHttpFactory.class, Names.named(ClientMode.NON_PRIVILEGED.name())))
         .in(Scopes.SINGLETON);
+    bindCaches();
+  }
+
+  private void bindCaches() {
+    MapBinder<String, Cache<?, ?>> mapBinder =
+        MapBinder.newMapBinder(binder(), TypeLiteral.get(String.class), new TypeLiteral<Cache<?, ?>>() {});
+    mapBinder.addBinding(NG_HARNESS_API_KEY_CACHE).to(Key.get(new TypeLiteral<Cache<String, TokenDTO>>() {
+    }, Names.named(NG_HARNESS_API_KEY_CACHE)));
   }
 }
