88171df2cca111cba8a7aa6e5bff484be098c714
VikasMaddukuriHarness <vikas.maddukuri@harness.io>
2021-07-12 22:44:22 +0530

    PL-13998 Cannot create secrets via the GraphQL API for custom secrets manager when input variables are required

--- scripts/jenkins/author-check.sh
+++ scripts/jenkins/author-check.sh
@@ -175,6 +175,7 @@ git log -1000 --oneline --format='%aN <%aE>' | sort -u |\
     grep -iv "^Vikas Naiyar <vikas.naiyar@harness.io>$" |\
     grep -iv "^Vikas Singh <vikas.singh@harness.io>$" |\
     grep -iv "^VikasMaddukuriHarness <vikas.maddukuri@harness.io>$" |\
+    grep -iv "^VikasMaddukuriHarness <86037848+VikasMaddukuriHarness@users.noreply.github.com>$" |\
     grep -iv "^Vistaar Juneja <vistaar.juneja@harness.io>$" |\
     grep -iv "^Vladimir Peric <vladimir.peric@harness.io>$" |\
     grep -iv "^Vojin Djukic <vojin.djukic@harness.io>$" |\
