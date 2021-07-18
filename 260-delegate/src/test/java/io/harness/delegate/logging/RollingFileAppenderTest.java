7df54818ef621d55fefa4e8abb1a4e48e78be1a8
Jenny James <jenny.james@harness.io>
2021-07-17 18:48:33 -0700

    [DEL-0]: rolling file appender reset fix

---
+++ 260-delegate/src/test/java/io/harness/delegate/logging/RollingFileAppenderTest.java
@@ -0,0 +1,13 @@
+package io.harness.delegate.logging;
+
+import io.harness.CategoryTest;
+
+public class RollingFileAppenderTest extends CategoryTest {
+
+    private static final String CONFIG = "src/main/resources/logback-test.xml";
+
+
+    public void setUp() throws Exception {
+
+
+}
---
+++ 980-commons/src/main/java/io/harness/logging/RemoteTriggeringPolicy.java
@@ -0,0 +1,18 @@
+package io.harness.logging;
+
+import ch.qos.logback.core.rolling.TriggeringPolicyBase;
+import java.io.File;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class RemoteTriggeringPolicy<E> extends TriggeringPolicyBase<E> {
+  static AtomicBoolean doRolling = new AtomicBoolean();
+
+  @Override
+  public boolean isTriggeringEvent(File file, E e) {
+    return doRolling.getAndSet(false);
+  }
+
+  public static void resetRollingPolicy() {
+    doRolling.set(true);
+  }
+}
