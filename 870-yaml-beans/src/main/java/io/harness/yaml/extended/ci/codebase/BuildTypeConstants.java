4ee69692b97464d92786aafad4c6b00ba04d3f23
Aleksandar Radisavljevic <aleksandar.radisavljevic@harness.io>
2021-07-20 16:14:40 +0200

    [CI-2153]: yaml beans for codebase

--- 870-yaml-beans/src/main/java/io/harness/yaml/extended/ci/codebase/BuildTypeConstants.java
+++ 870-yaml-beans/src/main/java/io/harness/yaml/extended/ci/codebase/BuildTypeConstants.java
@@ -1,6 +1,8 @@
 package io.harness.yaml.extended.ci.codebase;
 
 public class BuildTypeConstants {
-  public static final String BRANCH_TYPE = "branch";
-  public static final String TAG_TYPE = "tag";
+  public static final String BRANCH = "Branch";
+  public static final String TAG = "Tag";
+  public static final String PR = "PR";
+  public static final String COMMIT_SHA = "CommitSha";
 }
--- 870-yaml-beans/src/main/java/io/harness/yaml/extended/ci/codebase/impl/BranchBuildSpec.java
+++ 870-yaml-beans/src/main/java/io/harness/yaml/extended/ci/codebase/impl/BranchBuildSpec.java
@@ -2,7 +2,7 @@ package io.harness.yaml.extended.ci.codebase.impl;
 
 import static io.harness.annotations.dev.HarnessTeam.CI;
 import static io.harness.beans.common.SwaggerConstants.STRING_CLASSPATH;
-import static io.harness.yaml.extended.ci.codebase.BuildTypeConstants.BRANCH_TYPE;
+import static io.harness.yaml.extended.ci.codebase.BuildTypeConstants.BRANCH;
 
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.pms.yaml.ParameterField;
@@ -18,7 +18,7 @@ import org.springframework.data.annotation.TypeAlias;
 @Value
 @Builder
 @TypeAlias("io.harness.yaml.extended.ci.impl.BranchBuildSpec")
-@JsonTypeName(BRANCH_TYPE)
+@JsonTypeName(BRANCH)
 @OwnedBy(CI)
 public class BranchBuildSpec implements BuildSpec {
   @NotNull @ApiModelProperty(dataType = STRING_CLASSPATH) ParameterField<String> branch;
