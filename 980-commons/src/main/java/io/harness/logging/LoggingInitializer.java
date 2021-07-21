194919006c850868ed1b0977e83a981ba31f800b
Jenny James <jenny.james@harness.io>
2021-07-21 08:12:51 -0700

    [Del-0]: debug params for PR test

--- 980-commons/src/main/java/io/harness/logging/LoggingInitializer.java
+++ 980-commons/src/main/java/io/harness/logging/LoggingInitializer.java
@@ -3,10 +3,17 @@ package io.harness.logging;
 import io.harness.eraro.MessageManager;
 import io.harness.exception.WingsException;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.rolling.RollingFileAppender;
+import ch.qos.logback.core.rolling.SizeAndTimeBasedRollingPolicy;
+import ch.qos.logback.core.rolling.TimeBasedFileNamingAndTriggeringPolicy;
+import ch.qos.logback.core.rolling.TimeBasedRollingPolicy;
 import java.io.IOException;
 import java.io.InputStream;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 
 @UtilityClass
 @Slf4j
@@ -19,6 +26,14 @@ public class LoggingInitializer {
    * Initialize logging.
    */
   public static void initializeLogging() {
+    Logger logger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+    RollingFileAppender<?> appender = (RollingFileAppender<?>) logger.getAppender("file");
+    SizeAndTimeBasedRollingPolicy<?> policy = (SizeAndTimeBasedRollingPolicy<?>) appender.getRollingPolicy();
+    TimeBasedFileNamingAndTriggeringPolicy<?> trigger = policy.getTimeBasedFileNamingAndTriggeringPolicy();
+    trigger.setCurrentTime(System.currentTimeMillis() + 24 * 60 * 60 * 1000);
+    log.info("reset log files");
+    trigger.setCurrentTime(0);
+
     log.info("start initializeLogging");
     if (!initialized) {
       log.info("reset values initializeLogging");
@@ -30,6 +45,5 @@ public class LoggingInitializer {
 
       initialized = true;
     }
-    //LoggingTriggerPolicy.resetRollingPolicy();
   }
 }
