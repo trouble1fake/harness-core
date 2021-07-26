46feb15ac078071c68e5c14fb9ee8b545088a263
Nataraja Maruthi <nataraja@harness.io>
2021-07-26 15:29:47 +0530

    [ART-293]: Release Onprem Branch test Job

--- scripts/jenkins/release-onprem-branch-test.sh
+++ scripts/jenkins/release-onprem-branch-test.sh
@@ -4,29 +4,19 @@ set -e
 
 PROJECTS="ART|BT|CCE|CCM|CDC|CDNG|CDP|CE|CI|CV|CVNG|DEL|DOC|DX|ER|FFM|OPS|PL|SEC|SWAT|GTM|ONP"
 
-#git config --global credential.helper "cache --timeout=3600"
-#git init
-#git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/wings-software/portal.git
-#cd portal
-#git checkout $SAAS_BRANCH
-#
-#onprem=`git branch -r | grep "release/on-prem" |grep ".xx$"| tail -5`
-#
-#echo "onprem is : ${onprem}"
-#
-#
-#for line in `git branch -r | grep "release/on-prem" |grep ".xx$"| tail -5`;
-#	do
-#	       echo "Onprem branch is $line"
-#         git log --remotes=$line* --pretty=oneline --abbrev-commit | grep -iE "\[(${PROJECTS})-[0-9]+]:" -o | sort | uniq | tr '[:lower:]' '[:upper:]' > release_onprem_temp.txt
-#done
-#
-#cat release_onprem_temp.txt | sort | uniq >release_onprem.txt
-#
-#
-#echo "Saas branch is ${SAAS_BRANCH}"
-#
-#git log --remotes=origin/${SAAS_BRANCH}* --pretty=oneline --abbrev-commit | grep -iE "\[(${PROJECTS})-[0-9]+]:" -o | sort | uniq | tr '[:lower:]' '[:upper:]' > release_saas.txt
+
+for line in `git branch -r | grep "release/on-prem" |grep ".xx$"| tail -5`;
+	do
+	       echo "Onprem branch is $line"
+         git log --remotes=$line* --pretty=oneline --abbrev-commit | grep -iE "\[(${PROJECTS})-[0-9]+]:" -o | sort | uniq | tr '[:lower:]' '[:upper:]' > release_onprem_temp.txt
+done
+
+cat release_onprem_temp.txt | sort | uniq >release_onprem.txt
+
+
+echo "Saas branch is ${SAAS_BRANCH}"
+
+git log --remotes=origin/${SAAS_BRANCH}* --pretty=oneline --abbrev-commit | grep -iE "\[(${PROJECTS})-[0-9]+]:" -o | sort | uniq | tr '[:lower:]' '[:upper:]' > release_saas.txt
 
 NOT_MERGED=`comm -23 release_onprem.txt release_saas.txt | tr '\n' ' '`
 
