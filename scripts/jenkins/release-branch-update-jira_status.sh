29daa07b9d4849d5ee105b8346b8a2f027ce36bc
Nataraja Maruthi <nataraja@harness.io>
2021-07-27 16:26:39 +0530

    [ART-296]: Removed the CI credentials in Script

--- scripts/jenkins/release-branch-update-jira_status.sh
+++ scripts/jenkins/release-branch-update-jira_status.sh
@@ -4,7 +4,7 @@ KEYS=$(git log --pretty=oneline --abbrev-commit |\
       awk "/${PREVIOUS_CUT_COMMIT_MESSAGE}/ {exit} {print}" |\
       grep -o -iE '('$PROJECTS')-[0-9]+' | sort | uniq)
 
-QA_TEST_STATUS_ID=201
+STATUS_ID=$STATUS_ID_TO_MOVE
 
 for KEY in ${KEYS}
 do
@@ -21,7 +21,7 @@ do
                echo " ${KEY}  is in  ${status} , Hence moving to QA-Test status"
                curl \
                  -X POST \
-                --data "{\"transition\":{\"id\":\"${QA_TEST_STATUS_ID}\"}}" \
+                --data "{\"transition\":{\"id\":\"${STATUS_ID}\"}}" \
                 -H "Content-Type: application/json" \
                 https://harness.atlassian.net/rest/api/2/issue/${KEY}/transitions \
                 --user $JIRA_USERNAME:$JIRA_PASSWORD
