1b4c4c4867d89fd1ca24eda91d481d31837e5c09
Meet Rathod <rathod.meetsatish@harness.io>
2021-08-02 17:29:44 +0530

    removed unused annotation in ServiceInfoResponseSummary

--- 400-rest/src/main/java/software/wings/service/impl/instance/ServiceInfoResponseSummary.java
+++ 400-rest/src/main/java/software/wings/service/impl/instance/ServiceInfoResponseSummary.java
@@ -6,12 +6,10 @@ import io.harness.annotations.dev.OwnedBy;
 
 import lombok.Builder;
 import lombok.Value;
-import lombok.experimental.FieldNameConstants;
 
 @Value
 @Builder
 @OwnedBy(DX)
-@FieldNameConstants(innerTypeName = "ServiceInfoResponseSummaryKeys")
 public class ServiceInfoResponseSummary {
   String lastArtifactBuildNum;
   String lastWorkflowExecutionId;
