4c0dfb40cd9e0dcffac56423173e44c66f090133
Rohit Karelia <rohit.karelia@harness.io>
2021-08-10 14:24:35 -0700

    [CDNG-11497]: Update kryo registrars

--- 810-ng-triggers/src/main/java/io/harness/serializer/kryo/NGTriggerKryoRegistrar.java
+++ 810-ng-triggers/src/main/java/io/harness/serializer/kryo/NGTriggerKryoRegistrar.java
@@ -5,13 +5,9 @@ import static io.harness.annotations.dev.HarnessTeam.PIPELINE;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.ngtriggers.beans.config.NGTriggerConfig;
 import io.harness.ngtriggers.beans.config.NGTriggerConfigV2;
-import io.harness.polling.contracts.PollingItem;
-import io.harness.polling.contracts.service.PollingDocument;
 import io.harness.serializer.KryoRegistrar;
 
 import com.esotericsoftware.kryo.Kryo;
-import io.serializer.kryo.PollingDocumentKryoSerializer;
-import io.serializer.kryo.PollingItemKryoSerializer;
 
 @OwnedBy(PIPELINE)
 public class NGTriggerKryoRegistrar implements KryoRegistrar {
@@ -20,7 +16,5 @@ public class NGTriggerKryoRegistrar implements KryoRegistrar {
   public void register(Kryo kryo) {
     kryo.register(NGTriggerConfig.class, 400001);
     kryo.register(NGTriggerConfigV2.class, 400002);
-    //    kryo.register(PollingItem.class, PollingItemKryoSerializer.getInstance(), 400003);
-    //    kryo.register(PollingDocument.class, PollingDocumentKryoSerializer.getInstance(), 400004);
   }
 }
