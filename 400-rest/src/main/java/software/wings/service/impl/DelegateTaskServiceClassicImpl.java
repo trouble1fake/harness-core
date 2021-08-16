2e42841aba01e09b385695be5c6744298cb44a37
Xin Shao <xin.shao@harness.io>
2021-08-16 16:34:33 -0400

    [DEL-2320]: Fix aeriform

--- 400-rest/src/main/java/software/wings/service/impl/DelegateTaskServiceClassicImpl.java
+++ 400-rest/src/main/java/software/wings/service/impl/DelegateTaskServiceClassicImpl.java
@@ -207,6 +207,11 @@ import org.mongodb.morphia.query.UpdateOperations;
 @BreakDependencyOn("software.wings.app.MainConfiguration")
 @BreakDependencyOn("software.wings.app.PortalConfig")
 @BreakDependencyOn("software.wings.beans.Application")
+@BreakDependencyOn("io.harness.event.handler.impl.EventPublishHelper")
+@BreakDependencyOn("software.wings.beans.ExecutionCredential")
+@BreakDependencyOn("software.wings.beans.GitConfig")
+@BreakDependencyOn("software.wings.expression.NgSecretManagerFunctor")
+@BreakDependencyOn("software.wings.beans.GitValidationParameters")
 @OwnedBy(DEL)
 public class DelegateTaskServiceClassicImpl implements DelegateTaskServiceClassic {
   private static final String ASYNC = "async";
