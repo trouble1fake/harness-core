fcbd53e63c2961d0a408e155a7ac1aa92ca88c39
Meet Rathod <rathod.meetsatish@harness.io>
2021-08-17 16:20:15 +0530

    added mongoIndexes

--- 400-rest/src/main/java/software/wings/beans/infrastructure/instance/PerpetualTaskScheduleConfig.java
+++ 400-rest/src/main/java/software/wings/beans/infrastructure/instance/PerpetualTaskScheduleConfig.java
@@ -1,8 +1,12 @@
 package software.wings.beans.infrastructure.instance;
 
 import io.harness.iterator.PersistentRegularIterable;
+import io.harness.mongo.index.CompoundMongoIndex;
+import io.harness.mongo.index.MongoIndex;
 import io.harness.persistence.PersistentEntity;
 
+import com.google.common.collect.ImmutableList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,7 +23,15 @@ import org.mongodb.morphia.annotations.Id;
 @FieldNameConstants(innerTypeName = "PerpetualTaskScheduleConfigKeys")
 @Entity(value = "perpetualTaskScheduleConfig", noClassnameStored = true)
 public class PerpetualTaskScheduleConfig implements PersistentEntity {
-  @Id private String id;
+  public static List<MongoIndex> mongoIndexes() {
+    return ImmutableList.<MongoIndex>builder()
+        .add(CompoundMongoIndex.builder()
+                 .name("perpetualTaskScheduleConfig_index1")
+                 .field(PerpetualTaskScheduleConfigKeys.accountId)
+                 .field(PerpetualTaskScheduleConfigKeys.perpetualTaskType)
+                 .build())
+        .build();
+  }
   private String accountId;
   private int timeInterval;
   private String perpetualTaskType;
