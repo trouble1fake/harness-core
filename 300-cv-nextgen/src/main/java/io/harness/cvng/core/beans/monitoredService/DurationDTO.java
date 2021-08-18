8c525f87ae3b208da2bb2bd5369ab961ab5cf555
Vistaar Juneja <vistaar.juneja@harness.io>
2021-08-18 16:16:05 +0530

    [CI-0]: Update DurationDTO

--- 300-cv-nextgen/src/main/java/io/harness/cvng/core/beans/monitoredService/DurationDTO.java
+++ 300-cv-nextgen/src/main/java/io/harness/cvng/core/beans/monitoredService/DurationDTO.java
@@ -2,6 +2,8 @@ package io.harness.cvng.core.beans.monitoredService;
 
 import java.time.Duration;
 
+
+
 public enum DurationDTO {
   FOUR_HOURS(Duration.ofHours(4)),
   TWENTY_FOUR_HOURS(Duration.ofDays(1)),
