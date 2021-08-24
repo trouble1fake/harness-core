bfa9992ca82bdb3e110bc6e98d5c590e865aeec0
Jenny James <jenny.james@harness.io>
2021-08-24 09:43:56 -0700

    [DEL-2232]: resource split between manager and DMS

--- 400-rest/src/main/java/software/wings/service/intfc/DownloadTokenService.java
+++ 400-rest/src/main/java/software/wings/service/intfc/DownloadTokenService.java
@@ -1,8 +1,12 @@
 package software.wings.service.intfc;
 
+import io.harness.annotations.dev.HarnessModule;
+import io.harness.annotations.dev.TargetModule;
+
 /**
  * Created by peeyushaggarwal on 12/13/16.
  */
+@TargetModule(HarnessModule._980_COMMONS)
 public interface DownloadTokenService {
   String createDownloadToken(String resource);
   void validateDownloadToken(String resource, String token);
