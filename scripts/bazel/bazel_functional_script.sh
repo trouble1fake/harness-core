e89ce2654e4da0461d058a84d9d6723b9d809dfe
Arvind Choudhary <arvind.choudhary@harness.io>
2021-08-26 18:02:25 +0530

    [CDP-18336]: making 230-model-test non-background

--- scripts/bazel/bazel_functional_script.sh
+++ scripts/bazel/bazel_functional_script.sh
@@ -30,13 +30,13 @@ if [ "${RUN_BAZEL_FUNCTIONAL_TESTS}" == "true" ]; then
   bazel build ${GCP} ${BAZEL_ARGUMENTS} -- //200-functional-test/...
   curl https://storage.googleapis.com/harness-prod-public/public/shared/tools/alpn/release/8.1.13.v20181017/alpn-boot-8.1.13.v20181017.jar --output alpn-boot-8.1.13.v20181017.jar
 
-  bazel run ${GCP} ${BAZEL_ARGUMENTS} 230-model-test:app &
-  MODEL_TEST_APP_PID=$!
+  bazel run ${GCP} ${BAZEL_ARGUMENTS} 230-model-test:app
+#  MODEL_TEST_APP_PID=$!
   java -Xbootclasspath/p:alpn-boot-8.1.13.v20181017.jar -Xmx4096m -XX:+HeapDumpOnOutOfMemoryError \
     -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:mygclogfilename.gc -XX:+UseParallelGC \
     -XX:MaxGCPauseMillis=500 -jar /harness/bazel-out/k8-fastbuild/bin/260-delegate/module_deploy.jar /harness/260-delegate/config-delegate.yml &> /harness/delegate.log &
   DELEGATE_PID=$!
-  sleep 5m;
+#  sleep 5m;
   MANAGER_PID=`ps ax | grep 'java' | grep '360-cg-manager/modified_config' |  awk '{print $1}'`
   echo "INFO: MANAGER_PID = $MANAGER_PID"
 
@@ -49,10 +49,10 @@ if [ "${RUN_BAZEL_FUNCTIONAL_TESTS}" == "true" ]; then
 
   echo "INFO: MANAGER_PID = $MANAGER_PID"
   echo "INFO: DELEGATE_PID = $DELEGATE_PID"
-  echo "INFO: MODEL_TEST_APP_PID = $MODEL_TEST_APP_PID"
+#  echo "INFO: MODEL_TEST_APP_PID = $MODEL_TEST_APP_PID"
 
   kill -9 $MANAGER_PID || true
   kill -9 $DELEGATE_PID || true
-  kill -9 $MODEL_TEST_APP_PID || true
-  pkill -P $$
+#  kill -9 $MODEL_TEST_APP_PID || true
+#  pkill -P $$
 fi
