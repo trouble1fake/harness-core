b2c3cae22c3b3a5456af8ecf62b9fec239d93928
Karan Siwach <karan.siwach@harness.io>
2021-08-27 15:22:00 +0530

    [PL-19335]: permissions

--- 940-resource-group-beans/src/main/java/io/harness/resourcegroup/model/DynamicResourceSelector.java
+++ 940-resource-group-beans/src/main/java/io/harness/resourcegroup/model/DynamicResourceSelector.java
@@ -1,9 +1,13 @@
 package io.harness.resourcegroup.model;
 
 import javax.validation.constraints.NotNull;
+
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
 import lombok.Builder;
 import lombok.Data;
 
+@OwnedBy(HarnessTeam.PL)
 @Data
 @Builder
 public class DynamicResourceSelector implements ResourceSelector {
