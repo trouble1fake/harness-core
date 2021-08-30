239414db8abdc0e46600f49e056089d0d1b382ec
Abhijith Pradeep <abhijith.pradeep@harness.io>
2021-08-30 15:49:27 +0530

    [CVNG-3166]: ChangeEvents and HarnessCDChangeEvent

---
+++ 300-cv-nextgen/src/main/java/io/harness/cvng/core/services/api/ChangeEventService.java
@@ -0,0 +1,8 @@
+package io.harness.cvng.core.services.api;
+
+import io.harness.cvng.core.beans.change.event.ChangeEventDTO;
+import io.harness.cvng.core.entities.changeSource.event.ChangeEvent;
+
+public interface ChangeEventService extends DeleteEntityByHandler<ChangeEvent> {
+  Boolean register(String accountId, ChangeEventDTO changeEventDTO);
+}
