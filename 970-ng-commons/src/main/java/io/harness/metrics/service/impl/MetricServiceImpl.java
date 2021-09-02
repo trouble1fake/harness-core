63f2a7d78896c9ad53d65c8664acf1d9e640f880
Kamal Joshi <kamal.joshi@harness.io>
2021-09-02 12:17:23 +0530

    [CVNG-3310]:add owned by annotation

--- 970-ng-commons/src/main/java/io/harness/metrics/service/impl/MetricServiceImpl.java
+++ 970-ng-commons/src/main/java/io/harness/metrics/service/impl/MetricServiceImpl.java
@@ -5,6 +5,8 @@ import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
 import static io.harness.metrics.MetricConstants.ENV_LABEL;
 import static io.harness.metrics.MetricConstants.METRIC_LABEL_PREFIX;
 
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
 import io.harness.metrics.beans.MetricConfiguration;
 import io.harness.metrics.beans.MetricGroup;
 import io.harness.metrics.service.api.MetricDefinitionInitializer;
@@ -48,6 +50,7 @@ import org.reflections.Reflections;
 import org.reflections.scanners.ResourcesScanner;
 
 @Slf4j
+@OwnedBy(HarnessTeam.CV)
 public class MetricServiceImpl implements MetricService {
   public static final String GOOGLE_APPLICATION_CREDENTIALS = "GOOGLE_APPLICATION_CREDENTIALS";
   private static boolean WILL_PUBLISH_METRICS;
