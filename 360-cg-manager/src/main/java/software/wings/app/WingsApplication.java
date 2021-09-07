6b661c2bbba5b04c44f08549f54c8cc22211d9c3
Jenny James <jenny.james@harness.io>
2021-09-06 17:21:02 -0700

    dummy

--- 360-cg-manager/src/main/java/software/wings/app/WingsApplication.java
+++ 360-cg-manager/src/main/java/software/wings/app/WingsApplication.java
@@ -421,9 +421,7 @@ public class WingsApplication extends Application<MainConfiguration> {
     injector.getInstance(new Key<Map<String, Cache<?, ?>>>() {});
 
     boolean shouldEnableDelegateMgmt = shouldEnableDelegateMgmt(configuration);
-    if (shouldEnableDelegateMgmt) {
-      registerAtmosphereStreams(environment, injector);
-    }
+    registerAtmosphereStreams(environment, injector);
 
     initializeFeatureFlags(configuration, injector);
 
