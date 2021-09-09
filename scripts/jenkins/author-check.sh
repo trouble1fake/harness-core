28c0ec2f42261fdf8a213ab65cd7c19585513481
Yogesh Chauhan <yogesh.chauhan@harness.io>
2021-09-09 10:44:37 +0530

    Revert "[CDP-18536]: Add Argo Connector in NG (#26763) (#26871)"
    This reverts commit 4f4e92f1261815ce3a26407e93fe84c1448fcfc3.

--- scripts/jenkins/author-check.sh
+++ scripts/jenkins/author-check.sh
@@ -218,7 +218,7 @@ mv .mailmap.tmp .mailmap
 
 echo $EXECPTION_COMMITS exceptions
 
-if [ $EXECPTION_COMMITS -gt 246 ]
+if [ $EXECPTION_COMMITS -gt 244 ]
 then
     echo "You bringing commit with excepted author that is no longer allowed"
     git log --oneline --format='%aN <%aE>' | grep  "$EXCEPTIONS"
