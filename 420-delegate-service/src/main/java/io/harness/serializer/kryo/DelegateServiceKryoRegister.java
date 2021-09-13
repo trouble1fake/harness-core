5f169bf255f25e67d20ccc37449f3e0799775240
Jenny James <jenny.james@harness.io>
2021-09-12 19:22:12 -0700

    dummy

--- 420-delegate-service/src/main/java/io/harness/serializer/kryo/DelegateServiceKryoRegister.java
+++ 420-delegate-service/src/main/java/io/harness/serializer/kryo/DelegateServiceKryoRegister.java
@@ -3,10 +3,11 @@ package io.harness.serializer.kryo;
 import io.harness.serializer.KryoRegistrar;
 
 import com.esotericsoftware.kryo.Kryo;
+import software.wings.beans.PerpetualTaskBroadcastEvent;
 
 public class DelegateServiceKryoRegister implements KryoRegistrar {
   @Override
   public void register(Kryo kryo) {
-    // nothing to do
+    kryo.register(PerpetualTaskBroadcastEvent.class, 40015);
   }
 }
