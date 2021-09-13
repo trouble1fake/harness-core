49f8ad5bc9057bf5f4e15aee74d1ecb84d6926fb
Jenny James <jenny.james@harness.io>
2021-09-12 17:53:37 -0700

    [del-0]: PT kryo register failing fix

--- 420-delegate-service/src/main/java/io/harness/serializer/kryo/DelegateServiceKryoRegister.java
+++ 420-delegate-service/src/main/java/io/harness/serializer/kryo/DelegateServiceKryoRegister.java
@@ -2,11 +2,13 @@ package io.harness.serializer.kryo;
 
 import io.harness.serializer.KryoRegistrar;
 
+import software.wings.beans.PerpetualTaskBroadcastEvent;
+
 import com.esotericsoftware.kryo.Kryo;
 
 public class DelegateServiceKryoRegister implements KryoRegistrar {
   @Override
   public void register(Kryo kryo) {
-    // nothing to do
+    kryo.register(PerpetualTaskBroadcastEvent.class, 40015);
   }
 }
