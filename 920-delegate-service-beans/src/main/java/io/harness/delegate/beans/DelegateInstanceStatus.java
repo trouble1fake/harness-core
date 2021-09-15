7d2cfae684d80717f9a3a7c33f72f642ac0384e7
Zhuo Yin <zhuo.yin@harness.io>
2021-09-15 14:37:07 -0400

    [GTM-445]: Changes for updating jackson-dataformat-xml for GTM

--- 920-delegate-service-beans/src/main/java/io/harness/delegate/beans/DelegateInstanceStatus.java
+++ 920-delegate-service-beans/src/main/java/io/harness/delegate/beans/DelegateInstanceStatus.java
@@ -1,9 +1,12 @@
 package io.harness.delegate.beans;
 
 import io.harness.annotations.dev.HarnessModule;
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
 import io.harness.annotations.dev.TargetModule;
 
 @TargetModule(HarnessModule._955_DELEGATE_BEANS)
+@OwnedBy(HarnessTeam.DEL)
 public enum DelegateInstanceStatus {
   ENABLED,
   WAITING_FOR_APPROVAL,
