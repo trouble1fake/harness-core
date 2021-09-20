3a70b425e8435c12a2ad7e6e6fc0b72e935de817
Mohit Garg <mohit.garg@harness.io>
2021-09-21 02:04:05 +0530

    [PL-17645]: added unique index to perpetual task id in instance sync perpetual task status collection

--- 400-rest/src/main/java/software/wings/beans/infrastructure/instance/InstanceSyncPerpetualTaskStatus.java
+++ 400-rest/src/main/java/software/wings/beans/infrastructure/instance/InstanceSyncPerpetualTaskStatus.java
@@ -2,6 +2,7 @@ package software.wings.beans.infrastructure.instance;
 
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.mongo.index.FdUniqueIndex;
 import io.harness.persistence.PersistentEntity;
 
 import lombok.Builder;
@@ -15,7 +16,7 @@ import org.mongodb.morphia.annotations.Entity;
 @Entity(value = "instanceSyncPerpetualTaskStatus", noClassnameStored = true)
 @OwnedBy(HarnessTeam.PL)
 public class InstanceSyncPerpetualTaskStatus implements PersistentEntity {
-  String perpetualTaskId;
+  @FdUniqueIndex String perpetualTaskId;
   String lastFailureReason;
   long initialFailureAt;
 }
