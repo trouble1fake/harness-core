5cf3e50b71cd014d12b9bf08ad2abb6871fe09aa
Naman Talaycha <naman.talaycha@harness.io>
2021-10-03 23:25:22 +0530

    [CDP-11856]: Kustmize Patch support

---
+++ 127-cd-nextgen-beans/src/main/java/io/harness/cdng/manifest/yaml/KustomizePatchesManifestOutcome.java
@@ -0,0 +1,24 @@
+package io.harness.cdng.manifest.yaml;
+
+import static io.harness.annotations.dev.HarnessTeam.CDP;
+
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.cdng.manifest.ManifestType;
+import io.harness.cdng.manifest.yaml.storeConfig.StoreConfig;
+
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import lombok.Builder;
+import lombok.Value;
+import org.springframework.data.annotation.TypeAlias;
+
+@Value
+@Builder
+@TypeAlias("kustomizeManifestOutcome")
+@JsonTypeName(ManifestType.KustmizePatches)
+@OwnedBy(CDP)
+public class KustomizePatchesManifestOutcome implements ManifestOutcome {
+  String identifier;
+  String type = ManifestType.KustmizePatches;
+  StoreConfig store;
+  int order;
+}
