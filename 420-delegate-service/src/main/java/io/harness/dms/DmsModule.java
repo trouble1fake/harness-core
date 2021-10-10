9c47a5ddae98c1bf630780562f0234167e7c339d
Abhinav Singh <abhinav.singh@harness.io>
2021-10-10 20:31:01 +0530

    [DEL-2582]: DMS proxy

---
+++ 420-delegate-service/src/main/java/io/harness/dms/DmsModule.java
@@ -0,0 +1,27 @@
+package io.harness.dms;
+
+import com.google.inject.AbstractModule;
+
+public class DmsModule extends AbstractModule {
+  private static volatile DmsModule instance;
+  private static boolean isDmsMode;
+
+  private DmsModule() {}
+
+  public static DmsModule getInstance(boolean isDmsEnabled) {
+    if (instance == null) {
+      instance = new DmsModule();
+      isDmsMode = isDmsEnabled;
+    }
+    return instance;
+  }
+
+  @Override
+  protected void configure() {
+    if (isDmsMode) {
+      bind(DmsProxy.class).to(DmsProxyDelegateModeImpl.class);
+    } else {
+      bind(DmsProxy.class).to(DmsProxyManagerModeImpl.class);
+    }
+  }
+}
