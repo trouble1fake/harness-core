f02146eb3f4e7c3ec5b3d93114c95d26922377cc
Meet Rathod <rathod.meetsatish@harness.io>
2021-10-13 16:44:28 +0530

    made changes to get project name and org name

--- 290-dashboard-service/src/main/java/io/harness/overviewdashboard/dashboardaggregateservice/impl/OverviewDashboardServiceImpl.java
+++ 290-dashboard-service/src/main/java/io/harness/overviewdashboard/dashboardaggregateservice/impl/OverviewDashboardServiceImpl.java
@@ -357,8 +357,9 @@ public class OverviewDashboardServiceImpl implements OverviewDashboardService {
 
   private Map<String, String> getMapOfProjectIdentifierAndProjectName(List<ProjectDTO> listOfAccessibleProject) {
     Map<String, String> mapOfProjectIdentifierAndProjectName = new HashMap<>();
-    listOfAccessibleProject.stream().map(
-        projectDTO -> mapOfProjectIdentifierAndProjectName.put(projectDTO.getIdentifier(), projectDTO.getName()));
+    for(ProjectDTO projectDTO:listOfAccessibleProject){
+      mapOfProjectIdentifierAndProjectName.put(projectDTO.getIdentifier(), projectDTO.getName()));
+    }
     return mapOfProjectIdentifierAndProjectName;
   }
 
