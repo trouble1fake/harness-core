8b3985db10cb700997c6a5019cc847e347ede90d
Shivakumar Ningappa <shivakumar.ningappa@harness.io>
2021-10-19 07:42:15 -0700

    [CI-0]: test guice injection callgraph. testing PR:27158 that broke master and PR:27163 that fixed it

--- 950-delegate-tasks-beans/src/main/java/software/wings/service/impl/aws/model/AwsElbListAppElbsRequest.java
+++ 950-delegate-tasks-beans/src/main/java/software/wings/service/impl/aws/model/AwsElbListAppElbsRequest.java
@@ -2,9 +2,7 @@ package software.wings.service.impl.aws.model;
 
 import static io.harness.annotations.dev.HarnessTeam.CDP;
 
-import io.harness.annotations.dev.HarnessModule;
 import io.harness.annotations.dev.OwnedBy;
-import io.harness.annotations.dev.TargetModule;
 import io.harness.security.encryption.EncryptedDataDetail;
 
 import software.wings.beans.AwsConfig;
@@ -16,7 +14,6 @@ import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
-@TargetModule(HarnessModule._950_DELEGATE_TASKS_BEANS)
 @OwnedBy(CDP)
 public class AwsElbListAppElbsRequest extends AwsElbRequest {
   @Builder
