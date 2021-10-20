c6154ed39ddb95fe8eae553955e9651b08626cdf
Mohit Garg <mohit.garg@harness.io>
2021-10-20 12:00:22 +0530

    [PL-13917]: init commit - added aws encryption sdk, created feature flag for local encryption

--- 890-sm-core/src/main/java/io/harness/encryptors/clients/LocalEncryptor.java
+++ 890-sm-core/src/main/java/io/harness/encryptors/clients/LocalEncryptor.java
@@ -4,16 +4,27 @@ import static io.harness.annotations.dev.HarnessTeam.PL;
 import static io.harness.data.structure.EmptyPredicate.isEmpty;
 
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.beans.FeatureName;
 import io.harness.data.structure.UUIDGenerator;
 import io.harness.encryptors.KmsEncryptor;
+import io.harness.ff.FeatureFlagService;
 import io.harness.security.SimpleEncryption;
+import io.harness.security.encryption.AdditionalMetadata;
 import io.harness.security.encryption.EncryptedRecord;
 import io.harness.security.encryption.EncryptedRecordData;
 import io.harness.security.encryption.EncryptionConfig;
 
 import software.wings.beans.LocalEncryptionConfig;
 
+import com.amazonaws.encryptionsdk.AwsCrypto;
+import com.amazonaws.encryptionsdk.CryptoResult;
+import com.amazonaws.encryptionsdk.kms.KmsMasterKey;
+import com.amazonaws.encryptionsdk.kms.KmsMasterKeyProvider;
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
 import javax.validation.executable.ValidateOnExecution;
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,10 +33,24 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @OwnedBy(PL)
 public class LocalEncryptor implements KmsEncryptor {
+  private static final String AWS_LOCAL_ENCRYPTION_ENABLED_WITH_BACKUP = "AWS_LOCAL_ENCRYPTION_ENABLED_WITH_BACKUP";
+  private static final AwsCrypto crypto = AwsCrypto.standard();
+  @Inject private FeatureFlagService featureFlagService;
+
   @Override
   public EncryptedRecord encryptSecret(String accountId, String value, EncryptionConfig encryptionConfig) {
+    final char[] awsEncryptedSecret = getAwsEncryptedSecret(accountId, value);
+    if (featureFlagService.isEnabled(FeatureName.AWS_LOCAL_ENCRYPTION_WITHOUT_BACKWARD_COMP, accountId)) {
+      return EncryptedRecordData.builder().encryptionKey(accountId).encryptedValue(awsEncryptedSecret).build();
+    }
+
     char[] encryptedChars = new SimpleEncryption(accountId).encryptChars(value.toCharArray());
-    return EncryptedRecordData.builder().encryptionKey(accountId).encryptedValue(encryptedChars).build();
+    return EncryptedRecordData.builder()
+        .encryptionKey(accountId)
+        .encryptedValue(awsEncryptedSecret)
+        .backupEncryptedValue(encryptedChars)
+        .additionalMetadata(AdditionalMetadata.builder().value(AWS_LOCAL_ENCRYPTION_ENABLED_WITH_BACKUP, true).build())
+        .build();
   }
 
   @Override
@@ -33,6 +58,21 @@ public class LocalEncryptor implements KmsEncryptor {
     if (isEmpty(encryptedRecord.getEncryptionKey())) {
       return null;
     }
+
+    if (featureFlagService.isEnabled(FeatureName.AWS_LOCAL_ENCRYPTION_WITHOUT_BACKWARD_COMP, accountId)) {
+      return getAwsDecryptedSecret(accountId, new String(encryptedRecord.getEncryptedValue()).getBytes()).toCharArray();
+    }
+
+    if (encryptedRecord.getBackupEncryptedValue() != null) {
+      // The record has both aws encrypted secret as well as locally encrypted secret, try for both
+      try {
+        return getAwsDecryptedSecret(accountId, new String(encryptedRecord.getEncryptedValue()).getBytes())
+            .toCharArray();
+      } catch (Exception exception) {
+        // unable to decrypt via aws encryption sdk
+      }
+    }
+
     final SimpleEncryption simpleEncryption = new SimpleEncryption(encryptedRecord.getEncryptionKey());
     return simpleEncryption.decryptChars(encryptedRecord.getEncryptedValue());
   }
@@ -50,5 +90,23 @@ public class LocalEncryptor implements KmsEncryptor {
     }
     log.info("Validating Local KMS configuration End {}", encryptionConfig.getName());
     return true;
+  }
+
+  // ------------------------------ PRIVATE METHODS -----------------------------
+
+  private char[] getAwsEncryptedSecret(String accountId, String value) {
+    final KmsMasterKeyProvider prov = KmsMasterKeyProvider.builder().buildStrict(accountId);
+    final Map<String, String> context = Collections.singletonMap("accountId", accountId);
+    final byte[] encryptedBytes = crypto.encryptData(prov, value.getBytes(), context).getResult();
+    return new String(encryptedBytes).toCharArray();
+  }
+
+  private String getAwsDecryptedSecret(String accountId, byte[] encryptedSecret) {
+    final KmsMasterKeyProvider prov = KmsMasterKeyProvider.builder().buildStrict(accountId);
+    final CryptoResult<byte[], KmsMasterKey> decryptResult = crypto.decryptData(prov, encryptedSecret);
+    if (!decryptResult.getMasterKeyIds().get(0).equals(accountId)) {
+      // throw exception
+    }
+    return Arrays.toString(decryptResult.getResult());
   }
 }
