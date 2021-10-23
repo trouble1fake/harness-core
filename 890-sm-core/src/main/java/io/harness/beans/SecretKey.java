ac6694f9778631c44cb181b574c23bf8f110eed7
Mohit Garg <mohit.garg@harness.io>
2021-10-24 01:48:16 +0530

    [PL-13917]: added secret key repository

--- 890-sm-core/src/main/java/io/harness/beans/SecretKey.java
+++ 890-sm-core/src/main/java/io/harness/beans/SecretKey.java
@@ -3,7 +3,6 @@ package io.harness.beans;
 import static io.harness.annotations.dev.HarnessTeam.PL;
 
 import io.harness.annotations.dev.OwnedBy;
-import io.harness.mongo.index.FdUniqueIndex;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
@@ -11,6 +10,7 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.FieldNameConstants;
 import lombok.experimental.SuperBuilder;
 import org.mongodb.morphia.annotations.Entity;
+import org.springframework.data.annotation.Id;
 
 @OwnedBy(PL)
 @Data
@@ -20,6 +20,6 @@ import org.mongodb.morphia.annotations.Entity;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Entity(value = "SecretKey", noClassnameStored = true)
 public class SecretKey {
-  @FdUniqueIndex private String uuid;
+  @Id @org.mongodb.morphia.annotations.Id private String uuid;
   private String key;
 }
