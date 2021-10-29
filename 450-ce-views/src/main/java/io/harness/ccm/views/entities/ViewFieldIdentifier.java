c9c7031d5587488ae51af283d6f4c0b867c407ca
Utsav Krishnan <utsav.krishnan@harness.io>
2021-10-29 17:18:04 +0530

    [CCM-4999]: finish TODO item as well

--- 450-ce-views/src/main/java/io/harness/ccm/views/entities/ViewFieldIdentifier.java
+++ 450-ce-views/src/main/java/io/harness/ccm/views/entities/ViewFieldIdentifier.java
@@ -2,7 +2,7 @@ package io.harness.ccm.views.entities;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "//TODO")
+@Schema(description = "Perspective filter Category, CLUSTER means Kubernetes")
 public enum ViewFieldIdentifier {
   CLUSTER("Cluster"),
   AWS("AWS"),
