0d10e792b7f0a411fb7963bad043a22306860f18
Jamie Li <tianyi.li@harness.io>
2021-11-01 20:21:30 -0400

    [CI-2651]: fix total build type

--- 310-ci-manager/src/main/java/io/harness/core/ci/services/CITotalBuildImpl.java
+++ 310-ci-manager/src/main/java/io/harness/core/ci/services/CITotalBuildImpl.java
@@ -1,15 +1,15 @@
 package io.harness.core.ci.services;
 
-import io.harness.enforcement.beans.metadata.RateLimitRestrictionMetadataDTO;
+import io.harness.enforcement.beans.metadata.StaticLimitRestrictionMetadataDTO;
 import io.harness.enforcement.client.usage.RestrictionUsageInterface;
 
 import com.google.inject.Inject;
 
-public class CITotalBuildImpl implements RestrictionUsageInterface<RateLimitRestrictionMetadataDTO> {
+public class CITotalBuildImpl implements RestrictionUsageInterface<StaticLimitRestrictionMetadataDTO> {
   @Inject CIOverviewDashboardService ciOverviewDashboardService;
 
   @Override
-  public long getCurrentValue(String accountIdentifier, RateLimitRestrictionMetadataDTO restrictionMetadataDTO) {
+  public long getCurrentValue(String accountIdentifier, StaticLimitRestrictionMetadataDTO restrictionMetadataDTO) {
     return ciOverviewDashboardService.getTotalBuild(accountIdentifier).getCount();
   }
 }
