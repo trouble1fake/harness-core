5f7878fc6b245c2fc51489306640dd9c18697fc0
Utkarsh Gupta <utkarsh.gupta@harness.io>
2021-11-03 16:19:01 +0530

    [PL-0]: Improve swagger to NextGen Application

--- 120-ng-manager/src/main/java/io/harness/ng/NextGenConfiguration.java
+++ 120-ng-manager/src/main/java/io/harness/ng/NextGenConfiguration.java
@@ -166,7 +166,98 @@ public class NextGenConfiguration extends Configuration {
   //  @JsonProperty("secondary-mongo") MongoConfig secondaryMongoConfig;
 
   public SwaggerBundleConfiguration getSwaggerBundleConfiguration() {
-    SwaggerBundleConfiguration defaultSwaggerBundleConfiguration = new SwaggerBundleConfiguration();
+    SwaggerBundleConfiguration defaultSwaggerBundleConfiguration = new SwaggerBundleConfiguration() {
+      @JsonIgnore
+      public BeanConfig build(String urlPattern) {
+        if (Strings.isNullOrEmpty(getResourcePackage())) {
+          throw new IllegalStateException(
+              "Resource package needs to be specified for Swagger to correctly detect annotated resources");
+        }
+
+        final BeanConfig config = new BeanConfig() {
+          @Override
+          public Set<Class<?>> classes() {
+            ConfigurationBuilder config = new ConfigurationBuilder();
+            Set<String> acceptablePackages = new HashSet<String>();
+
+            boolean allowAllPackages = false;
+
+            final FilterBuilder filter = new FilterBuilder();
+            if (getResourcePackage() != null && !"".equals(getResourcePackage())) {
+              String[] parts = getResourcePackage().split(",");
+              for (String pkg : parts) {
+                if (!"".equals(pkg)) {
+                  acceptablePackages.add(pkg);
+                  filter.includePackage(pkg);
+                  config.addUrls(ClasspathHelper.forPackage(pkg));
+                }
+              }
+            } else {
+              allowAllPackages = true;
+            }
+
+            config.setExpandSuperTypes(getExpandSuperTypes());
+            config.filterInputsBy(filter);
+            config.setScanners(new ResourcesScanner(), new TypeAnnotationsScanner(), new SubTypesScanner());
+
+            Set<Class<?>> classes = new HashSet<>(getResourceClasses());
+
+            /*
+             * Find concrete types annotated with @Api, but with a supertype annotated with @Path.
+             * This would handle split resources where the interface has jax-rs annotations
+             * and the implementing class has Swagger annotations
+             */
+
+            Set<Class<?>> output = new HashSet<Class<?>>();
+            for (Class<?> cls : classes) {
+              if (allowAllPackages) {
+                output.add(cls);
+              } else {
+                for (String pkg : acceptablePackages) {
+                  // startsWith allows everything within a package
+                  // the dots ensures that package siblings are not considered
+                  if ((cls.getPackage().getName() + ".").startsWith(pkg + ".")) {
+                    output.add(cls);
+                    break;
+                  }
+                }
+              }
+            }
+            return output;
+          }
+        };
+        config.setTitle(getTitle());
+        config.setVersion(getVersion());
+        config.setDescription(getDescription());
+        config.setContact(getContact());
+        config.setLicense(getLicense());
+        config.setLicenseUrl(getLicenseUrl());
+        config.setTermsOfServiceUrl(getTermsOfServiceUrl());
+        config.setPrettyPrint(isPrettyPrint());
+        config.setBasePath(("/".equals(getContextRoot()) ? "" : getContextRoot()) + urlPattern);
+        config.setResourcePackage(getResourcePackage());
+        config.setSchemes(getSchemes());
+        config.setHost(getHost());
+        config.setScan(true);
+
+        // Assign contact email/url after scan, since BeanConfig.scan will
+        // create a new info.Contact instance, thus overriding any info.Contact
+        // settings prior to scan.
+        if (getContactEmail() != null || getContactUrl() != null) {
+          if (config.getInfo().getContact() == null) {
+            config.getInfo().setContact(new Contact());
+          }
+          if (getContactEmail() != null) {
+            config.getInfo().getContact().setEmail(getContactEmail());
+          }
+          if (getContactUrl() != null) {
+            config.getInfo().getContact().setUrl(getContactUrl());
+          }
+        }
+
+        return config;
+      }
+    };
 
     String resourcePackage = String.join(",", getUniquePackages(getResourceClasses()));
     defaultSwaggerBundleConfiguration.setResourcePackage(resourcePackage);
