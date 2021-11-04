83fff49eda45d9e5b1362125afafbf17138d56de
Sagnik De <sagnik.de@harness.io>
2021-11-04 18:21:31 -0400

    feat: [GTM-524]: stylefix

--- 920-ng-signup/src/main/java/io/harness/signup/resources/SignupResource.java
+++ 920-ng-signup/src/main/java/io/harness/signup/resources/SignupResource.java
@@ -23,7 +23,13 @@ import org.apache.commons.lang3.StringUtils;
 import javax.annotation.Nullable;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.NotNull;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
