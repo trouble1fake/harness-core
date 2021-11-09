b4c44a7a75b2ea61fd2e858e8e8f6a50264d4fd1
Mounik V V S S <mounik.velavalivenkata@harness.io>
2021-11-09 22:36:09 +0530

    [CDC-14699]: Added OutputVariableSupport For Jira and Approval Steps

---
+++ 860-orchestration-steps/src/main/java/io/harness/steps/approval/ApprovalStepVariableCreator.java
@@ -0,0 +1,47 @@
+package io.harness.steps.approval;
+
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.pms.contracts.plan.YamlOutputProperties;
+import io.harness.pms.contracts.plan.YamlProperties;
+import io.harness.pms.sdk.core.pipeline.variables.GenericStepVariableCreator;
+import io.harness.pms.sdk.core.pipeline.variables.VariableCreatorHelper;
+import io.harness.pms.yaml.YAMLFieldNameConstants;
+import io.harness.pms.yaml.YamlField;
+import io.harness.pms.yaml.YamlNode;
+import io.harness.steps.StepSpecTypeConstants;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@OwnedBy(HarnessTeam.CDC)
+public class ApprovalStepVariableCreator extends GenericStepVariableCreator {
+  @Override
+  public Set<String> getSupportedStepTypes() {
+    Set<String> strings = new HashSet<>();
+    strings.add(StepSpecTypeConstants.HARNESS_APPROVAL);
+    return strings;
+  }
+
+  @Override
+  protected void addVariablesInComplexObject(Map<String, YamlProperties> yamlPropertiesMap,
+      Map<String, YamlOutputProperties> yamlOutputPropertiesMap, YamlNode yamlNode) {
+    List<String> complexFields = new ArrayList<>();
+    complexFields.add(YAMLFieldNameConstants.OUTPUT_VARIABLES);
+
+    List<YamlField> fields = yamlNode.fields();
+    fields.forEach(field -> {
+      if (!field.getName().equals(YAMLFieldNameConstants.UUID) && !complexFields.contains(field.getName())) {
+        addFieldToPropertiesMapUnderStep(field, yamlPropertiesMap);
+      }
+    });
+
+    YamlField outputVariablesField = yamlNode.getField(YAMLFieldNameConstants.OUTPUT_VARIABLES);
+    if (VariableCreatorHelper.isNotYamlFieldEmpty(outputVariablesField)) {
+      addVariablesForOutputVariables(outputVariablesField, yamlOutputPropertiesMap);
+    }
+  }
+}
---
+++ 860-orchestration-steps/src/main/java/io/harness/steps/jira/JiraVariableCreator.java
@@ -0,0 +1,42 @@
+package io.harness.steps.jira;
+
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.pms.contracts.plan.YamlOutputProperties;
+import io.harness.pms.contracts.plan.YamlProperties;
+import io.harness.pms.sdk.core.pipeline.variables.GenericStepVariableCreator;
+import io.harness.pms.sdk.core.pipeline.variables.VariableCreatorHelper;
+import io.harness.pms.yaml.YAMLFieldNameConstants;
+import io.harness.pms.yaml.YamlField;
+import io.harness.pms.yaml.YamlNode;
+import io.harness.steps.StepSpecTypeConstants;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@OwnedBy(HarnessTeam.CDC)
+public class JiraVariableCreator extends GenericStepVariableCreator {
+  @Override
+  public Set<String> getSupportedStepTypes() {
+    Set<String> strings = new HashSet<>();
+    strings.add(StepSpecTypeConstants.JIRA_CREATE);
+    strings.add(StepSpecTypeConstants.JIRA_UPDATE);
+    strings.add(StepSpecTypeConstants.JIRA_APPROVAL);
+    return strings;
+  }
+
+  @Override
+  protected void addVariablesInComplexObject(Map<String, YamlProperties> yamlPropertiesMap,
+      Map<String, YamlOutputProperties> yamlOutputPropertiesMap, YamlNode yamlNode) {
+    List<String> complexFields = new ArrayList<>();
+    complexFields.add(YAMLFieldNameConstants.OUTPUT_VARIABLES);
+
+    YamlField outputVariablesField = yamlNode.getField(YAMLFieldNameConstants.OUTPUT_VARIABLES);
+    if (VariableCreatorHelper.isNotYamlFieldEmpty(outputVariablesField)) {
+      addVariablesForOutputVariables(outputVariablesField, yamlOutputPropertiesMap);
+    }
+  }
+}
