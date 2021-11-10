034ae65166f416f75b4dab666dc2a70d6efb4888
Meet Rathod <rathod.meetsatish@harness.io>
2021-11-10 11:31:37 +0530

    logging to appd enable

--- dockerization/ng-dashboard-service/scripts/start_process.sh
+++ dockerization/ng-dashboard-service/scripts/start_process.sh
@@ -12,25 +12,33 @@ if [[ -z "$MEMORY" ]]; then
 fi
 
 echo "Using memory " $MEMORY
+echo "memory display complete"
 
 if [[ -z "$COMMAND" ]]; then
    export COMMAND=server
 fi
 
+echo "COMMAND=server complete"
+
 if [[ -z "$CAPSULE_JAR" ]]; then
    export CAPSULE_JAR=/opt/harness/ng-dashboard-service.jar
 fi
 
+echo "capsulejar complete"
+
 export JAVA_OPTS="-Xms${MEMORY}m -Xmx${MEMORY}m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:mygclogfilename.gc -XX:+UseParallelGC -XX:MaxGCPauseMillis=500 -Dfile.encoding=UTF-8"
 
+echo "export java_opt complete"
 
 if [[ "${DEPLOY_MODE}" == "KUBERNETES" ]] || [[ "${DEPLOY_MODE}" == "KUBERNETES_ONPREM" ]]; then
     java $JAVA_OPTS -jar $CAPSULE_JAR $COMMAND /opt/harness/config.yml
 else
     java $JAVA_OPTS -jar $CAPSULE_JAR $COMMAND /opt/harness/config.yml > /opt/harness/logs/ng-dashboard-service.log 2>&1
 fi
+
 echo "enable_appD is"
 echo $ENABLE_APPDYNAMICS
+
 if [[ "${ENABLE_APPDYNAMICS}" == "true" ]]; then
     echo "enable_appD=true"
     mkdir /opt/harness/AppServerAgent-20.8.0.30686 && unzip AppServerAgent-20.8.0.30686.zip -d /opt/harness/AppServerAgent-20.8.0.30686
