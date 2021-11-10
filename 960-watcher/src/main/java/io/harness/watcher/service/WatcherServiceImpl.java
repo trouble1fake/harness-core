11f77513926894d80878f4f563ee8c9316bc07a5
Jenny James <jenny.james@harness.io>
2021-11-10 14:12:20 -0800

    [DEL-2744]: Fix on versionInfo yaml file read error on watcher upgrade

--- 960-watcher/src/main/java/io/harness/watcher/service/WatcherServiceImpl.java
+++ 960-watcher/src/main/java/io/harness/watcher/service/WatcherServiceImpl.java
@@ -1451,7 +1451,7 @@ public class WatcherServiceImpl implements WatcherService {
 
   @VisibleForTesting
   String getVersion() {
-    return (new VersionInfoManager()).getVersionInfo().getFullVersion();
+    return new VersionInfoManager().getFullVersion();
   }
 
   private void migrate(String newUrl) {
