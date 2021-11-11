8cf562374c8da25bb7e5a67cc181e6e912bd9334
Jamie Li <tianyi.li@harness.io>
2021-11-11 00:27:23 -0500

    [CI-2651]: add test cases

--- 800-pipeline-service/src/test/java/io/harness/repositories/executions/AccountExecutionMetadataRepositoryCustomImplTest.java
+++ 800-pipeline-service/src/test/java/io/harness/repositories/executions/AccountExecutionMetadataRepositoryCustomImplTest.java
@@ -1,5 +1,6 @@
 package io.harness.repositories.executions;
 
+import static io.harness.rule.OwnerRule.JAMIE;
 import static io.harness.rule.OwnerRule.SAHIL;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,7 +26,8 @@ import org.mockito.Mockito;
 @OwnedBy(HarnessTeam.PIPELINE)
 public class AccountExecutionMetadataRepositoryCustomImplTest extends PipelineServiceTestBase {
   private static final String ACCOUNT_ID = "accountId";
-
+  private static final String CI = "ci";
+  private static final String PRIVATE_REPO_BUILD_CI = "ci_private_build";
   @Inject PersistentLocker persistentLocker;
   @Inject AccountExecutionMetadataRepository accountExecutionMetadataRepository;
 
@@ -41,5 +43,24 @@ public class AccountExecutionMetadataRepositoryCustomImplTest extends PipelineSe
         accountExecutionMetadataRepository.findByAccountId(ACCOUNT_ID);
     assertThat(accountExecutionMetadata.isPresent()).isTrue();
     assertThat(accountExecutionMetadata.get().getModuleToExecutionCount().get("cd")).isEqualTo(1L);
+  }
+
+  @Test
+  @Owner(developers = JAMIE)
+  @Category(UnitTests.class)
+  @RealMongo
+  public void testUpdateAccountExecutionMetadataCIPrivateRepo() {
+    Mockito.when(persistentLocker.tryToAcquireLock(any(), any())).thenReturn(AcquiredDistributedLock.builder().build());
+    accountExecutionMetadataRepository.updateAccountExecutionMetadata(
+        ACCOUNT_ID, Sets.newHashSet(PRIVATE_REPO_BUILD_CI, CI), 1636606650379L);
+    Optional<AccountExecutionMetadata> accountExecutionMetadata =
+        accountExecutionMetadataRepository.findByAccountId(ACCOUNT_ID);
+    assertThat(accountExecutionMetadata.isPresent()).isTrue();
+    assertThat(accountExecutionMetadata.get().getModuleToExecutionCount().get(CI)).isEqualTo(1L);
+    assertThat(accountExecutionMetadata.get().getModuleToExecutionInfoMap().get(CI).getCountPerMonth().getOrDefault(
+                   "2021-11", 0L))
+        .isEqualTo(1L);
+    assertThat(accountExecutionMetadata.get().getModuleToExecutionCount().get(PRIVATE_REPO_BUILD_CI)).isEqualTo(1L);
+    assertThat(accountExecutionMetadata.get().getModuleToExecutionInfoMap().get(PRIVATE_REPO_BUILD_CI)).isEqualTo(null);
   }
 }
