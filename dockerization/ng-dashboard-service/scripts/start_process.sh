4b40555711585a7793c0fb41354157c11f034a5a
Meet Rathod <rathod.meetsatish@harness.io>
2021-11-12 14:41:13 +0530

    added appd configs

--- dockerization/ng-dashboard-service/scripts/start_process.sh
+++ dockerization/ng-dashboard-service/scripts/start_process.sh
@@ -29,6 +29,16 @@ fi
 
 export JAVA_OPTS="-Xms${MEMORY}m -Xmx${MEMORY}m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:mygclogfilename.gc $GC_PARAMS"
 
+if [[ "${ENABLE_APPDYNAMICS}" == "true" ]]; then
+    echo "enable_appD=true"
+    mkdir /opt/harness/AppServerAgent-20.8.0.30686 && unzip AppServerAgent-20.8.0.30686.zip -d /opt/harness/AppServerAgent-20.8.0.30686
+    echo "unzip complete"
+    node_name="-Dappdynamics.agent.nodeName=$(hostname)"
+    echo "node name added"
+    JAVA_OPTS=$JAVA_OPTS" -javaagent:/opt/harness/AppServerAgent-20.8.0.30686/javaagent.jar -Dappdynamics.jvm.shutdown.mark.node.as.historical=true"
+    JAVA_OPTS="$JAVA_OPTS $node_name"
+    echo "Using Appdynamics java agent"
+fi
 
 if [[ "${DEPLOY_MODE}" == "KUBERNETES" ]] || [[ "${DEPLOY_MODE}" == "KUBERNETES_ONPREM" ]]; then
     java $JAVA_OPTS -jar $CAPSULE_JAR $COMMAND /opt/harness/config.yml
