53bdcb0ae335f880caed03aac264f13eeefdfaa3
Kamal Joshi <kamal.joshi@harness.io>
2021-11-12 16:23:30 +0530

    [CVNG-3792]: memory optimization by making service clients singleton

--- 300-cv-nextgen/src/main/java/io/harness/cvng/core/jobs/ConnectorChangeEventMessageProcessor.java
+++ 300-cv-nextgen/src/main/java/io/harness/cvng/core/jobs/ConnectorChangeEventMessageProcessor.java
@@ -1,34 +1,26 @@
 package io.harness.cvng.core.jobs;
 
-import io.harness.beans.IdentifierRef;
-import io.harness.cvng.activity.source.services.api.KubernetesActivitySourceService;
-import io.harness.cvng.core.entities.MonitoringSourcePerpetualTask;
-import io.harness.cvng.core.services.api.CVConfigService;
-import io.harness.cvng.core.services.api.DataCollectionTaskService;
-import io.harness.cvng.core.services.api.MonitoringSourcePerpetualTaskService;
-import io.harness.cvng.core.services.api.VerificationTaskService;
-import io.harness.eventsframework.EventsFrameworkMetadataConstants;
-import io.harness.eventsframework.consumer.Message;
-import io.harness.eventsframework.entity_crud.EntityChangeDTO;
-import io.harness.utils.IdentifierRefHelper;
-
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.google.protobuf.InvalidProtocolBufferException;
+import io.harness.beans.IdentifierRef;
+import io.harness.cvng.core.entities.MonitoringSourcePerpetualTask;
+import io.harness.cvng.core.services.api.MonitoringSourcePerpetualTaskService;
+import io.harness.eventsframework.EventsFrameworkMetadataConstants;
+import io.harness.eventsframework.consumer.Message;
+import io.harness.eventsframework.entity_crud.EntityChangeDTO;
+import io.harness.utils.IdentifierRefHelper;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.List;
 import java.util.Map;
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Singleton
 public class ConnectorChangeEventMessageProcessor implements ConsumerMessageProcessor {
-  @Inject private CVConfigService cvConfigService;
   @Inject private MonitoringSourcePerpetualTaskService monitoringSourcePerpetualTaskService;
-  @Inject private KubernetesActivitySourceService kubernetesActivitySourceService;
-  @Inject private DataCollectionTaskService dataCollectionTaskService;
-  @Inject private VerificationTaskService verificationTaskService;
   @Override
   public void processMessage(Message message) {
     Preconditions.checkState(validateMessage(message), "Invalid message received by Connector Change Event Processor");
