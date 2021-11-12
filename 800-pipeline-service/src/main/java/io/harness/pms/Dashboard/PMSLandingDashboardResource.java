ddc0d21f00f14c5d7dfc4c3c5ee3496de9b7211b
Vaibhav Singhal <vaibhav.si@harness.io>
2021-11-12 12:45:33 +0530

    [CDNG-14399]: changed landing dashboardd apis from get to post

--- 800-pipeline-service/src/main/java/io/harness/pms/Dashboard/PMSLandingDashboardResource.java
+++ 800-pipeline-service/src/main/java/io/harness/pms/Dashboard/PMSLandingDashboardResource.java
@@ -7,11 +7,11 @@ import io.harness.NGResourceFilterConstants;
 import io.harness.accesscontrol.AccountIdentifier;
 import io.harness.accesscontrol.NGAccessControlCheck;
 import io.harness.annotations.dev.OwnedBy;
-import io.harness.ng.core.OrgProjectIdentifier;
 import io.harness.ng.core.dto.ErrorDTO;
 import io.harness.ng.core.dto.FailureDTO;
 import io.harness.ng.core.dto.ResponseDTO;
 import io.harness.pms.annotations.PipelineServiceAuth;
+import io.harness.pms.dashboards.LandingDashboardRequest;
 import io.harness.pms.dashboards.PipelinesCount;
 
 import com.google.inject.Inject;
@@ -19,10 +19,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import java.util.List;
-import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -46,16 +44,15 @@ import lombok.extern.slf4j.Slf4j;
 public class PMSLandingDashboardResource {
   private final PMSLandingDashboardService pmsLandingDashboardService;
 
-  @GET
+  @POST
   @Path("/pipelinesCount")
   @ApiOperation(value = "Get pipelines count", nickname = "getPipelinesCount")
   @NGAccessControlCheck(resourceType = "ACCOUNT", permission = "core_account_view")
   public ResponseDTO<PipelinesCount> getPipelinesCount(
-      @NotNull @QueryParam(NGCommonEntityConstants.ACCOUNT_KEY) @AccountIdentifier String accountIdentifier,
-      @NotNull @QueryParam("orgProjectIdentifiers") List<OrgProjectIdentifier> orgProjectIdentifiers,
-      @NotNull @QueryParam(NGResourceFilterConstants.START_TIME) long startInterval,
-      @NotNull @QueryParam(NGResourceFilterConstants.END_TIME) long endInterval) {
+          @NotNull @QueryParam(NGCommonEntityConstants.ACCOUNT_KEY) @AccountIdentifier String accountIdentifier,
+          @NotNull @QueryParam(NGResourceFilterConstants.START_TIME) long startInterval,
+          @NotNull @QueryParam(NGResourceFilterConstants.END_TIME) long endInterval, @NotNull LandingDashboardRequest landingDashboardRequest) {
     return ResponseDTO.newResponse(pmsLandingDashboardService.getPipelinesCount(
-        accountIdentifier, orgProjectIdentifiers, startInterval, endInterval));
+        accountIdentifier, landingDashboardRequest.getOrgProjectIdentifiers(), startInterval, endInterval));
   }
 }
