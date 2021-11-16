8beaeb24518ad19ceda1d33476d44f3f82e3d0bc
Karan Siwach <karan.siwach@harness.io>
2021-11-16 10:19:40 +0530

    [PL-20944]: initial changes

--- 123-ng-core-user/src/main/java/io/harness/ng/core/invites/mapper/RoleBindingMapper.java
+++ 123-ng-core-user/src/main/java/io/harness/ng/core/invites/mapper/RoleBindingMapper.java
@@ -2,6 +2,7 @@ package io.harness.ng.core.invites.mapper;
 
 import static io.harness.annotations.dev.HarnessTeam.PL;
 import static io.harness.data.structure.EmptyPredicate.isEmpty;
+import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
 
 import static java.util.stream.Collectors.toList;
 import static org.apache.commons.lang3.StringUtils.isBlank;
@@ -10,6 +11,7 @@ import io.harness.accesscontrol.principals.PrincipalDTO;
 import io.harness.accesscontrol.principals.PrincipalType;
 import io.harness.accesscontrol.roleassignments.api.RoleAssignmentDTO;
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.beans.Scope;
 import io.harness.ng.core.invites.dto.RoleBinding;
 
 import java.util.ArrayList;
@@ -20,9 +22,6 @@ import lombok.experimental.UtilityClass;
 @OwnedBy(PL)
 @UtilityClass
 public class RoleBindingMapper {
-  private static final String DEFAULT_RESOURCE_GROUP_NAME = "All Resources";
-  private static final String DEFAULT_RESOURCE_GROUP_IDENTIFIER = "_all_resources";
-
   public static io.harness.audit.beans.custom.user.RoleBinding toAuditRoleBinding(RoleBinding roleBinding) {
     return io.harness.audit.beans.custom.user.RoleBinding.builder()
         .roleIdentifier(roleBinding.getRoleIdentifier())
@@ -38,15 +37,18 @@ public class RoleBindingMapper {
     return roleBindings.stream().map(RoleBindingMapper::toAuditRoleBinding).collect(toList());
   }
 
-  public static List<RoleAssignmentDTO> createRoleAssignmentDTOs(List<RoleBinding> roleBindings, String userId) {
+  public static List<RoleAssignmentDTO> createRoleAssignmentDTOs(
+      List<RoleBinding> roleBindings, String userId, Scope scope) {
     if (isEmpty(roleBindings)) {
       return new ArrayList<>();
     }
     return roleBindings.stream()
         .map(roleBinding -> {
           if (isBlank(roleBinding.getResourceGroupIdentifier())) {
-            roleBinding.setResourceGroupIdentifier(DEFAULT_RESOURCE_GROUP_IDENTIFIER);
-            roleBinding.setResourceGroupName(DEFAULT_RESOURCE_GROUP_NAME);
+            roleBinding.setResourceGroupIdentifier(
+                getDefaultResourceGroupIdentifier(scope.getOrgIdentifier(), scope.getProjectIdentifier()));
+            roleBinding.setResourceGroupName(
+                getDefaultResourceGroupName(scope.getOrgIdentifier(), scope.getProjectIdentifier()));
           }
           return RoleAssignmentDTO.builder()
               .roleIdentifier(roleBinding.getRoleIdentifier())
@@ -56,5 +58,25 @@ public class RoleBindingMapper {
               .build();
         })
         .collect(Collectors.toList());
+  }
+
+  public static String getDefaultResourceGroupIdentifier(String orgIdentifier, String projectIdentifier) {
+    if (isNotEmpty(projectIdentifier)) {
+      return "_all_project_resources";
+    } else if (isNotEmpty(orgIdentifier)) {
+      return "_all_organization_resources";
+    } else {
+      return "_all_account_resources";
+    }
+  }
+
+  public String getDefaultResourceGroupName(String orgIdentifier, String projectIdentifier) {
+    if (isNotEmpty(projectIdentifier)) {
+      return "All Project Resources";
+    } else if (isNotEmpty(orgIdentifier)) {
+      return "All Organization Resources";
+    } else {
+      return "All Account Resources";
+    }
   }
 }
