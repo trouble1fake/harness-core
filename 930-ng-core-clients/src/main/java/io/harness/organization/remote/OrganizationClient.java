4aa590e285e386baef810d85e01f1181acaaa540
Meet Rathod <rathod.meetsatish@harness.io>
2021-11-17 15:26:05 +0530

    [PL-20959]: changed to POST

--- 930-ng-core-clients/src/main/java/io/harness/organization/remote/OrganizationClient.java
+++ 930-ng-core-clients/src/main/java/io/harness/organization/remote/OrganizationClient.java
@@ -62,8 +62,8 @@ public interface OrganizationClient {
       @Path(value = NGCommonEntityConstants.IDENTIFIER_KEY) String identifier,
       @Query(value = NGCommonEntityConstants.ACCOUNT_KEY) String accountIdentifier);
 
-  @GET(ORGANIZATIONS_API + "/all-organizations")
+  @POST(ORGANIZATIONS_API + "/all-organizations")
   Call<ResponseDTO<PageResponse<OrganizationResponse>>> listAllOrganizations(
       @Query(value = NGCommonEntityConstants.ACCOUNT_KEY) String accountIdentifier,
-      @Query(value = NGResourceFilterConstants.IDENTIFIERS) List<String> identifiers);
+      @Body List<String> identifiers);
 }
