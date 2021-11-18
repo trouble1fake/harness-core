209979730fa964e0b792cbb5e8b673e9591f1854
Meet Rathod <rathod.meetsatish@harness.io>
2021-11-18 10:48:27 +0530

    fixed unit test

--- 930-ng-core-clients/src/main/java/io/harness/organization/remote/OrganizationClient.java
+++ 930-ng-core-clients/src/main/java/io/harness/organization/remote/OrganizationClient.java
@@ -64,6 +64,5 @@ public interface OrganizationClient {
 
   @POST(ORGANIZATIONS_API + "/all-organizations")
   Call<ResponseDTO<PageResponse<OrganizationResponse>>> listAllOrganizations(
-      @Query(value = NGCommonEntityConstants.ACCOUNT_KEY) String accountIdentifier,
-      @Body List<String> identifiers);
+      @Query(value = NGCommonEntityConstants.ACCOUNT_KEY) String accountIdentifier, @Body List<String> identifiers);
 }
