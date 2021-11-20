185cf0276d9a5e93ec3da995150bd280bca478af
Utkarsh Gupta <utkarsh.gupta@harness.io>
2021-11-20 22:10:09 +0530

    [PL-0]: Disable scan at application startup

--- 120-ng-manager/src/main/java/io/harness/ng/NextGenConfiguration.java
+++ 120-ng-manager/src/main/java/io/harness/ng/NextGenConfiguration.java
@@ -1,5 +1,7 @@
 package io.harness.ng;
 
+import static io.harness.swagger.SwaggerBundleConfigurationFactory.buildSwaggerBundleConfiguration;
+
 import static java.util.stream.Collectors.toSet;
 
 import io.harness.AccessControlClientConfiguration;
@@ -36,17 +38,11 @@ import io.harness.telemetry.segment.SegmentConfiguration;
 import io.harness.threading.ThreadPoolConfig;
 import io.harness.timescaledb.TimeScaleDBConfig;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.api.client.util.Strings;
 import com.google.common.collect.Lists;
 import io.dropwizard.Configuration;
 import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
-import io.swagger.jaxrs.config.BeanConfig;
-import io.swagger.jaxrs.config.SwaggerContextService;
-import io.swagger.models.Contact;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -56,12 +52,6 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.reflections.Reflections;
-import org.reflections.scanners.ResourcesScanner;
-import org.reflections.scanners.SubTypesScanner;
-import org.reflections.scanners.TypeAnnotationsScanner;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.ConfigurationBuilder;
-import org.reflections.util.FilterBuilder;
 
 @Getter
 @OwnedBy(HarnessTeam.PL)
@@ -170,115 +160,8 @@ public class NextGenConfiguration extends Configuration {
   //  @JsonProperty("secondary-mongo") MongoConfig secondaryMongoConfig;
 
   public SwaggerBundleConfiguration getSwaggerBundleConfiguration() {
-    SwaggerBundleConfiguration defaultSwaggerBundleConfiguration = new SwaggerBundleConfiguration() {
-      @JsonIgnore
-      public BeanConfig build(String urlPattern) {
-        if (Strings.isNullOrEmpty(getResourcePackage())) {
-          throw new IllegalStateException(
-              "Resource package needs to be specified for Swagger to correctly detect annotated resources");
-        }
-
-        String resourcePackage = String.join(",", getUniquePackages(getResourceClasses()));
-        final BeanConfig config = new BeanConfig() {
-          @Override
-          public Set<Class<?>> classes() {
-            ConfigurationBuilder config = new ConfigurationBuilder();
-            Set<String> acceptablePackages = new HashSet<String>();
-
-            boolean allowAllPackages = false;
-
-            final FilterBuilder filter = new FilterBuilder();
-            if (getResourcePackage() != null && !"".equals(getResourcePackage())) {
-              String[] parts = getResourcePackage().split(",");
-              for (String pkg : parts) {
-                if (!"".equals(pkg)) {
-                  acceptablePackages.add(pkg);
-                  filter.includePackage(pkg);
-                  config.addUrls(ClasspathHelper.forPackage(pkg));
-                }
-              }
-            } else {
-              allowAllPackages = true;
-            }
-
-            config.setExpandSuperTypes(getExpandSuperTypes());
-            config.filterInputsBy(filter);
-            config.setScanners(new ResourcesScanner(), new TypeAnnotationsScanner(), new SubTypesScanner());
-
-            Set<Class<?>> classes = new HashSet<>(HARNESS_RESOURCE_CLASSES);
-
-            /*
-             * Find concrete types annotated with @Api, but with a supertype annotated with @Path.
-             * This would handle split resources where the interface has jax-rs annotations
-             * and the implementing class has Swagger annotations
-             */
-
-            Set<Class<?>> output = new HashSet<Class<?>>();
-            for (Class<?> cls : classes) {
-              if (allowAllPackages) {
-                output.add(cls);
-              } else {
-                for (String pkg : acceptablePackages) {
-                  // startsWith allows everything within a package
-                  // the dots ensures that package siblings are not considered
-                  if ((cls.getPackage().getName() + ".").startsWith(pkg + ".")) {
-                    output.add(cls);
-                    break;
-                  }
-                }
-              }
-            }
-            return output;
-          }
-
-          @Override
-          public void setScan(boolean shouldScan) {
-            new SwaggerContextService()
-                .withConfigId(this.getConfigId())
-                .withScannerId(this.getScannerId())
-                .withContextId(this.getContextId())
-                .withServletConfig(this.servletConfig)
-                .withSwaggerConfig(this)
-                .withScanner(this)
-                .withBasePath(getBasePath())
-                .withPathBasedConfig(isUsePathBasedConfig())
-                .initConfig()
-                .initScanner();
-          }
-        };
-        config.setTitle(getTitle());
-        config.setVersion(getVersion());
-        config.setDescription(getDescription());
-        config.setContact(getContact());
-        config.setLicense(getLicense());
-        config.setLicenseUrl(getLicenseUrl());
-        config.setTermsOfServiceUrl(getTermsOfServiceUrl());
-        config.setPrettyPrint(isPrettyPrint());
-        config.setBasePath(("/".equals(getContextRoot()) ? "" : getContextRoot()) + urlPattern);
-        config.setResourcePackage(getResourcePackage());
-        config.setSchemes(getSchemes());
-        config.setHost(getHost());
-        config.setScan(true);
-
-        // Assign contact email/url after scan, since BeanConfig.scan will
-        // create a new info.Contact instance, thus overriding any info.Contact
-        // settings prior to scan.
-        if (getContactEmail() != null || getContactUrl() != null) {
-          if (config.getInfo().getContact() == null) {
-            config.getInfo().setContact(new Contact());
-          }
-          if (getContactEmail() != null) {
-            config.getInfo().getContact().setEmail(getContactEmail());
-          }
-          if (getContactUrl() != null) {
-            config.getInfo().getContact().setUrl(getContactUrl());
-          }
-        }
-
-        return config;
-      }
-    };
-
+    SwaggerBundleConfiguration defaultSwaggerBundleConfiguration =
+        buildSwaggerBundleConfiguration(HARNESS_RESOURCE_CLASSES);
     String resourcePackage = String.join(",", getUniquePackages(HARNESS_RESOURCE_CLASSES));
     defaultSwaggerBundleConfiguration.setResourcePackage(resourcePackage);
     defaultSwaggerBundleConfiguration.setSchemes(new String[] {"https", "http"});
@@ -286,7 +169,6 @@ public class NextGenConfiguration extends Configuration {
     defaultSwaggerBundleConfiguration.setUriPrefix(basePathPrefix);
     defaultSwaggerBundleConfiguration.setTitle("CD NextGen API Reference");
     defaultSwaggerBundleConfiguration.setVersion("2.0");
-
     return Optional.ofNullable(swaggerBundleConfiguration).orElse(defaultSwaggerBundleConfiguration);
   }
 
