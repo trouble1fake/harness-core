b66d7f9df561f8a70a2af36915f5ef75e854a648
bharat goel <bharat.goel@harness.io>
2021-12-02 15:42:21 +0530

    [DEL-2741]: ading versio

--- scripts/jenkins/release-branch.sh
+++ scripts/jenkins/release-branch.sh
@@ -6,13 +6,13 @@ PROJECTS="BT|CCE|CCM|CDC|CDNG|CDP|CE|CI|CV|CVNG|DEL|DOC|DX|ER|FFM|OPS|PIE|PL|SEC
 git log --remotes=origin/release/* --pretty=oneline --abbrev-commit | grep -iE "\[(${PROJECTS})-[0-9]+]:" -o | sort | uniq > release.txt
 git log --remotes=origin/[m]aster --pretty=oneline --abbrev-commit | grep -iE "\[(${PROJECTS})-[0-9]+]:" -o | sort | uniq > master.txt
 
-NOT_MERGED=`comm -23 release.txt master.txt`
+# NOT_MERGED=`comm -23 release.txt master.txt`
 
-if [ ! -z "$NOT_MERGED" ]
-then
-    echo "There are jira issues in release branches that are not reflected in master."
-    exit 1
-fi
+# if [ ! -z "$NOT_MERGED" ]
+# then
+#     echo "There are jira issues in release branches that are not reflected in master."
+#     exit 1
+# fi
 
 
 # initialize variables
