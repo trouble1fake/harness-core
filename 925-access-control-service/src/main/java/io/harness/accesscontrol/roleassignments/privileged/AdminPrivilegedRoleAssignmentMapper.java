6189334b399809549fb9a99846ff1403965753cb
Karan Siwach <karan.siwach@harness.io>
2021-12-03 15:42:09 +0530

    [PL-20944]: comments

--- 925-access-control-service/src/main/java/io/harness/accesscontrol/roleassignments/privileged/AdminPrivilegedRoleAssignmentMapper.java
+++ 925-access-control-service/src/main/java/io/harness/accesscontrol/roleassignments/privileged/AdminPrivilegedRoleAssignmentMapper.java
@@ -26,6 +26,7 @@ public class AdminPrivilegedRoleAssignmentMapper {
           .of(new AbstractMap.SimpleEntry<>("_account_admin", "_super_account_admin"),
               new AbstractMap.SimpleEntry<>("_organization_admin", "_super_organization_admin"))
           .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+  public static final String ALL_RESOURCES_RESOURCE_GROUP = "_all_resources";
   public static final List<String> MANAGED_RESOURCE_GROUP_IDENTIFIERS =
       ImmutableList.of(DEFAULT_RESOURCE_GROUP_IDENTIFIER, DEFAULT_ACCOUNT_LEVEL_RESOURCE_GROUP_IDENTIFIER,
           DEFAULT_ORGANIZATION_LEVEL_RESOURCE_GROUP_IDENTIFIER, DEFAULT_PROJECT_LEVEL_RESOURCE_GROUP_IDENTIFIER);
