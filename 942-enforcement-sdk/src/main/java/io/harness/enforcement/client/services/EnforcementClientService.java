6beaba04e086f2c5de81c640989a3a233b261315
Zhuo Yin <zhuo.yin@harness.io>
2021-12-03 16:54:55 -0500

    [GTM-593]: Enable enforcement SDK check by default in Community (#28959)

--- 942-enforcement-sdk/src/main/java/io/harness/enforcement/client/services/EnforcementClientService.java
+++ 942-enforcement-sdk/src/main/java/io/harness/enforcement/client/services/EnforcementClientService.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 import java.util.Set;
 
 public interface EnforcementClientService {
+  boolean isEnforcementEnabled();
   /**
    * Check if available for next feature consume
    * @param featureRestrictionName
