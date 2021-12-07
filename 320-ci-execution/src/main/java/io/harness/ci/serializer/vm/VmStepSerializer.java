cbfde85f1a7d67b439668bc1480b2d0ecd65c059
Shubham Agrawal <shubham.agrawal@harness.io>
2021-12-07 20:45:14 +0530

    fix formatting

--- 320-ci-execution/src/main/java/io/harness/ci/serializer/vm/VmStepSerializer.java
+++ 320-ci-execution/src/main/java/io/harness/ci/serializer/vm/VmStepSerializer.java
@@ -1,11 +1,7 @@
 package io.harness.ci.serializer.vm;
 
-import io.harness.annotations.dev.OwnedBy;
-import io.harness.beans.plugin.compatible.PluginCompatibleStep;
 import io.harness.beans.steps.CIStepInfo;
-import io.harness.beans.steps.stepinfo.PluginStepInfo;
 import io.harness.beans.steps.stepinfo.RunStepInfo;
-import io.harness.beans.steps.stepinfo.RunTestsStepInfo;
 import io.harness.delegate.beans.ci.vm.steps.VmStepInfo;
 import io.harness.pms.yaml.ParameterField;
 import io.harness.yaml.core.timeout.Timeout;
@@ -22,30 +18,6 @@ public class VmStepSerializer {
     switch (stepInfo.getNonYamlInfo().getStepInfoType()) {
       case RUN:
         return vmRunStepSerializer.serialize((RunStepInfo) stepInfo, identifier, parameterFieldTimeout, stepName);
-        //            case PLUGIN:
-        //                return pluginStepProtobufSerializer.serializeStepWithStepParameters((PluginStepInfo)
-        //                ciStepInfo, port, taskId,
-        //                        logKey, stepIdentifier, ParameterField.createValueField(Timeout.fromString(timeout)),
-        //                        accountId, stepName);
-        //            case GCR:
-        //            case DOCKER:
-        //            case ECR:
-        //            case UPLOAD_ARTIFACTORY:
-        //            case UPLOAD_GCS:
-        //            case UPLOAD_S3:
-        //            case SAVE_CACHE_GCS:
-        //            case RESTORE_CACHE_GCS:
-        //            case SAVE_CACHE_S3:
-        //            case RESTORE_CACHE_S3:
-        //                return pluginCompatibleStepSerializer.serializeStepWithStepParameters((PluginCompatibleStep)
-        //                ciStepInfo, port,
-        //                        taskId, logKey, stepIdentifier,
-        //                        ParameterField.createValueField(Timeout.fromString(timeout)), accountId, stepName);
-        //            case RUN_TESTS:
-        //                return runTestsStepProtobufSerializer.serializeStepWithStepParameters((RunTestsStepInfo)
-        //                ciStepInfo, port,
-        //                        taskId, logKey, stepIdentifier,
-        //                        ParameterField.createValueField(Timeout.fromString(timeout)), accountId, stepName);
       case CLEANUP:
       case TEST:
       case BUILD:
