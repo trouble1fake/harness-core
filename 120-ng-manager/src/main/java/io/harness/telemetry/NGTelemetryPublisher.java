35a181a95a5f5b6ecbfea3fe8a79d4f89a7b2479
Ved Tambat <ved.atultambat@harness.io>
2021-12-09 15:17:57 +0530

    [PIE-1957]: community edition check

--- 120-ng-manager/src/main/java/io/harness/telemetry/NGTelemetryPublisher.java
+++ 120-ng-manager/src/main/java/io/harness/telemetry/NGTelemetryPublisher.java
@@ -2,7 +2,6 @@ package io.harness.telemetry;
 
 import com.google.inject.Singleton;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.mongodb.core.query.Criteria;
 
 @Slf4j
 @Singleton
