0fc00ac8a878ddad7b14cf39d2b421ce06ae8789
Mounik V V S S <mounik.velavalivenkata@harness.io>
2021-12-10 12:52:25 +0530

    feat: [CDC-16236]: Case added

--- 400-rest/src/main/java/software/wings/service/impl/WorkflowExecutionUpdate.java
+++ 400-rest/src/main/java/software/wings/service/impl/WorkflowExecutionUpdate.java
@@ -378,9 +378,8 @@ public class WorkflowExecutionUpdate implements StateMachineExecutionCallback {
   }
 
   private PipelineEventData getPipelineEventData(PipelineSummary summary) {
-    if (summary == null) {
+    if (summary == null)
       return null;
-    }
     return PipelineEventData.builder().id(summary.getPipelineId()).name(summary.getPipelineName()).build();
   }
 
