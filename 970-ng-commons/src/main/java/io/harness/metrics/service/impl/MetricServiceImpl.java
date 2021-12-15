e7cdcb0d2a8e70e4151a7a2b074c2ac83505d8e8
Amit Jambure <amit.jambure@harness.io>
2021-12-15 19:26:25 +0530

    [CCM-5292]: opencensus: do not publish default labels
    By default, StackDriverStatsExporter publishes `opencensus_task` label.
    The label messes up with the visualisation of the graphs for `LastValue` type of metrics.
    Configure the StackDriverStatsExporter to not to publish any default labels.

--- 970-ng-commons/src/main/java/io/harness/metrics/service/impl/MetricServiceImpl.java
+++ 970-ng-commons/src/main/java/io/harness/metrics/service/impl/MetricServiceImpl.java
@@ -37,6 +37,7 @@ import java.io.IOException;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -136,6 +137,7 @@ public class MetricServiceImpl implements MetricService {
             StackdriverStatsConfiguration.builder()
                 .setExportInterval(Duration.fromMillis(TimeUnit.MINUTES.toMillis(1)))
                 .setDeadline(Duration.fromMillis(TimeUnit.MINUTES.toMillis(5)))
+                .setConstantLabels(Collections.emptyMap())
                 .build();
         StackdriverStatsExporter.createAndRegister(configuration);
         log.info("StackdriverStatsExporter created");
