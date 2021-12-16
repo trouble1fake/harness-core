88641b82735cf74edf0c8e62afb34dd73db74663
Achyuth Prakash <achyuth.prakash@harness.io>
2021-12-16 23:37:42 +0530

    fix: [CDP-19723]: added storeTypes to CmdFlags API

--- 127-cd-nextgen-beans/src/main/java/io/harness/cdng/manifest/yaml/HelmCommandFlagType.java
+++ 127-cd-nextgen-beans/src/main/java/io/harness/cdng/manifest/yaml/HelmCommandFlagType.java
@@ -1,5 +1,6 @@
 package io.harness.cdng.manifest.yaml;
 
+import io.harness.cdng.manifest.ManifestStoreType;
 import io.harness.cdng.service.beans.ServiceSpecType;
 import io.harness.helm.HelmSubCommandType;
 
@@ -9,23 +10,26 @@ import lombok.Getter;
 
 @Getter
 public enum HelmCommandFlagType {
-  Fetch(HelmSubCommandType.FETCH, ImmutableSet.of(ServiceSpecType.NATIVE_HELM, ServiceSpecType.KUBERNETES)),
-  Version(HelmSubCommandType.VERSION, ImmutableSet.of(ServiceSpecType.NATIVE_HELM, ServiceSpecType.KUBERNETES)),
-  Template(HelmSubCommandType.TEMPLATE, ImmutableSet.of(ServiceSpecType.NATIVE_HELM, ServiceSpecType.KUBERNETES)),
-  Pull(HelmSubCommandType.PULL, ImmutableSet.of(ServiceSpecType.NATIVE_HELM, ServiceSpecType.KUBERNETES)),
-  Install(HelmSubCommandType.INSTALL, ImmutableSet.of(ServiceSpecType.NATIVE_HELM)),
-  Upgrade(HelmSubCommandType.UPGRADE, ImmutableSet.of(ServiceSpecType.NATIVE_HELM)),
-  Rollback(HelmSubCommandType.ROLLBACK, ImmutableSet.of(ServiceSpecType.NATIVE_HELM)),
-  History(HelmSubCommandType.HISTORY, ImmutableSet.of(ServiceSpecType.NATIVE_HELM)),
-  Delete(HelmSubCommandType.DELETE, ImmutableSet.of(ServiceSpecType.NATIVE_HELM)),
-  Uninstall(HelmSubCommandType.UNINSTALL, ImmutableSet.of(ServiceSpecType.NATIVE_HELM)),
-  List(HelmSubCommandType.LIST, ImmutableSet.of(ServiceSpecType.NATIVE_HELM));
+  Fetch(HelmSubCommandType.FETCH, ImmutableSet.of(ServiceSpecType.NATIVE_HELM, ServiceSpecType.KUBERNETES),
+      ManifestStoreType.HelmChartRepo),
+  Template(HelmSubCommandType.TEMPLATE, ImmutableSet.of(ServiceSpecType.NATIVE_HELM, ServiceSpecType.KUBERNETES),
+      ManifestStoreType.HelmNGRepo),
+  Pull(HelmSubCommandType.PULL, ImmutableSet.of(ServiceSpecType.NATIVE_HELM, ServiceSpecType.KUBERNETES),
+      ManifestStoreType.HelmChartRepo),
+  Install(HelmSubCommandType.INSTALL, ImmutableSet.of(ServiceSpecType.NATIVE_HELM), ManifestStoreType.HelmNGRepo),
+  Upgrade(HelmSubCommandType.UPGRADE, ImmutableSet.of(ServiceSpecType.NATIVE_HELM), ManifestStoreType.HelmNGRepo),
+  Rollback(HelmSubCommandType.ROLLBACK, ImmutableSet.of(ServiceSpecType.NATIVE_HELM), ManifestStoreType.HelmNGRepo),
+  History(HelmSubCommandType.HISTORY, ImmutableSet.of(ServiceSpecType.NATIVE_HELM), ManifestStoreType.HelmNGRepo),
+  Delete(HelmSubCommandType.DELETE, ImmutableSet.of(ServiceSpecType.NATIVE_HELM), ManifestStoreType.HelmNGRepo),
+  Uninstall(HelmSubCommandType.UNINSTALL, ImmutableSet.of(ServiceSpecType.NATIVE_HELM), ManifestStoreType.HelmNGRepo),
+  List(HelmSubCommandType.LIST, ImmutableSet.of(ServiceSpecType.NATIVE_HELM), ManifestStoreType.HelmNGRepo);
 
   private final HelmSubCommandType subCommandType;
   private final Set<String> serviceSpecTypes;
-
-  HelmCommandFlagType(HelmSubCommandType subCommandType, Set<String> serviceSpecTypes) {
+  private final Set<String> storeTypes;
+  HelmCommandFlagType(HelmSubCommandType subCommandType, Set<String> serviceSpecTypes, Set<String> storeTypes) {
     this.subCommandType = subCommandType;
     this.serviceSpecTypes = serviceSpecTypes;
+    this.storeTypes = storeTypes;
   }
 }
