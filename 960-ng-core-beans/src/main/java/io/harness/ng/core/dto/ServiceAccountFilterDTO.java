78696b33d11d54b899231d7c2c5c52c1260d7970
Meenakshi Raikwar <meenakshi.raikwar@harness.io>
2021-12-17 14:04:59 +0530

    [PL-21407]: Added description to Filter DTO

--- 960-ng-core-beans/src/main/java/io/harness/ng/core/dto/ServiceAccountFilterDTO.java
+++ 960-ng-core-beans/src/main/java/io/harness/ng/core/dto/ServiceAccountFilterDTO.java
@@ -2,8 +2,11 @@ package io.harness.ng.core.dto;
 
 import static io.harness.annotations.dev.HarnessTeam.PL;
 
+import io.harness.NGCommonEntityConstants;
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.filter.FilterConstants;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -14,10 +17,12 @@ import lombok.experimental.FieldDefaults;
 @Data
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Schema(name = "ServiceAccountFilter",
+    description = "This has the details of the Service Account Filter as defined in Harness")
 public class ServiceAccountFilterDTO {
-  String searchTerm;
-  String accountIdentifier;
-  String orgIdentifier;
-  String projectIdentifier;
-  List<String> identifiers;
+  @Schema(description = FilterConstants.SEARCH_TERM) String searchTerm;
+  @Schema(description = NGCommonEntityConstants.ACCOUNT_PARAM_MESSAGE) String accountIdentifier;
+  @Schema(description = NGCommonEntityConstants.ORG_PARAM_MESSAGE) String orgIdentifier;
+  @Schema(description = NGCommonEntityConstants.PROJECT_PARAM_MESSAGE) String projectIdentifier;
+  @Schema(description = FilterConstants.IDENTIFIER_LIST) List<String> identifiers;
 }
