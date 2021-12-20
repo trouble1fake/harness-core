5a176e073ff435c12a10c8be2c927ed05a579429
Ved Tambat <ved.atultambat@harness.io>
2021-12-20 13:25:21 +0530

    [PIE-1860]: config secrets changes

--- 960-yaml-sdk/src/main/java/io/harness/yaml/schema/client/config/YamlSchemaClientConfig.java
+++ 960-yaml-sdk/src/main/java/io/harness/yaml/schema/client/config/YamlSchemaClientConfig.java
@@ -3,6 +3,7 @@ package io.harness.yaml.schema.client.config;
 import static io.harness.annotations.dev.HarnessTeam.DX;
 
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.secret.ConfigSecret;
 
 import java.util.Map;
 import lombok.Builder;
@@ -12,5 +13,5 @@ import lombok.Value;
 @Builder
 @OwnedBy(DX)
 public class YamlSchemaClientConfig {
-  Map<String, YamlSchemaHttpClientConfig> yamlSchemaHttpClientMap;
+  @ConfigSecret Map<String, YamlSchemaHttpClientConfig> yamlSchemaHttpClientMap;
 }
