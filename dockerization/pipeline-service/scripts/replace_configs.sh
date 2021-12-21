e59be5f81f6e9a8c13f389e5c2a1cbcc8016d28d
Ved Tambat <ved.atultambat@harness.io>
2021-12-21 13:31:23 +0530

    [PIE-1860]: removed repeated changes

--- dockerization/pipeline-service/scripts/replace_configs.sh
+++ dockerization/pipeline-service/scripts/replace_configs.sh
@@ -97,17 +97,6 @@ if [[ "" != "$MONGO_TRANSACTIONS_ALLOWED" ]]; then
   yq write -i $CONFIG_FILE mongo.transactionsEnabled $MONGO_TRANSACTIONS_ALLOWED
 fi
 
-if [[ "" != "$MONGO_HOSTS_AND_PORTS" ]]; then
-  yq delete -i $CONFIG_FILE mongo.uri
-  yq write -i $CONFIG_FILE mongo.username "$MONGO_USERNAME"
-  yq write -i $CONFIG_FILE mongo.password "$MONGO_PASSWORD"
-  yq write -i $CONFIG_FILE mongo.database "$MONGO_DATABASE"
-  yq write -i $CONFIG_FILE mongo.schema "$MONGO_SCHEMA"
-  write_mongo_hosts_and_ports mongo "$MONGO_HOSTS_AND_PORTS"
-  write_mongo_params mongo "$MONGO_PARAMS"
-fi
-
-
 if [[ "" != "$DISTRIBUTED_LOCK_IMPLEMENTATION" ]]; then
   yq write -i $CONFIG_FILE distributedLockImplementation "$DISTRIBUTED_LOCK_IMPLEMENTATION"
 fi
