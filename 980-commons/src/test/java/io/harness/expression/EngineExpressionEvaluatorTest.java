2656d5d36b82a86c4792b698ebb352549f52fbd9
Sahil Hindwani <sahil.hindwani@harness.io>
2021-12-22 14:34:01 +0530

    [CI-3184]: Fix expression evaluation to null value

--- 980-commons/src/test/java/io/harness/expression/EngineExpressionEvaluatorTest.java
+++ 980-commons/src/test/java/io/harness/expression/EngineExpressionEvaluatorTest.java
@@ -131,7 +131,6 @@ public class EngineExpressionEvaluatorTest extends CategoryTest {
     validateExpression(evaluator, "bVal2.cVal1.strVal", "c21");
     validateExpression(evaluator, "bVal2.cVal2.strVal", "finalC22", true);
     validateExpression(evaluator, "bVal2.strVal1", "finalB21", true);
-    validateSingleExpression(evaluator, "bVal2.strVal2", "<+bVal2.strVal2>", true);
     validateSingleExpression(evaluator,
         "obj."
             + "bVal2.strVal2",
