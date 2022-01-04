486a73c83818a54fbe1e7ba381b079196cf5453c
Mounik V V S S <mounik.velavalivenkata@harness.io>
2022-01-04 19:07:40 +0530

    fix: [CDC-16418]: Added few files

---
+++ 865-cg-events/src/main/java/io/harness/beans/event/cg/CgWorkflowResumePayload.java
@@ -0,0 +1,39 @@
+package io.harness.beans.event.cg;
+
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.beans.CreatedByType;
+import io.harness.beans.EmbeddedUser;
+import io.harness.beans.event.cg.application.ApplicationEventData;
+import io.harness.beans.event.cg.entities.EnvironmentEntity;
+import io.harness.beans.event.cg.entities.InfraDefinitionEntity;
+import io.harness.beans.event.cg.entities.ServiceEntity;
+import io.harness.beans.event.cg.pipeline.ExecutionArgsEventData;
+import io.harness.beans.event.cg.pipeline.PipelineEventData;
+import io.harness.beans.event.cg.pipeline.PipelineExecData;
+import io.harness.beans.event.cg.workflow.WorkflowEventData;
+import io.harness.beans.event.cg.workflow.WorkflowExecData;
+import io.harness.beans.event.cg.workflow.WorkflowStepInfo;
+
+import java.util.List;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@OwnedBy(HarnessTeam.CDC)
+public class CgWorkflowResumePayload extends CgWorkflowExecutionPayload {
+  public CgWorkflowResumePayload() {}
+
+  private List<WorkflowStepInfo> steps;
+
+  @Builder
+  public CgWorkflowResumePayload(ApplicationEventData application, WorkflowEventData workflow,
+      PipelineEventData pipeline, ExecutionArgsEventData executionArgs, EmbeddedUser triggeredBy,
+      CreatedByType triggeredByType, long startedAt, List<ServiceEntity> services, List<EnvironmentEntity> environments,
+      List<InfraDefinitionEntity> infraDefinitions, WorkflowExecData workflowExecution,
+      PipelineExecData pipelineExecution, List<WorkflowStepInfo> steps) {
+    super(application, workflow, pipeline, executionArgs, triggeredBy, triggeredByType, startedAt, services,
+        environments, infraDefinitions, workflowExecution, pipelineExecution);
+    this.steps = steps;
+  }
+}
---
+++ 865-cg-events/src/main/java/io/harness/beans/event/cg/pipeline/PipelineExecData.java
@@ -0,0 +1,19 @@
+package io.harness.beans.event.cg.pipeline;
+
+import static io.harness.annotations.dev.HarnessTeam.CDC;
+
+import io.harness.annotations.dev.OwnedBy;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@OwnedBy(CDC)
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PipelineExecData {
+  private String id;
+}
