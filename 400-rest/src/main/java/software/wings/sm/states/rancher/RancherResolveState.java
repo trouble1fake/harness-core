c5782dcb5c2be1d737e2cbd904bf3eb8753cce01
Sainath Batthala <sainath.batthala@harness.io>
2022-01-11 10:35:05 +0530

    [CDP-19684]: Rancher backend changes

--- 400-rest/src/main/java/software/wings/sm/states/rancher/RancherResolveState.java
+++ 400-rest/src/main/java/software/wings/sm/states/rancher/RancherResolveState.java
@@ -1,6 +1,10 @@
 package software.wings.sm.states.rancher;
 
-import com.google.inject.Inject;
+import static io.harness.data.structure.UUIDGenerator.generateUuid;
+import static io.harness.exception.ExceptionUtils.getMessage;
+
+import static software.wings.sm.StateType.RANCHER_RESOLVE;
+
 import io.harness.beans.Cd1SetupFields;
 import io.harness.beans.DelegateTask;
 import io.harness.beans.ExecutionStatus;
@@ -12,9 +16,7 @@ import io.harness.exception.InvalidRequestException;
 import io.harness.exception.WingsException;
 import io.harness.security.encryption.EncryptedDataDetail;
 import io.harness.tasks.ResponseData;
-import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
-import org.mongodb.morphia.annotations.Transient;
+
 import software.wings.api.PhaseElement;
 import software.wings.api.RancherClusterElement;
 import software.wings.beans.RancherConfig;
@@ -35,110 +37,118 @@ import software.wings.sm.ExecutionContext;
 import software.wings.sm.ExecutionResponse;
 import software.wings.sm.State;
 import software.wings.sm.StateExecutionData;
+import software.wings.sm.states.utils.StateTimeoutUtils;
+import software.wings.stencils.DefaultValue;
 
+import com.github.reinert.jjschema.Attributes;
+import com.google.inject.Inject;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
-import static io.harness.data.structure.UUIDGenerator.generateUuid;
-import static io.harness.exception.ExceptionUtils.getMessage;
-import static software.wings.sm.StateType.RANCHER_RESOLVE;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.mongodb.morphia.annotations.Transient;
 
 @Slf4j
 public class RancherResolveState extends State {
+  @Inject public RancherStateHelper rancherStateHelper;
+  @Inject @Transient private SweepingOutputService sweepingOutputService;
+  @Inject private transient SettingsService settingsService;
+  @Inject private transient DelegateService delegateService;
+  @Inject private transient InfrastructureDefinitionService infrastructureDefinitionService;
+  @Inject private SecretManager secretManager;
+  @Getter @Setter @Attributes(title = "Timeout (Minutes)") @DefaultValue("10") private Integer stateTimeoutInMinutes;
 
-    @Inject public RancherStateHelper rancherStateHelper;
-    @Inject @Transient private SweepingOutputService sweepingOutputService;
-    @Inject private transient SettingsService settingsService;
-    @Inject private transient DelegateService delegateService;
-    @Inject private transient InfrastructureDefinitionService infrastructureDefinitionService;
-    @Inject private SecretManager secretManager;
+  public RancherResolveState(String name) {
+    super(name, RANCHER_RESOLVE.name());
+  }
 
-    public RancherResolveState(String name) {
-        super(name, RANCHER_RESOLVE.name());
+  @Override
+  public ExecutionResponse execute(ExecutionContext context) {
+    try {
+      return executeInternal(context);
+    } catch (WingsException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new InvalidRequestException(getMessage(e), e);
     }
+  }
 
-    @Override
-    public ExecutionResponse execute(ExecutionContext context) {
-        try {
-            return executeInternal(context);
-        } catch (WingsException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new InvalidRequestException(getMessage(e), e);
-        }
-    }
-    
-    private ExecutionResponse executeInternal(ExecutionContext context) {
-        RancherKubernetesInfrastructureMapping rancherKubernetesInfrastructureMapping = rancherStateHelper.fetchRancherKubernetesInfrastructureMapping(context);
+  private ExecutionResponse executeInternal(ExecutionContext context) {
+    RancherKubernetesInfrastructureMapping rancherKubernetesInfrastructureMapping =
+        rancherStateHelper.fetchRancherKubernetesInfrastructureMapping(context);
 
-        InfrastructureDefinition infrastructureDefinition = infrastructureDefinitionService.getInfraDefById(context.getAccountId(), rancherKubernetesInfrastructureMapping.getInfrastructureDefinitionId());
-        SettingAttribute settingAttribute = settingsService.get(rancherKubernetesInfrastructureMapping.getComputeProviderSettingId());
-        RancherConfig rancherConfig = (RancherConfig) settingAttribute.getValue();
+    InfrastructureDefinition infrastructureDefinition = infrastructureDefinitionService.getInfraDefById(
+        context.getAccountId(), rancherKubernetesInfrastructureMapping.getInfrastructureDefinitionId());
+    SettingAttribute settingAttribute =
+        settingsService.get(rancherKubernetesInfrastructureMapping.getComputeProviderSettingId());
+    RancherConfig rancherConfig = (RancherConfig) settingAttribute.getValue();
 
-        List<EncryptedDataDetail> encryptedDataDetails =
-                secretManager.getEncryptionDetails(rancherConfig, context.getAppId(), context.getWorkflowExecutionId());
+    List<EncryptedDataDetail> encryptedDataDetails =
+        secretManager.getEncryptionDetails(rancherConfig, context.getAppId(), context.getWorkflowExecutionId());
 
-        RancherResolveClustersTaskParameters rancherResolveClustersTaskParameters =
-                RancherResolveClustersTaskParameters.builder()
-                        .rancherConfig(rancherConfig)
-                        .encryptedDataDetails(encryptedDataDetails)
-                        .clusterSelectionCriteria(((RancherKubernetesInfrastructure) infrastructureDefinition.getInfrastructure()).getClusterSelectionCriteria())
-                        .build();
+    RancherResolveClustersTaskParameters rancherResolveClustersTaskParameters =
+        RancherResolveClustersTaskParameters.builder()
+            .rancherConfig(rancherConfig)
+            .encryptedDataDetails(encryptedDataDetails)
+            .clusterSelectionCriteria(((RancherKubernetesInfrastructure) infrastructureDefinition.getInfrastructure())
+                                          .getClusterSelectionCriteria())
+            .build();
 
-        String waitId = generateUuid();
-        DelegateTask delegateTask =
-                DelegateTask.builder()
-                        .accountId(context.getApp().getAccountId())
-                        .setupAbstraction(Cd1SetupFields.APP_ID_FIELD, context.getApp().getUuid())
-                        .waitId(waitId)
-                        .data(TaskData.builder()
-                                .async(true)
-                                .taskType(TaskType.RANCHER_RESOLVE_CLUSTERS.name())
-                                .parameters(new Object[] {rancherResolveClustersTaskParameters})
-                                // TODO: Update task timeout from state
-                                .timeout(60000)
-                                .build())
-                        .selectionLogsTrackingEnabled(true)
-                        .build();
+    String waitId = generateUuid();
+    DelegateTask delegateTask = DelegateTask.builder()
+                                    .accountId(context.getApp().getAccountId())
+                                    .setupAbstraction(Cd1SetupFields.APP_ID_FIELD, context.getApp().getUuid())
+                                    .waitId(waitId)
+                                    .data(TaskData.builder()
+                                              .async(true)
+                                              .taskType(TaskType.RANCHER_RESOLVE_CLUSTERS.name())
+                                              .parameters(new Object[] {rancherResolveClustersTaskParameters})
+                                              // TODO: Update task timeout from state
+                                              .timeout(60000)
+                                              .build())
+                                    .selectionLogsTrackingEnabled(true)
+                                    .build();
 
-        delegateService.queueTask(delegateTask);
-        StateExecutionData executionData = new StateExecutionData();
+    delegateService.queueTask(delegateTask);
+    StateExecutionData executionData = new StateExecutionData();
 
-        return ExecutionResponse.builder()
-                .async(true)
-                .correlationIds(Collections.singletonList(waitId))
-                .stateExecutionData(executionData)
-                .build();
-    }
+    return ExecutionResponse.builder()
+        .async(true)
+        .correlationIds(Collections.singletonList(waitId))
+        .stateExecutionData(executionData)
+        .build();
+  }
 
+  @Override
+  public ExecutionResponse handleAsyncResponse(ExecutionContext context, Map<String, ResponseData> response) {
+    RancherResolveClustersResponse rancherResolveClustersResponse =
+        (RancherResolveClustersResponse) response.values().iterator().next();
+    List<String> clusterNames = rancherResolveClustersResponse.getClusters();
+    List<RancherClusterElement> clusterElements =
+        clusterNames.stream()
+            .map(name -> new RancherClusterElement(UUIDGenerator.generateUuid(), name))
+            .collect(Collectors.toList());
 
-    @Override
-    public ExecutionResponse handleAsyncResponse(ExecutionContext context, Map<String, ResponseData> response) {
-        RancherResolveClustersResponse rancherResolveClustersResponse = (RancherResolveClustersResponse) response.values().iterator().next();
-        List<String> clusterNames = rancherResolveClustersResponse.getClusters();
-        List<RancherClusterElement> clusterElements = clusterNames.stream()
-                .map(name -> new RancherClusterElement(UUIDGenerator.generateUuid(), name))
-                .collect(Collectors.toList());
+    sweepingOutputService.save(
+        context.prepareSweepingOutputBuilder(SweepingOutputInstance.Scope.WORKFLOW)
+            .name(RancherK8sClusterProcessor.RancherClusterElementList.getSweepingOutputID(context))
+            .value(new RancherK8sClusterProcessor.RancherClusterElementList(clusterElements))
+            .build());
 
-        sweepingOutputService.save(context.prepareSweepingOutputBuilder(SweepingOutputInstance.Scope.WORKFLOW)
-                .name(RancherK8sClusterProcessor.RancherClusterElementList.getSweepingOutputID(context))
-                .value(new RancherK8sClusterProcessor.RancherClusterElementList(clusterElements))
-                .build());
+    return ExecutionResponse.builder().executionStatus(ExecutionStatus.SUCCESS).build();
+  }
 
-        return ExecutionResponse.builder().executionStatus(ExecutionStatus.SUCCESS).build();
-    }
+  @Override
+  public Integer getTimeoutMillis() {
+    return StateTimeoutUtils.getTimeoutMillisFromMinutes(stateTimeoutInMinutes);
+  }
 
-    @NotNull
-    private String getPhaseParamName(ExecutionContext context) {
-        return ((PhaseElement) context.getContextElement(ContextElementType.PARAM,
-                PhaseElement.PHASE_PARAM))
-                .getPhaseName().trim();
-    }
-
-    @Override
-    public void handleAbortEvent(ExecutionContext context) {
-        // NoOp
-    }
+  @Override
+  public void handleAbortEvent(ExecutionContext context) {
+    // NoOp
+  }
 }
