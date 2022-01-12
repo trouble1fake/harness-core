6bae06398bbefc87e998a456e7d4ea9152310ccc
Abhinav Singh <abhinav.singh@harness.io>
2022-01-12 14:53:53 +0530

    feat: [DEL-2958]: style

--- 930-delegate-tasks/src/test/java/io/harness/delegate/configuration/InstallUtilsTest.java
+++ 930-delegate-tasks/src/test/java/io/harness/delegate/configuration/InstallUtilsTest.java
@@ -47,7 +47,7 @@ public class InstallUtilsTest extends CategoryTest implements MockableTestMixin 
   @Test
   @Owner(developers = VUK)
   @Category(UnitTests.class)
-  @Ignore("Use cdn is enabled")
+  @Ignore("Disable since Use cdn is enabled")
   public void testGetTerraformConfigInspectDownloadUrlPathWindows() throws Exception {
     setStaticFieldValue(SystemUtils.class, "IS_OS_WINDOWS", true);
 
@@ -62,7 +62,7 @@ public class InstallUtilsTest extends CategoryTest implements MockableTestMixin 
   @Test
   @Owner(developers = VUK)
   @Category(UnitTests.class)
-  @Ignore("Use cdn is enabled")
+  @Ignore("Disable since Use cdn is enabled")
   public void testGetTerraformConfigInspectDownloadUrlPath() throws Exception {
     boolean useCdn = delegateConfiguration.isUseCdn();
     assertThat(useCdn).isFalse();
@@ -100,7 +100,7 @@ public class InstallUtilsTest extends CategoryTest implements MockableTestMixin 
   @Test
   @Owner(developers = SHUBHAM)
   @Category(UnitTests.class)
-  @Ignore("Use cdn is enabled")
+  @Ignore("Disable since Use cdn is enabled")
   public void testGetScmDownloadUrlPath() throws Exception {
     boolean useCdn = delegateConfiguration.isUseCdn();
     assertThat(useCdn).isFalse();
