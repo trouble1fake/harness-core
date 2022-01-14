67f773e0d54ac99683d89ad3de93650d2b5fa3e7
Brijesh Dhakar <brijesh.dhakar@harness.io>
2022-01-14 10:13:12 +0530

    [feat]: [PIE-2349]: sending real account id in community group events (#29906)
    * [feat]: [PIE-2349]: sending real account id in community group events
    
    * [feat]: [PIE-2349]: sending real account id in community group events
    
    * [feat]: [PIE-2349]: sending real account id in community group events

--- 800-pipeline-service/src/main/java/io/harness/metrics/PipelineTelemetryPublisher.java
+++ 800-pipeline-service/src/main/java/io/harness/metrics/PipelineTelemetryPublisher.java
@@ -24,6 +24,7 @@ import org.springframework.data.mongodb.core.query.Criteria;
 @Slf4j
 @Singleton
 public class PipelineTelemetryPublisher {
+  public static final String GLOBAL_ACCOUNT_ID = "__GLOBAL_ACCOUNT_ID__";
   @Inject PMSPipelineService pmsPipelineService;
   @Inject PMSExecutionService pmsExecutionService;
   @Inject TelemetryReporter telemetryReporter;
@@ -43,7 +44,7 @@ public class PipelineTelemetryPublisher {
       Long totalPipelinesExecuted = 0L;
 
       String accountId = getAccountId();
-      if (EmptyPredicate.isNotEmpty(accountId)) {
+      if (EmptyPredicate.isNotEmpty(accountId) || !accountId.equals(GLOBAL_ACCOUNT_ID)) {
         Criteria criteria =
             Criteria.where(PipelineEntityKeys.createdAt).gt(System.currentTimeMillis() - MILLISECONDS_IN_A_DAY);
         pipelinesCreatedInADay = pmsPipelineService.countAllPipelines(criteria);
@@ -80,9 +81,10 @@ public class PipelineTelemetryPublisher {
 
   private String getAccountId() {
     List<AccountDTO> accountDTOList = RestClientUtils.getResponse(accountClient.getAllAccounts());
-    if (accountDTOList == null || accountDTOList.size() == 0) {
-      return null;
+    String accountId = accountDTOList.get(0).getIdentifier();
+    if (accountDTOList.size() > 1 && accountId.equals(GLOBAL_ACCOUNT_ID)) {
+      accountId = accountDTOList.get(1).getIdentifier();
     }
-    return accountDTOList.get(0).getIdentifier();
+    return accountId;
   }
 }
