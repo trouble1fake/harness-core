5b028d876135d4e8443ebbe02583d38b8a67725a
Nikunj Badjatya <nikunj.badjatya@harness.io>
2022-01-18 16:47:42 +0530

    [feat]: [CCM-5328]: Use the new client util

--- 440-connector-nextgen/src/main/java/io/harness/connector/validator/CEAzureConnectorValidator.java
+++ 440-connector-nextgen/src/main/java/io/harness/connector/validator/CEAzureConnectorValidator.java
@@ -12,6 +12,7 @@ import static io.harness.connector.ConnectivityStatus.FAILURE;
 import static io.harness.errorhandling.NGErrorHelper.DEFAULT_ERROR_SUMMARY;
 
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.ccm.RestCallToCENGClientUtils;
 import io.harness.ccm.manager.CENextGenResourceClient;
 import io.harness.connector.ConnectorResponseDTO;
 import io.harness.connector.ConnectorValidationResult;
@@ -21,7 +22,6 @@ import io.harness.errorhandling.NGErrorHelper;
 import io.harness.exception.InvalidRequestException;
 import io.harness.exception.UnexpectedException;
 import io.harness.ng.core.dto.ErrorDetail;
-import io.harness.utils.RestCallToNGManagerClientUtils;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -47,7 +47,7 @@ public class CEAzureConnectorValidator extends AbstractConnectorValidator {
       String orgIdentifier, String projectIdentifier, String identifier) {
     log.info("Calling ce-nextgen for connector {}", connectorResponseDTO);
     try {
-      return RestCallToNGManagerClientUtils.execute(
+      return RestCallToCENGClientUtils.execute(
           ceNextGenResourceClient.testConnection(accountIdentifier, connectorResponseDTO));
     } catch (InvalidRequestException | UnexpectedException ex) {
       log.info("Error could be in CENG microservice or in connecting to it");
