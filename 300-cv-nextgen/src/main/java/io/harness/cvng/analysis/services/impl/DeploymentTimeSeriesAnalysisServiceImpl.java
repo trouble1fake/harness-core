c57719e38b29c03486897028f3a09aceb14c9dff
Kapil Choudhary <kapil.choudhary@harness.io>
2022-01-28 19:35:34 +0530

    [CVS-9003]: added sorting of txn/metric pairs & resolved duplicate counts

--- 300-cv-nextgen/src/main/java/io/harness/cvng/analysis/services/impl/DeploymentTimeSeriesAnalysisServiceImpl.java
+++ 300-cv-nextgen/src/main/java/io/harness/cvng/analysis/services/impl/DeploymentTimeSeriesAnalysisServiceImpl.java
@@ -213,6 +213,12 @@ public class DeploymentTimeSeriesAnalysisServiceImpl implements DeploymentTimeSe
     return new ArrayList<>(nodeNameSet);
   }
 
+  @Override
+  public List<TransactionMetricInfo> getMetricsWithoutFiltersAndPagination(
+      String accountId, String verificationJobInstanceId) {
+    return getMetrics(accountId, verificationJobInstanceId, DeploymentTimeSeriesAnalysisFilter.builder().build());
+  }
+
   private List<TransactionMetricInfo> getMetrics(String accountId, String verificationJobInstanceId,
       DeploymentTimeSeriesAnalysisFilter deploymentTimeSeriesAnalysisFilter) {
     List<DeploymentTimeSeriesAnalysis> latestDeploymentTimeSeriesAnalysis =
@@ -270,8 +276,7 @@ public class DeploymentTimeSeriesAnalysisServiceImpl implements DeploymentTimeSe
     }
 
     List<TransactionMetricInfo> transactionMetricInfoList = new ArrayList<>(transactionMetricInfoSet);
-    transactionMetricInfoList.sort(
-        (d1, d2) -> Double.compare(d2.getTransactionMetric().getScore(), d1.getTransactionMetric().getScore()));
+    Collections.sort(transactionMetricInfoList);
     return transactionMetricInfoList;
   }
 
