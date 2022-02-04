797c38b764054e4382dd3f28229c696566fbde00
Shalini Agrawal <shalini.agarwal@harness.io>
2022-02-04 15:01:35 +0530

    fetching data with JOOQ queries

--- 800-pipeline-service/src/main/java/io/harness/pms/pipeline/service/PipelineDashboardServiceImpl.java
+++ 800-pipeline-service/src/main/java/io/harness/pms/pipeline/service/PipelineDashboardServiceImpl.java
@@ -24,6 +24,8 @@ import io.harness.pms.execution.ExecutionStatus;
 import io.harness.timescaledb.DBUtils;
 import io.harness.timescaledb.Tables;
 import io.harness.timescaledb.TimeScaleDBService;
+import io.harness.timescaledb.tables.pojos.PipelineExecutionSummaryCd;
+import io.harness.timescaledb.tables.pojos.PipelineExecutionSummaryCi;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -35,6 +37,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.jooq.DSLContext;
 import org.jooq.Query;
 import org.jooq.conf.ParamType;
@@ -55,24 +58,58 @@ public class PipelineDashboardServiceImpl implements PipelineDashboardService {
 
   private static final int MAX_RETRY_COUNT = 5;
 
-  public String queryBuilderSelectStatusAndTime(String accountId, String orgId, String projectId, String pipelineId,
-      long startInterval, long endInterval, String tableName) {
-    Query selectStatusQuery =
-        this.dsl.select(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STATUS, Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS)
-            .from(Tables.PIPELINE_EXECUTION_SUMMARY_CD)
-            .where(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ACCOUNTID.eq(accountId)
-                       .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ORGIDENTIFIER.eq(orgId))
-                       .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PIPELINEIDENTIFIER.eq(pipelineId))
-                       .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PROJECTIDENTIFIER.eq(projectId))
-                       .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.ge(startInterval))
-                       .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.lt(endInterval))
-                       .and(accountId != null)
-                       .and(orgId != null)
-                       .and(projectId != null)
-                       .and(pipelineId != null)
-                       .and(startInterval > 0)
-                       .and(endInterval > 0));
-    return selectStatusQuery.getSQL(ParamType.INLINED);
+  public StatusAndTime queryBuilderSelectStatusAndTime(String accountId, String orgId, String projectId,
+      String pipelineId, long startInterval, long endInterval, String tableName) {
+    List<PipelineExecutionSummaryCd> pipelineExecutionSummaryCds;
+    List<PipelineExecutionSummaryCi> pipelineExecutionSummaryCis;
+    List<String> status = new ArrayList<>();
+    List<Long> time = new ArrayList<>();
+    if (tableName == CD_TableName) {
+      pipelineExecutionSummaryCds =
+          this.dsl.select(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STATUS, Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS)
+              .from(Tables.PIPELINE_EXECUTION_SUMMARY_CD)
+              .where(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ACCOUNTID.eq(accountId)
+                         .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ORGIDENTIFIER.eq(orgId))
+                         .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PIPELINEIDENTIFIER.eq(pipelineId))
+                         .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PROJECTIDENTIFIER.eq(projectId))
+                         .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.ge(startInterval))
+                         .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.lt(endInterval))
+                         .and(accountId != null)
+                         .and(orgId != null)
+                         .and(projectId != null)
+                         .and(pipelineId != null)
+                         .and(startInterval > 0)
+                         .and(endInterval > 0))
+              .fetch()
+              .into(PipelineExecutionSummaryCd.class);
+      for (PipelineExecutionSummaryCd x : pipelineExecutionSummaryCds) {
+        status.add(x.getStatus());
+        time.add(x.getStartts());
+      }
+    } else {
+      pipelineExecutionSummaryCis =
+          this.dsl.select(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STATUS, Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS)
+              .from(Tables.PIPELINE_EXECUTION_SUMMARY_CI)
+              .where(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ACCOUNTID.eq(accountId)
+                         .and(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ORGIDENTIFIER.eq(orgId))
+                         .and(Tables.PIPELINE_EXECUTION_SUMMARY_CI.PIPELINEIDENTIFIER.eq(pipelineId))
+                         .and(Tables.PIPELINE_EXECUTION_SUMMARY_CI.PROJECTIDENTIFIER.eq(projectId))
+                         .and(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS.ge(startInterval))
+                         .and(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS.lt(endInterval))
+                         .and(accountId != null)
+                         .and(orgId != null)
+                         .and(projectId != null)
+                         .and(pipelineId != null)
+                         .and(startInterval > 0)
+                         .and(endInterval > 0))
+              .fetch()
+              .into(PipelineExecutionSummaryCi.class);
+      for (PipelineExecutionSummaryCi x : pipelineExecutionSummaryCis) {
+        status.add(x.getStatus());
+        time.add(x.getStartts());
+      }
+    }
+    return StatusAndTime.builder().status(status).time(time).build();
   }
 
   public double getRate(long current, long previous) {
@@ -93,52 +130,100 @@ public class PipelineDashboardServiceImpl implements PipelineDashboardService {
     return rate;
   }
 
-  public String queryBuilderMean(String accountId, String orgId, String projectId, String pipelineId,
-      long startInterval, long endInterval, String tableName) {
-    Query selectMeanQuery =
-        this.dsl
-            .select(
-                DSL.avg((Tables.PIPELINE_EXECUTION_SUMMARY_CD.ENDTS.sub(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS))
-                            .div(1000)))
-            .from(Tables.PIPELINE_EXECUTION_SUMMARY_CD)
-            .where(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ACCOUNTID.eq(accountId)
-                       .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ORGIDENTIFIER.eq(orgId))
-                       .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PIPELINEIDENTIFIER.eq(pipelineId))
-                       .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PROJECTIDENTIFIER.eq(projectId))
-                       .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.ge(startInterval))
-                       .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.lt(endInterval))
-                       .and(accountId != null)
-                       .and(orgId != null)
-                       .and(projectId != null)
-                       .and(pipelineId != null)
-                       .and(startInterval > 0)
-                       .and(endInterval > 0)
-                       .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ENDTS != null));
-    return selectMeanQuery.getSQL(ParamType.INLINED);
+  public long queryBuilderMean(String accountId, String orgId, String projectId, String pipelineId, long startInterval,
+      long endInterval, String tableName) {
+    @NotNull List<Long> mean;
+    if (tableName == CD_TableName)
+      mean = this.dsl
+                 .select(DSL.avg(
+                     (Tables.PIPELINE_EXECUTION_SUMMARY_CD.ENDTS.sub(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS))
+                         .div(1000)))
+                 .from(Tables.PIPELINE_EXECUTION_SUMMARY_CD)
+                 .where(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ACCOUNTID.eq(accountId)
+                            .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ORGIDENTIFIER.eq(orgId))
+                            .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PIPELINEIDENTIFIER.eq(pipelineId))
+                            .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PROJECTIDENTIFIER.eq(projectId))
+                            .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.ge(startInterval))
+                            .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.lt(endInterval))
+                            .and(accountId != null)
+                            .and(orgId != null)
+                            .and(projectId != null)
+                            .and(pipelineId != null)
+                            .and(startInterval > 0)
+                            .and(endInterval > 0)
+                            .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ENDTS != null))
+                 .fetch()
+                 .into(long.class);
+    else
+      mean = this.dsl
+                 .select(DSL.avg(
+                     (Tables.PIPELINE_EXECUTION_SUMMARY_CI.ENDTS.sub(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS))
+                         .div(1000)))
+                 .from(Tables.PIPELINE_EXECUTION_SUMMARY_CI)
+                 .where(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ACCOUNTID.eq(accountId)
+                            .and(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ORGIDENTIFIER.eq(orgId))
+                            .and(Tables.PIPELINE_EXECUTION_SUMMARY_CI.PIPELINEIDENTIFIER.eq(pipelineId))
+                            .and(Tables.PIPELINE_EXECUTION_SUMMARY_CI.PROJECTIDENTIFIER.eq(projectId))
+                            .and(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS.ge(startInterval))
+                            .and(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS.lt(endInterval))
+                            .and(accountId != null)
+                            .and(orgId != null)
+                            .and(projectId != null)
+                            .and(pipelineId != null)
+                            .and(startInterval > 0)
+                            .and(endInterval > 0)
+                            .and(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ENDTS != null))
+                 .fetch()
+                 .into(long.class);
+    return mean.get(0);
   }
 
-  public String queryBuilderMedian(String accountId, String orgId, String projectId, String pipelineId,
+  public long queryBuilderMedian(String accountId, String orgId, String projectId, String pipelineId,
       long startInterval, long endInterval, String tableName) {
-    Query selectMedianQuery =
-        this.dsl
-            .select(DSL.percentileDisc(0.5).withinGroupOrderBy(
-                (Tables.PIPELINE_EXECUTION_SUMMARY_CD.ENDTS.sub(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS))
-                    .div(1000)))
-            .from(Tables.PIPELINE_EXECUTION_SUMMARY_CD)
-            .where(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ACCOUNTID.eq(accountId)
-                       .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ORGIDENTIFIER.eq(orgId))
-                       .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PIPELINEIDENTIFIER.eq(pipelineId))
-                       .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PROJECTIDENTIFIER.eq(projectId))
-                       .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.ge(startInterval))
-                       .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.lt(endInterval))
-                       .and(accountId != null)
-                       .and(orgId != null)
-                       .and(projectId != null)
-                       .and(pipelineId != null)
-                       .and(startInterval > 0)
-                       .and(endInterval > 0)
-                       .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ENDTS != null));
-    return selectMedianQuery.getSQL(ParamType.INLINED);
+    @NotNull List<Long> median;
+    if (tableName == CD_TableName)
+      median = this.dsl
+                   .select(DSL.percentileDisc(0.5).withinGroupOrderBy(
+                       (Tables.PIPELINE_EXECUTION_SUMMARY_CD.ENDTS.sub(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS))
+                           .div(1000)))
+                   .from(Tables.PIPELINE_EXECUTION_SUMMARY_CD)
+                   .where(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ACCOUNTID.eq(accountId)
+                              .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ORGIDENTIFIER.eq(orgId))
+                              .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PIPELINEIDENTIFIER.eq(pipelineId))
+                              .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.PROJECTIDENTIFIER.eq(projectId))
+                              .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.ge(startInterval))
+                              .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.STARTTS.lt(endInterval))
+                              .and(accountId != null)
+                              .and(orgId != null)
+                              .and(projectId != null)
+                              .and(pipelineId != null)
+                              .and(startInterval > 0)
+                              .and(endInterval > 0)
+                              .and(Tables.PIPELINE_EXECUTION_SUMMARY_CD.ENDTS != null))
+                   .fetch()
+                   .into(long.class);
+    else
+      median = this.dsl
+                   .select(DSL.percentileDisc(0.5).withinGroupOrderBy(
+                       (Tables.PIPELINE_EXECUTION_SUMMARY_CI.ENDTS.sub(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS))
+                           .div(1000)))
+                   .from(Tables.PIPELINE_EXECUTION_SUMMARY_CI)
+                   .where(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ACCOUNTID.eq(accountId)
+                              .and(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ORGIDENTIFIER.eq(orgId))
+                              .and(Tables.PIPELINE_EXECUTION_SUMMARY_CI.PIPELINEIDENTIFIER.eq(pipelineId))
+                              .and(Tables.PIPELINE_EXECUTION_SUMMARY_CI.PROJECTIDENTIFIER.eq(projectId))
+                              .and(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS.ge(startInterval))
+                              .and(Tables.PIPELINE_EXECUTION_SUMMARY_CI.STARTTS.lt(endInterval))
+                              .and(accountId != null)
+                              .and(orgId != null)
+                              .and(projectId != null)
+                              .and(pipelineId != null)
+                              .and(startInterval > 0)
+                              .and(endInterval > 0)
+                              .and(Tables.PIPELINE_EXECUTION_SUMMARY_CI.ENDTS != null))
+                   .fetch()
+                   .into(long.class);
+    return median.get(0);
   }
 
   public StatusAndTime queryCalculatorForStatusAndTime(String query) {
@@ -237,10 +322,9 @@ public class PipelineDashboardServiceImpl implements PipelineDashboardService {
 
     String tableName = selectTableFromModuleInfo(moduleInfo);
 
-    String query = queryBuilderSelectStatusAndTime(
+    StatusAndTime statusAndTime = queryBuilderSelectStatusAndTime(
         accountId, orgId, projectId, pipelineId, previousStartInterval, endInterval, tableName);
 
-    StatusAndTime statusAndTime = queryCalculatorForStatusAndTime(query);
     List<String> status = statusAndTime.getStatus();
     List<Long> time = statusAndTime.getTime();
 
@@ -265,22 +349,17 @@ public class PipelineDashboardServiceImpl implements PipelineDashboardService {
     }
 
     // mean calculation
-    String queryMeanCurrent =
-        queryBuilderMean(accountId, orgId, projectId, pipelineId, startInterval, endInterval, tableName);
-    long currentMean = queryCalculatorMean(queryMeanCurrent);
+    long currentMean = queryBuilderMean(accountId, orgId, projectId, pipelineId, startInterval, endInterval, tableName);
 
-    String queryMeanPrevious =
+    long previousMean =
         queryBuilderMean(accountId, orgId, projectId, pipelineId, previousStartInterval, startInterval, tableName);
-    long previousMean = queryCalculatorMean(queryMeanPrevious);
 
     // Median calculation
-    String queryMedianCurrent =
+    long currentMedian =
         queryBuilderMedian(accountId, orgId, projectId, pipelineId, startInterval, endInterval, tableName);
-    long currentMedian = queryCalculatorMedian(queryMedianCurrent);
 
-    String queryMedianPrevious =
+    long previousMedian =
         queryBuilderMedian(accountId, orgId, projectId, pipelineId, previousStartInterval, startInterval, tableName);
-    long previousMedian = queryCalculatorMedian(queryMedianPrevious);
 
     return DashboardPipelineHealthInfo.builder()
         .executions(
@@ -307,9 +386,8 @@ public class PipelineDashboardServiceImpl implements PipelineDashboardService {
 
     String tableName = selectTableFromModuleInfo(moduleInfo);
 
-    String query =
+    StatusAndTime statusAndTime =
         queryBuilderSelectStatusAndTime(accountId, orgId, projectId, pipelineId, startInterval, endInterval, tableName);
-    StatusAndTime statusAndTime = queryCalculatorForStatusAndTime(query);
     List<String> status = statusAndTime.getStatus();
     List<Long> time = statusAndTime.getTime();
 
